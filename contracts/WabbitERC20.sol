8f431d5f55f5d3cc6b81a2ce1c298c48d03b5253
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-05 13:01:31 +1100

    🎉 Update

--- contracts/WabbitERC20.sol
+++ contracts/WabbitERC20.sol
@@ -9,11 +9,11 @@ contract Wabbit is ERC20, Ownable {
 
     }
 
-    function mint(address _to, uint256 _amount) external {
+    function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
     }
 
-    function burn(address _from, uint256 _amount) external {
+    function burn(address _from, uint256 _amount) external onlyOwner {
         _burn(_from, _amount);
     }
 }
