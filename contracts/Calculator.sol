dfcfa56bfae261de1a06e204af9071066c7bab2b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-05 22:11:56 +1100

    🎉 Update

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -7,8 +7,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./ILPool.sol";
+import "./ICalculator.sol";
 
-contract Calculator is Ownable {
+contract Calculator is ICalculator, Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
--- contracts/ICalculator.sol
+++ contracts/ICalculator.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-interface Calculator {
+interface ICalculator {
     /**
      *  @notice gets the amount of approved tokens each pool token is worth
      *  @param _token address
