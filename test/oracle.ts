e2c92aeaf141a9665513ef79adad23b512164a7e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-06 07:58:37 +1100

    🎉 Update

---
+++ test/oracle.ts
@@ -0,0 +1,32 @@
+import { Contract } from "@ethersproject/contracts";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import config from "../config.json";
+
+describe("Oracle", function () {
+    let oracle: Contract;
+
+    beforeEach(async () => {
+        const Oracle = await ethers.getContractFactory("Oracle");
+        oracle = await Oracle.deploy(config.routerAddress, "", (1e5).toString());
+        await oracle.deployed();
+    });
+
+    it("Should return the trade value of two non-pool tokens", async function () {
+        const asset1 = "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E"; // DAI
+        const asset2 = "0x841fad6eae12c286d1fd18d1d525dffa75c7effe"; // BOO
+
+        // const Greeter = await ethers.getContractFactory("Greeter");
+        // const greeter = await Greeter.deploy("Hello, world!");
+        // await greeter.deployed();
+
+        // expect(await greeter.greet()).to.equal("Hello, world!");
+
+        // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+        // // wait until the transaction is mined
+        // await setGreetingTx.wait();
+
+        // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    });
+});
