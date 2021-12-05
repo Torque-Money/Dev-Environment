7dbcca38056a63a4630b696867fcf653e49d19ba
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-06 09:38:44 +1100

    🎉 Update

--- test/oracle.ts
+++ test/oracle.ts
@@ -8,7 +8,8 @@ describe("Oracle", function () {
     let lPool: Contract;
     const decimals = 1e5;
 
-    const pairs = ["0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E", "0x841fad6eae12c286d1fd18d1d525dffa75c7effe"]; // DAI, BOO
+    const assets = ["0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E", "0x841fad6eae12c286d1fd18d1d525dffa75c7effe"]; // DAI, BOO
+    const poolAssets: string[] = []; // waDAI, waBOO
 
     beforeEach(async () => {
         const LPool = await ethers.getContractFactory("LPool");
@@ -21,12 +22,19 @@ describe("Oracle", function () {
     });
 
     it("Should approve the asset pairs for the pool and get the pool tokens", async () => {
-        await lPool.approveAsset(pairs[0], "Wabbit Dai", "waDAI");
-        await lPool.approveAsset(pairs[1], "Wabbit Boo", "waBOO");
+        await lPool.approveAsset(assets[0], "Wabbit Dai", "waDAI");
+        await lPool.approveAsset(assets[1], "Wabbit Boo", "waBOO");
+
+        const poolAsset1 = await lPool.getPoolToken(assets[0]);
+        const poolAsset2 = await lPool.getPoolToken(assets[1]);
+        poolAssets.push(poolAsset1);
+        poolAssets.push(poolAsset2);
+
+        console.log(`Approved assets ${assets} that correspond to pool tokens ${poolAssets}`);
     });
 
     it("Should return the trade value of two non-pool tokens", async () => {
-        const value = await oracle.pairValue(pairs[0], pairs[1]);
+        const value = await oracle.pairValue(assets[0], assets[1]);
         const result = value.toNumber() / decimals;
         console.log(`Value: ${result}`);
     });
