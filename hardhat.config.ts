ab45ad098c4d1dd6c65acab740986ff3e8c2f714
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-05 12:46:32 +1100

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,17 @@
 import "@nomiclabs/hardhat-waffle";
 
+// Fork from mainnet to local environment
 const NETWORK_URL = "https://rpc.ftm.tools/";
+const BLOCK_NUMBER = 24017532;
 
 export default {
     solidity: "0.8.4",
+    networks: {
+        hardhat: {
+            forking: {
+                url: NETWORK_URL,
+                blockNUmber: BLOCK_NUMBER,
+            },
+        },
+    },
 };
