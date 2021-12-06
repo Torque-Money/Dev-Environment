f5863fc91dbbbc2039a3e73c45e2fdceb42594e6
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-06 22:29:37 +1100

    🎉 Update

---
+++ contracts/Lending.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./ILending.sol";
+
+contract Lending is ILending, Ownable {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+}
