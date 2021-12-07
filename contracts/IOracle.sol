fd090892671d589a13eb730f4abcfe765b517cc1
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-08 09:40:15 +1100

    🎉 Update

--- contracts/IOracle.sol
+++ contracts/IOracle.sol
@@ -2,12 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface IOracle {
-    function requestPrice(address _token1, address _token2) external;
-
-    function useRequestedPrice(address _token1, address _token2) external view returns (uint256);
-
-    function getRequestExpiry() external view returns (uint256);
-
     function pairPrice(address _token1, address _token2) external view returns (uint256);
 
     function getDecimals() external view returns (uint256);
--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -12,59 +12,44 @@ contract Oracle is IOracle, Context {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    UniswapV2Router02 private router;
-
+    UniswapV2Router02[] private routers;
     uint256 private decimals;
 
-    struct RequestedPrice {
-        uint256 price;
-        uint256 timeRequested;
-    }
-    mapping(bytes => RequestedPrice) private requestedPrices;
-    uint256 requestExpiry;
-
-    constructor(UniswapV2Router02 router_, uint256 decimals_, uint256 requestExpiry_) {
-        router = router_;
+    constructor(uint256 decimals_) {
         decimals = decimals_;
-        requestExpiry = requestExpiry_;
     }
 
-    function requestPrice(address _token1, address _token2) public override {
-        // Get the pair hash and timestamp of the request
-        bytes memory pairId = abi.encodePacked(_token1, _token2);
-        uint256 timestamp = block.timestamp;
-        uint256 price = pairPrice(_token1, _token2);
-
-        // Record the timestamp and the value of the request
-        requestedPrices[pairId] = RequestedPrice({
-            price: price,
-            timeRequested: timestamp
-        });
+    function _swap(int256[] memory array, uint256 i, uint256 j) private pure {
+        (array[i], array[j]) = (array[j], array[i]);
     }
 
-    function useRequestedPrice(address _token1, address _token2) public view override returns (uint256) {
-        // Get the requested value
-        bytes memory pairId = abi.encodePacked(_token1, _token2);
-        RequestedPrice memory req = requestedPrices[pairId];
-
-        // Check that the consumer requested the value between a specific amount of time
-        require(block.timestamp > req.timeRequested, "You must wait for the cooldown period to expire before consuming this value");
-        require(block.timestamp < req.timeRequested + requestExpiry, "This requested price has expired, please request again");
-
-        // Return the requested value for use
-        return req.price;
+    function _sort(int256[] memory array, uint256 begin, uint256 end) private pure {
+        if (begin < end) {
+            uint256 j = begin;
+            int256 pivot = array[j];
+            for (uint256 i = begin + 1; i < end; ++i) {
+                if (array[i] < pivot) {
+                    _swap(array, i, ++j);
+                }
+            }
+            _swap(array, begin, j);
+            _sort(array, begin, j);
+            _sort(array, j + 1, end);
+        }
     }
 
-    function getRequestExpiry() public view override returns (uint256) {
-        return requestExpiry;
+    function _median(uint256[] memory array) private pure returns(int256) {
+        uint256 length = array.length;
+        _sort(array, 0, length);
+        return length % 2 == 0 ? array[length/2-1].add(array[length/2]).div(2) : array[length/2];
     }
 
-    function pairPrice(address _token1, address _token2) public view override returns (uint256) {
+    function pairPrice(IERC20 _token1, IERC20 _token2) public view override returns (uint256) {
         // If they are the same return 1 to 1 conversion
         if (_token1 == _token2) return decimals;
 
         // Update the path if the tokens are pool tokens, and return the converted values if we are trying to compare the pool asset with its approved asset
-        address[] memory path = new address[](2);
+        IERC20[] memory path = new IERC20[](2);
         path[0] = _token1;
         path[1] = _token2;
 
