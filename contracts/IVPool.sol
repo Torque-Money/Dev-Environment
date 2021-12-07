503a8a6dfdae413b1fca5ce3291d3d6207c3ca3a
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-07 20:33:31 +1100

    🎉 Update

--- contracts/IVPool.sol
+++ contracts/IVPool.sol
@@ -4,26 +4,62 @@ pragma solidity ^0.8.0;
 interface IVPool {
     // ======== Check the staking period and cooldown periods ========
 
+    /**
+     *  @dev Checks if the period Id is on a cooldown
+     *  @param _periodId The id of the period to check if it is on cooldown
+     */
     function isCooldown(uint256 _periodId) external view returns (bool);
 
+    /**
+     *  @dev Checks if the current period is on cooldown
+     */
     function isCooldown() external view returns (bool);
 
+    /**
+     *  @dev Checks if the specified period is the current period
+     *  @param _periodId The id of the period to check
+     */
     function isCurrentPeriod(uint256 _periodId) external view returns (bool);
 
+    /**
+     *  @dev Returns the id of the current period
+     */
     function currentPeriodId() external view returns (uint256);
 
     // ======== Approved tokens ========
 
+    /**
+     *  @dev Approves a token for use with the protocol
+     *  @param _token The address of the token to approve
+     */
     function approveToken(address _token) external;
 
+    /**
+     *  @dev Returns whether or not a token is approved
+     *  @param _token The address of the token to check
+     */
     function isApproved(address _token) external view returns (bool);
 
+    /**
+     *  @dev Returns a list of approved tokens
+     */
     function getApproved() external view returns (address[] memory);
 
     // ======== Balance management ========
 
+    /**
+     *  @dev Returns the deposited balance of a given account for a given token for a given period
+     *  @param _account The account to check the deposited balance of
+     *  @param _token The token to check the desposited balance of
+     *  @param _periodId The id of the period of the balance to be checked
+     */
     function balanceOf(address _account, address _token, uint256 _periodId) external view returns (uint256);
 
+    /**
+     *  @dev Returns the deposited balance of a given account for a given token at the current period
+     *  @param _account The account to check the deposited balance of
+     *  @param _token The token to check the deposited balance of
+     */
     function balanceOf(address _account, address _token) external view returns (uint256);
 
     function redeemValue(address _token, uint256 _periodId, uint256 _amount) external view returns (uint256);
@@ -39,6 +75,7 @@ interface IVPool {
     function withdraw(address _token, uint256 _amount, address _to) external;
 
     // ======== Events ========
+
     event Stake(address indexed sender, address indexed token, uint256 indexed periodId, uint256 amount);
     event Redeem(address indexed sender, address indexed token, uint256 indexed periodId, uint256 amount, uint256 liquidity);
 
