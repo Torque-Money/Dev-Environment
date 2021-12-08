e82212890ac9de97c92f7bb58bdb86311ef6a964
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-08 12:39:56 +1100

    🎉 Update

--- contracts/Margin.sol
+++ contracts/Margin.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -9,36 +10,46 @@ import "./IOracle.sol";
 import "./IVPool.sol";
 import "./ILiquidator.sol";
 
-contract Margin is IMargin {
+contract Margin is IMargin, Context {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
     IVPool private vPool;
     IOracle private oracle;
 
-    struct AccountBorrow {
+    struct BorrowAccount {
         uint256 collateral;
         uint256 borrowed;
+        uint256 initialPrice;
         uint256 borrowTime;
     }
     struct BorrowPeriod {
         uint256 totalBorrowed;
-        mapping(address => mapping(address => AccountBorrow)) collateral; // account => token => borrow - this way the same account can have different borrows with different collaterals independently
+        mapping(address => mapping(IERC20 => BorrowAccount)) collateral; // account => token => borrow - this way the same account can have different borrows with different collaterals independently
     }
     mapping(uint256 => mapping(IERC20 => BorrowPeriod)) private borrowPeriods;
     uint256 private minBorrowPeriod;
+    uint256 private minMarginLevel;
 
     uint256 private interestInterval;
 
-    constructor(IVPool vPool_, IOracle oracle_, uint256 minBorrowPeriod_, uint256 interestInterval_) {
+    constructor(IVPool vPool_, IOracle oracle_, uint256 minBorrowPeriod_, uint256 interestInterval_, uint256 minMarginLevel_) {
         vPool = vPool_;
         oracle = oracle_;
         minBorrowPeriod = minBorrowPeriod_;
         interestInterval = interestInterval_;
+        minMarginLevel = minMarginLevel_;
     }
+
+    // ======== Modifiers ========
 
     modifier approvedOnly(IERC20 _token) {
         require(vPool.isApproved(_token), "This token has not been approved");
+        _;
+    }
+
+    modifier isNotCooldown {
+        require(!vPool.isCooldown(), "Cannot perform operation during cooldown");
         _;
     }
 
@@ -52,12 +63,26 @@ contract Margin is IMargin {
         return liquidity - borrowed;
     }
 
-    function marginLevel(IERC20 _collateral, IERC20 _borrowed) public approvedOnly(_collateral) approvedOnly(_borrowed) returns (uint256) {
+    function marginLevel(address _account, IERC20 _collateral, IERC20 _borrowed) public view approvedOnly(_collateral) approvedOnly(_borrowed) returns (uint256) {
+        // Get the margin level of an account for the current period
+        uint256 periodId = vPool.currentPeriodId();
 
+        // Get the borrowed period and and borrowed asset data
+        BorrowPeriod storage borrowPeriod = borrowPeriods[periodId][_borrowed];
+        BorrowAccount storage borrowAccount = borrowPeriod.collateral[_account][_collateral];
+
+        // Calculate margin level - if there is none borrowed then return 999
+        if (borrowAccount.borrowed == 0) return uint256(999).mul(oracle.getDecimals());
+        else {
+            uint256 borrowedCurrentPrice = oracle.pairPrice(_borrowed, _collateral);
+            uint256 borrowedInitialPrice = borrowAccount.initialPrice;
+            uint256 interest = calculateInterest(_borrowed, borrowAccount.borrowed, block.timestamp - borrowAccount.borrowTime);
+            return oracle.getDecimals().mul(borrowedCurrentPrice.add(borrowAccount.collateral)).div(borrowedInitialPrice.add(interest));
+        }
     }
 
-    function liquidatable(address _account) public returns (uint256) {
-        // Check if a given account is liquidatable
+    function getMinMarginLevel() public view returns (uint256) {
+        return uint256(minMarginLevel).mul(oracle.getDecimals()).div(100);
     }
 
     function depositCollateral() external {
@@ -68,7 +93,7 @@ contract Margin is IMargin {
         // Borrow against collateral
     }
 
-    function interest(IERC20 _token, uint256 _borrowed, uint256 _time) public view returns (uint256) {
+    function calculateInterest(IERC20 _token, uint256 _borrowed, uint256 _time) public view approvedOnly(_token) returns (uint256) {
         // interest = timesAccumulated * amountBorrowed * (totalBorrowed / (totalBorrowed + liquiditiyAvailable))
 
         uint256 periodId = vPool.currentPeriodId();
