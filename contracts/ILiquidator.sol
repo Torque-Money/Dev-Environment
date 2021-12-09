f4c4d0ddcd4a96c5989c65020ab5503b9fd58483
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-09 14:15:11 +1100

    🎉 Update

--- contracts/ILiquidator.sol
+++ contracts/ILiquidator.sol
@@ -5,8 +5,9 @@ interface ILiquidator {
     /**
      *  @dev Liquidates the assets and returns the amount back to the specified pool and returns true if the callback was successful
      *  @param _initiator The caller of the function that executed this callback
-     *  @param _token The address of the token received
-     *  @param _amount The amount of the token received
+     *  @param _collateral The address of the token received
+     *  @param _amount The amount of collateral
+     *  @param _borrow The token that was borrowed and needs to be returned to
      *  @param _owing The amount required to be returned to the pool
      *  @param _data Data to be passed to the callback
      */
