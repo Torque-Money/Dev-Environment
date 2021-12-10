2168b0c7ce541e78a9e3f2c1797b29238f7456d8
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-10 21:40:19 +1100

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,9 @@ const NETWORK_URL = "https://rpc.ftm.tools/";
 const BLOCK_NUMBER = 24017532;
 
 export default {
-    solidity: "0.8.4",
+    solidity: {
+        compilers: [{ version: "0.8.4", settings: { optimizer: { enabled: true, runs: 200 } } }],
+    },
     networks: {
         hardhat: {
             forking: {
