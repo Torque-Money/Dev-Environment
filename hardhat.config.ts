81dc775a6abbc2db46c6ca6f0c47b791d722c337
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-17 10:40:27 +1100

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,17 @@
 import "@nomiclabs/hardhat-waffle";
 import dotenv from "dotenv";
+import { task } from "hardhat/config";
 dotenv.config();
+
+// Change the time of the network
+task("time", "Changes the time of the blockchain")
+    .addParam("time", "The seconds forward to set the blockchain to")
+    .setAction(async (args, hre) => {
+        // Get the time to set the blockchain forward by
+        const seconds = args.time * 60;
+        await hre.network.provider.send("evm_increaseTime", [seconds]);
+        await hre.network.provider.send("evm_mine");
+    });
 
 // Fork from mainnet to local environment
 const NETWORK_URL = "https://rpc.ftm.tools/";
