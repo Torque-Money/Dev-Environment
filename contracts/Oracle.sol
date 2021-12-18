db78387d932e1232d5927839df52501d7dc14ba9
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-19 10:28:46 +1100

    🎉 Update

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,14 +1,14 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IOracle.sol";
 import "./lib/UniswapV2Router02.sol";
 
-contract Oracle is IOracle, Context {
+contract Oracle is IOracle, Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
@@ -25,7 +25,7 @@ contract Oracle is IOracle, Context {
         return decimals;
     }
 
-    function addRouter(UniswapV2Router02 _router) external override {
+    function addRouter(UniswapV2Router02 _router) external override onlyOwner {
         require(storedRouters[_router] != true, "This router has already been added");
         routers.push(_router);
         storedRouters[_router] = true;
