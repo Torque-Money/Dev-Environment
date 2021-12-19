6232a1095623c6fe70a9ae2faf8e6af55887e36a
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-20 09:35:24 +1100

    🎉 Update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -16,6 +16,7 @@ contract Token is ERC20, ERC20Permit, ERC20Votes, Ownable {
     function yield(address to, uint256 value) external {
         // **** This function should be responsible for yielding out farm tokens to users, however it is not clear how they should be distributed ???
         // **** Perhaps use the oracle and look at the value that should be received for each deposit amount in regard to the entire allowed block ?
+        // **** Perhaps a proportion of tokens will be lent out relative to what was deposited and then this amount will be slashed with time
     }
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
