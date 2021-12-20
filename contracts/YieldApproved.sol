ac25d63e2cbdc7871658fda75aa541296da99afb
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-20 18:27:46 +1100

    🎉 Update

--- contracts/YieldApproved.sol
+++ contracts/YieldApproved.sol
@@ -3,38 +3,38 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./VPool.sol";
+import "./LPool.sol";
 import "./IYieldApproved.sol";
 
 contract YieldApproved is Ownable, IYieldApproved {
     using SafeMath for uint256;
 
-    mapping(uint256 => mapping(address => bool)) private yields; // Track all of the approved yields to ensure no double payouts
-    mapping(IERC20 => uint256) private minStake;
+    mapping(uint256 => mapping(address => bool)) private Yields; // Track all of the approved yields to ensure no double payouts
+    mapping(IERC20 => uint256) private MinStakes;
 
-    VPool public pool;
+    LPool public immutable pool;
 
-    constructor(VPool pool_) {
+    constructor(LPool pool_) {
         pool = pool_; 
     }
 
     function setMinStake(IERC20 _token, uint256 _amount) external onlyOwner {
         require(pool.isApproved(_token), "This token has not been approved");
-        minStake[_token] = _amount;
+        MinStakes[_token] = _amount;
     }
 
     function yieldApproved(address _account) external override returns (bool) {
         // Get the period id
         uint256 periodId = pool.currentPeriodId();
-        require(!yields[periodId][_account], "Yield has already been approved");
+        require(!Yields[periodId][_account], "Yield has already been approved");
         require(!pool.isPrologue(periodId), "Cannot approve yield during prologue phase");
 
         // Check if the account has an asset staked that exceeds the amount needed for a reward to be paid out
-        IERC20[] memory approved = pool.approvedList;
+        IERC20[] memory approved = pool.approvedList();
         for (uint256 i = 0; i < approved.length; i++) {
             IERC20 asset = approved[i];
-            if (pool.balanceOf(_account, asset, periodId) >= minStake[_token]) {
-                yields[periodId][_account] = true;
+            if (pool.balanceOf(_account, asset, periodId) >= MinStakes[asset]) {
+                Yields[periodId][_account] = true;
                 return true;
             }
         }
