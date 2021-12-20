ac7ff81d008da22ab52fc0b565f69247423d5aa2
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-20 21:34:29 +1100

    🎉 Update

--- test/dao.ts
+++ test/dao.ts
@@ -11,6 +11,12 @@ describe("DAO", async () => {
         const signerAddress = await signer.getAddress();
         const dao = new ethers.Contract(config.daoAddress, DAO.abi, signer);
         const timelock = new ethers.Contract(config.timelockAddress, Timelock.abi, signer);
+        const testToken = new ethers.Contract(config.approved[0].address, ERC20.abi, signer);
+
+        // Transfer tokens to the timelock
+        const tokenAmount = (1e18).toString();
+        await testToken.transfer(timelock.address, tokenAmount);
+        console.log("Transferred tokens to the timelock");
 
         // **** Eventually integrate the yield and other tokens into this for a full test AND add the correct ownerships and such
     });
