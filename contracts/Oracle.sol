610218db5e3acd1966dac7de5ea01c89fdd04b2b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-20 20:00:53 +1100

    🎉 Update

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -11,7 +11,7 @@ contract Oracle is Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    UniswapV2Router02[] public routers;
+    UniswapV2Router02[] private Routers;
     mapping(UniswapV2Router02 => bool) private StoredRouters;
 
     uint256 public immutable decimals;
@@ -20,11 +20,22 @@ contract Oracle is Ownable {
         decimals = decimals_;
     }
 
+    // ======== Routers ========
+
     /** @dev Adds a new router to be used in price calculation */
     function addRouter(UniswapV2Router02 _router) external onlyOwner {
         require(StoredRouters[_router] != true, "This router has already been added");
-        routers.push(_router);
+        Routers.push(_router);
         StoredRouters[_router] = true;
+    }
+
+    /** @dev Return the list of routers used in the oracle */
+    function routers() external view returns (UniswapV2Router02[] memory) { return Routers; }
+
+    /** @dev Returns a pseudo-random router to use */
+    function router() external view returns (UniswapV2Router02) {
+        uint256 index = uint256(keccak256(abi.encodePacked(_msgSender(), block.timestamp))).mod(Routers.length);
+        return Routers[index];
     }
 
     // ======== Verify price from multiple sources ========
@@ -71,16 +82,10 @@ contract Oracle is Ownable {
         path[1] = address(_token2);
 
         // Get the median price of token2 earned from token1 from the different exchanges
-        uint256[] memory prices = new uint256[](routers.length);
-        for (uint256 i = 0; i < routers.length; i++) {
-            prices[i] = routers[i].getAmountsOut(decimals, path)[1];
+        uint256[] memory prices = new uint256[](Routers.length);
+        for (uint256 i = 0; i < Routers.length; i++) {
+            prices[i] = Routers[i].getAmountsOut(decimals, path)[1];
         }
         return _median(prices);
-    }
-
-    /** @dev Returns a pseudo-random router to use */
-    function getRouter() external view returns (UniswapV2Router02) {
-        uint256 index = uint256(keccak256(abi.encodePacked(_msgSender(), block.timestamp))).mod(routers.length);
-        return routers[index];
     }
 }
