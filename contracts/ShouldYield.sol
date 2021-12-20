dd4e2b94f7cc270e26753c629daf9fa1cd746590
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-20 11:15:09 +1100

    🎉 Update

--- contracts/ShouldYield.sol
+++ contracts/ShouldYield.sol
@@ -1,5 +1,15 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./VPool.sol";
 
 contract ShouldYield {
     using SafeMath for uint256;
+
+    // **** Essentially here we are going to keep track of all of the previous yielders and make sure that no double yields occur, then the DAO will use this to determine where or not a yield should be made
+
+    function shouldYield(address _account) {
+
+    }
 }
