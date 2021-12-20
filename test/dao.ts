9a8ebbd5d973796972fb0ff21f15ff9989f9aff8
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-20 21:55:59 +1100

    🎉 Update

--- test/dao.ts
+++ test/dao.ts
@@ -3,6 +3,7 @@ import config from "../config.json";
 import DAO from "../artifacts/contracts/Governor.sol/DAO.json";
 import Timelock from "../artifacts/@openzeppelin/contracts/governance/TimelockController.sol/TimelockController.json";
 import ERC20 from "@openzeppelin/contracts/build/contracts/ERC20.json";
+import { expect } from "chai";
 
 describe("DAO", async () => {
     it("Should create a proposal, vote on the proposal, then execute the proposal after the given time", async () => {
@@ -14,9 +15,22 @@ describe("DAO", async () => {
         const testToken = new ethers.Contract(config.approved[0].address, ERC20.abi, signer);
 
         // Transfer tokens to the timelock
+        const initialBal = await testToken.balanceOf(signerAddress);
         const tokenAmount = (1e18).toString();
         await testToken.transfer(timelock.address, tokenAmount);
         console.log("Transferred tokens to the timelock");
+
+        // Create a proposal to transfer tokens back to owner
+        const transferCallData = testToken.interface.encodeFunctionData("transfer", [signerAddress, tokenAmount]);
+        const proposalId = await dao["propose(address[],uint256[],bytes[],string)"](
+            [testToken.address],
+            [0],
+            [transferCallData],
+            `Proposal #${Date.now()}: Give grant to owner`
+        );
+        console.log(`Proposed grant for owner with proposal id: ${proposalId.toString()}`);
+
+        // Vote on proposal
 
         // **** Eventually integrate the yield and other tokens into this for a full test AND add the correct ownerships and such
     });
