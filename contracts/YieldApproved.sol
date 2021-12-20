81846740bc5f1e2e54659acdf5c35ab5896663eb
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-20 11:39:16 +1100

    🎉 Update

--- contracts/YieldApproved.sol
+++ contracts/YieldApproved.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./VPool.sol";
 
-contract ShouldYield {
+contract YieldApproved {
     using SafeMath for uint256;
 
     // **** Essentially here we are going to keep track of all of the previous yielders and make sure that no double yields occur, then the DAO will use this to determine where or not a yield should be made
 
-    function shouldYield(address _account) {
+    function yieldApproved(address _account) {
 
     }
 }
