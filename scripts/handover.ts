aa9c71b4630f4653ee66b0c1e21d7e3169cc7e46
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-21 21:24:35 +1100

    🎉 Update

--- scripts/handover.ts
+++ scripts/handover.ts
@@ -4,7 +4,7 @@ import hre from "hardhat";
 import config from "../config.json";
 
 export default async function main() {
-    // Hand the contracts over to the DAO, approve funds to exit
+    // Hand the contracts over to the DAO, approve funds to exit, and renounce admin of the timelock
 }
 
 main()
