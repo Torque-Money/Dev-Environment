ce3aba33f11c4b3c8b917e6772edf4e76599edcb
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-21 20:33:52 +1100

    🎉 Update

--- .test/borrow.ts
+++ .test/borrow.ts
@@ -3,6 +3,8 @@ import config from "../config.json";
 import LPool from "../artifacts/contracts/LPool.sol/LPool.json";
 import Margin from "../artifacts/contracts/Margin.sol/Margin.json";
 import { expect } from "chai";
+import resetTime from "../utils/resetTime";
+import timeTravel from "../utils/timeTravel";
 
 describe("Borrow", async () => {
     it("should stake, deposit, borrow, repay, withdraw, unstake", async () => {
@@ -13,13 +15,7 @@ describe("Borrow", async () => {
         const margin = new ethers.Contract(config.marginAddress, Margin.abi, signer);
 
         // Set the time of the network to be at the start of the next hour
-        await network.provider.send("evm_mine");
-        const blockNumber = ethers.provider.blockNumber;
-        const timeStamp = (await ethers.provider.getBlock(blockNumber)).timestamp;
-        const startTime = timeStamp - (timeStamp % 3600) + 3600;
-        await network.provider.send("evm_setNextBlockTimestamp", [startTime]);
-        await network.provider.send("evm_mine");
-
+        await resetTime();
         const periodId = await pool.currentPeriodId();
 
         // Stake into the pool
@@ -37,15 +33,13 @@ describe("Borrow", async () => {
         expect(await margin.collateralOf(signerAddress, depositAsset.address, stakeAsset.address, periodId)).to.equal(depositAmount);
 
         // Borrow against the collateral
-        await network.provider.send("evm_increaseTime", [20 * 60]);
-        await network.provider.send("evm_mine");
+        await timeTravel(20);
         await margin.borrow(depositAsset.address, stakeAsset.address, stakeAmount);
 
         expect(await margin.debtOf(signerAddress, depositAsset.address, stakeAsset.address)).to.equal(stakeAmount);
 
         // Repay the debt
-        await network.provider.send("evm_increaseTime", [5 * 60]);
-        await network.provider.send("evm_mine");
+        await timeTravel(5);
         await margin.repay(signerAddress, depositAsset.address, stakeAsset.address, periodId);
 
         expect(await margin.debtOf(signerAddress, depositAsset.address, stakeAsset.address)).to.equal(0);
@@ -57,8 +51,7 @@ describe("Borrow", async () => {
         expect(await margin.collateralOf(signerAddress, depositAsset.address, stakeAsset.address, periodId)).to.equal(0);
 
         // Unstake
-        await network.provider.send("evm_increaseTime", [40 * 60]);
-        await network.provider.send("evm_mine");
+        await timeTravel(40);
         await pool.redeem(stakeAsset.address, stakeAmount, periodId);
 
         expect(await pool.liquidity(stakeAsset.address, periodId)).to.equal(0);
