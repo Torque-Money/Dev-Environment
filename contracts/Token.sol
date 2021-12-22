a51df0ec441567b735f7e0594330199a7b4b7f82
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-22 21:10:02 +1100

    🎉 Update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,47 +11,59 @@ import "./IYieldApproved.sol";
 contract Token is ERC20, ERC20Permit, ERC20Votes, Ownable {
     using SafeMath for uint256;
 
-    uint256 public numYields;
-    uint256 public yieldSlashRate;
-    uint256 public baseYieldReward;
+    // **** Perhaps they should get the same amount back that they pay in interest
 
     IYieldApproved public yieldApproved;
 
-    constructor(uint256 initialSupply_, uint256 yieldSlashRate_, uint256 baseYieldReward_, IYieldApproved yieldApproved_) ERC20("Wabbit", "WBT") ERC20Permit("WBT") {
+    uint256 public yieldSlashRate;
+    uint256 public maxYieldRate;
+
+    mapping(IERC20 => uint256) private NumYields;
+
+    constructor(uint256 initialSupply_, uint256 yieldSlashRate_, uint256 maxYieldRate_, IYieldApproved yieldApproved_) ERC20("Wabbit", "WBT") ERC20Permit("WBT") {
         yieldSlashRate = yieldSlashRate_;
-        baseYieldReward = baseYieldReward_;
         yieldApproved = yieldApproved_;
+        maxYieldRate = maxYieldRate_;
 
         _mint(owner(), initialSupply_);
     }
 
     /** @dev Set the yield slash rate */
-    function setYieldSlashRate(uint256 _yieldSlashRate) external onlyOwner { yieldSlashRate = _yieldSlashRate; }
-
-    /** @dev Set the yield reward */
-    function setBaseYieldReward(uint256 _baseYieldReward) external onlyOwner { baseYieldReward = _baseYieldReward; }
+    function setYieldSlashRate(uint256 _yieldSlashRate) external onlyOwner {
+        yieldSlashRate = _yieldSlashRate;
+    }
 
     /** @dev Set the yield approval function */
-    function setYieldApproved(IYieldApproved _yieldApproved) external onlyOwner { yieldApproved = _yieldApproved; }
+    function setYieldApproved(IYieldApproved _yieldApproved) external onlyOwner {
+        yieldApproved = _yieldApproved;
+    }
 
-    /** @dev Get the current yield reward */
-    function currentYieldReward() public view returns (uint256) {
-        uint256 slash = numYields.div(yieldSlashRate);
-        if (slash == 0) slash = 1;
+    /** @dev Set the max yield rate */
+    function setMaxYieldRate(uint256 _maxYieldRate) external onlyOwner {
+        maxYieldRate = _maxYieldRate;
+    }
 
-        return baseYieldReward.div(slash);
+    /** @dev Get the current yield slash for the given token */
+    function currentYieldSlash(IERC20 _token) public view returns (uint256) {
+        return NumYields[_token].div(yieldSlashRate);
     }
 
     /** @dev Yield new tokens as a reward to the caller if approved to do so by the yield function */
     function yield(IERC20 _token) external {
-        // Make sure the yield has been approved first
         address account = _msgSender();
         (uint256 stake, uint256 borrowed) = yieldApproved.yieldApproved(account, _token);
         require(stake > 0 || borrowed > 0, "Account has not staked or borrowed this token to earn a yield on it");
 
-        uint256 reward = currentYieldReward();
-        _mint(account, reward);
-        numYields = numYields.add(1);
+        uint256 stakeReward = 0;
+        if (stake > 0) {
+            
+        }
+        _mint(account, stakeReward);
+
+        uint256 borrowedReward = 0;
+        _mint(account, borrowedReward);
+
+        NumYields[_token] = NumYields[_token].add(1);
     }
 
     function _afterTokenTransfer(address _from, address _to, uint256 _amount)
