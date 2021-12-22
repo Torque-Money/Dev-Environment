213d9b7a0c10366643eed09a8fb3c4eaf902cf02
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-22 20:28:53 +1100

    🎉 Update

--- contracts/IYieldApproved.sol
+++ contracts/IYieldApproved.sol
@@ -5,9 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IYieldApproved {
     /**
-     *  @dev Check if an account is eligible to earn a yield and return their stake
+     *  @dev Check if an account is eligible to earn a yield and return their stake / borrow
      *  @param _account The account to check if the yield is eligible for
      *  @param _token The token to check if the account is eligible for a yield on
      */
-    function yieldApproved(address _account, IERC20 _token) external returns (uint256);
+    function yieldApproved(address _account, IERC20 _token) external returns (uint256, uint256);
 }
--- contracts/YieldApproved.sol
+++ contracts/YieldApproved.sol
@@ -5,27 +5,48 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./LPool.sol";
+import "./Margin.sol";
 import "./IYieldApproved.sol";
 
 contract YieldApproved is Ownable, IYieldApproved {
     using SafeMath for uint256;
 
     LPool public immutable pool;
+    Margin public immutable margin;
 
-    mapping(uint256 => mapping(address => mapping(IERC20 => uint256))) private Yields; // Period id => account => token => stake
+    struct Yield {
+        uint256 stake;
+        uint256 borrow;
+    }
+    mapping(uint256 => mapping(address => mapping(IERC20 => Yield))) private Yields; // Period id => account => token => stake
 
-    constructor(LPool pool_) {
+    constructor(LPool pool_, Margin margin_) {
         pool = pool_; 
+        margin = margin_;
     }
 
     /** @dev Check if an account is eligible to earn a yield on a stake */
-    function yieldApproved(address _account, IERC20 _token) external override returns (uint256) {
+    function yieldApproved(address _account, IERC20 _token) external override returns (uint256, uint256) {
         uint256 periodId = pool.currentPeriodId();
         require(!pool.isPrologue(periodId), "Cannot approve yield during prologue phase");
-        require(Yields[periodId][_account][_token] == 0, "Yield has already been approved");
+        Yield storage yield = Yields[periodId][_account][_token];
+        require(yield.stake == 0 || yield.borrow == 0, "Yield has already been approved");
 
-        uint256 stake = pool.balanceOf(_account, _token, periodId);
-        Yields[periodId][_account][_token] = stake;
-        return stake;
+        uint256 stake = 0;
+        if (yield.stake == 0) {
+            stake = pool.balanceOf(_account, _token, periodId);
+            Yields[periodId][_account][_token].stake = stake;
+        }
+
+        uint256 borrow = 0;
+        if (yield.borrow == 0) {
+            IERC20[] memory tokens = pool.approvedList();
+            for (uint256 i = 0; i < tokens.length; i++) {
+                uint256 borrowed = margin.debtOf(_account, tokens[i], _token);
+                if (borrowed > borrow) borrow = borrowed;
+            }
+        }
+
+        return (stake, borrow);
     }
 }
