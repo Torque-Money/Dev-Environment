1121cb18a74626de65a976f24d7a75de61ac9e3c
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-24 07:58:11 +1100

    🎉 Update

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -2,3 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract Timelock is TimelockController {
+    address public taxAccount;
+    uint256 public immutable taxPercentage;
+
+    uint256 public immutable taxCooldown;
+    uint256 public lastTax;
+
+    constructor(
+        uint256 minDelay_,
+        address[] memory proposers_,
+        address[] memory executors_,
+        uint256 taxPercentage_,
+        uint256 taxCooldown_
+    ) TimelockController(minDelay_, proposers_, executors_) {
+        taxAccount = _msgSender();
+        taxPercentage = taxPercentage_;
+        taxCooldown = taxCooldown_;
+    }
+}
