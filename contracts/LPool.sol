1d6a5640a881a75b72c53f7bc6644537b3f7c097
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-24 10:44:54 +1100

    🎉 Update

--- contracts/LPool.sol
+++ contracts/LPool.sol
@@ -1,18 +1,18 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./lib/LPool/LPoolAccount.sol";
+import "./lib/LPool/LPoolAdmin.sol";
+import "./lib/LPool/LPoolApproved.sol";
 import "./lib/LPool/LPoolCore.sol";
-import "./Margin.sol";
+import "./lib/LPool/LPoolLiquidity.sol";
+import "./lib/LPool/LPoolPeriod.sol";
+import "./lib/LPool/LPoolTax.sol";
 
-contract LPool is LPoolCore {
-    using SafeMath for uint256;
-    using SafeERC20 for IERC20;
-
-    constructor(uint256 periodLength_, uint256 cooldownLength_, uint256 taxPercent_) LPoolCore(periodLength_, cooldownLength_) {
-    }
-
-
+// **** Problem occurs with account and with admin - potentially add seperate interfaces into them or something ?
+contract LPool is LPoolCore, LPoolPeriod, LPoolApproved, LPoolTax, LPoolLiquidity {
+    constructor(uint256 periodLength_, uint256 cooldownLength_, uint256 taxPercent_)
+    LPoolPeriod(periodLength_, cooldownLength_)
+    LPoolTax(taxPercent_)
+    {}
 }
