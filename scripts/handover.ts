e2fd07cabbb03641022929fb37f09c2b760a41d9
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-24 17:55:57 +1100

    🎉 Update

--- scripts/handover.ts
+++ scripts/handover.ts
@@ -1,5 +1,3 @@
-// **** This will be responsible for handing the protocol over to the DAO to be operated
-
 import hre from "hardhat";
 import config from "../config.json";
 
--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,4 +1,14 @@
 import hre from "hardhat";
+import approve from "./approve";
+import deployDao from "./deployDao";
+import deployMargin from "./deployMargin";
+import deployOracle from "./deployOracle";
+import deployPool from "./deployPool";
+import deployTimelock from "./deployTimelock";
+import deployToken from "./deployToken";
+import deployYield from "./deployYield";
+import fund from "./fund";
+import handover from "./handover";
 
 async function main() {}
 
