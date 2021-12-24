884d4b5bf62cc359fe26a1bd3735e1f9b9c0d1f8
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-24 11:04:58 +1100

    🎉 Update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IYield.sol";
+import "./lib/IYield.sol";
 
 contract Token is ERC20, ERC20Permit, ERC20Votes, Ownable {
     IYield public yield;
