25df9eafa8b616c288ebd9023efbce7c32a4ce84
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-24 21:19:22 +1100

    🎉 Update

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -1,14 +1,18 @@
 import hre from "hardhat";
 import fs from "fs";
 import config from "../config.json";
+import getFutureAddress from "../utils/getFutureAddress";
 
 export default async function main() {
     // Deploy the token
+    const signer = hre.ethers.provider.getSigner();
+    const yieldApprovalAddress = await getFutureAddress(signer, 1);
+
     const tokenConfig = {
         tokenAmount: hre.ethers.BigNumber.from(10).pow(18).mul(1000),
         yieldSlashRate: 10000,
         yieldReward: hre.ethers.BigNumber.from(10).pow(18).mul(5),
-        yieldApproval: config.yieldApprovedAddress,
+        yieldApproval: yieldApprovalAddress,
     };
     const Token = await hre.ethers.getContractFactory("Token");
     const token = await Token.deploy(...Object.values(tokenConfig));
