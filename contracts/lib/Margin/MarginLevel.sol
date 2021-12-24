6a382821cc67a71cd30e986a4abe50c8584fa1b3
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-24 14:30:58 +1100

    🎉 Update

--- contracts/lib/Margin/MarginLevel.sol
+++ contracts/lib/Margin/MarginLevel.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./MarginInterest.sol";
 
 abstract contract MarginLevel is MarginInterest {
+    using SafeMath for uint256;
+
     uint256 public minMarginThreshold; // Stored as the percentage above equilibrium
 
     /** @dev Set the minimum margin level */
