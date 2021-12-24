fd47494dfb81d37fc0d8a3942ee3eab256c7c2a5
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-25 07:58:19 +1100

    🎉 Update

--- contracts/Yield.sol
+++ contracts/Yield.sol
@@ -1,15 +1,15 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./LPool.sol";
 import "./Margin.sol";
 import "./Oracle.sol";
 import "./lib/IYield.sol";
 
-contract YieldApproved is Ownable, IYield {
+contract YieldApproved is IYield, Context {
     using SafeMath for uint256;
 
     LPool public immutable pool;
