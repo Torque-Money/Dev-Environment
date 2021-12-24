d314bc6bb75c8d1f79dc4ea39b1b8240de464583
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-24 11:02:59 +1100

    🎉 Update

--- contracts/LPool.sol
+++ contracts/LPool.sol
@@ -3,14 +3,9 @@ pragma solidity ^0.8.0;
 
 import "./lib/LPool/LPoolAccount.sol";
 import "./lib/LPool/LPoolAdmin.sol";
-import "./lib/LPool/LPoolApproved.sol"; // Done
-import "./lib/LPool/LPoolCore.sol"; // Done
-import "./lib/LPool/LPoolLiquidity.sol"; // Done
-import "./lib/LPool/LPoolPeriod.sol"; // Done
 import "./lib/LPool/LPoolTax.sol"; // Done
 
-// **** Problem occurs with account and with admin - potentially add seperate interfaces into them or something ?
-contract LPool is LPoolTax, LPoolLiquidity, LPoolAccount, LPoolAdmin {
+contract LPool is LPoolTax, LPoolAccount, LPoolAdmin {
     constructor(uint256 periodLength_, uint256 cooldownLength_, uint256 taxPercent_)
     LPoolPeriod(periodLength_, cooldownLength_)
     LPoolTax(taxPercent_)
