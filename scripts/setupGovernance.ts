6a92eec9ab0bd07dae8b1909a8a76e487ac27199
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-26 08:17:48 +1100

    🎉 Update

--- scripts/setupGovernance.ts
+++ scripts/setupGovernance.ts
@@ -1,31 +1,18 @@
-import approve from "./approve";
+import hre from "hardhat";
+
 import deployGovernance from "./deployGovernance";
-import deployMargin from "./deployMargin";
-import deployOracle from "./deployOracle";
-import deployPool from "./deployPool";
 import deployTimelock from "./deployTimelock";
 import deployToken from "./deployToken";
 import deployYield from "./deployYield";
-import fund from "./fund";
-import handover from "./handover";
 
 async function main() {
-    // Protocol
-    await deployOracle();
-    await deployPool();
-    await deployMargin();
-
     // Governance
     await deployToken();
     await deployYield();
     await deployGovernance();
     await deployTimelock();
 
-    // Setup
-    await fund();
-    await approve();
-
-    await handover();
+    // Custom handover
 }
 
 main()
