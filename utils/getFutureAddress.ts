d9e3e0aa14cde0160bf65c6df5c2b1f79d829575
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-25 11:05:25 +1100

    🎉 Update

--- utils/getFutureAddress.ts
+++ utils/getFutureAddress.ts
@@ -4,6 +4,7 @@ import { getContractAddress } from "ethers/lib/utils";
 export default async function getFutureAddress(signer: ethers.providers.JsonRpcSigner, stepsAhead: number) {
     // Deploy and setup pool contract + find the margin address before deployment
     const transactionCount = await signer.getTransactionCount();
+    console.log("WE MADE IT HERE GOOD");
     return getContractAddress({
         from: await signer.getAddress(),
         nonce: transactionCount + stepsAhead,
