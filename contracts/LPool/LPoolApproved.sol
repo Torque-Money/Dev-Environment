719b3643986bae5ac812a17d1137e2ebdcf79a6d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-28 10:35:40 +1100

    🎉 Update

--- contracts/LPool/LPoolApproved.sol
+++ contracts/LPool/LPoolApproved.sol
@@ -5,36 +5,61 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./LPoolCore.sol";
+import "./LPoolToken.sol";
 
 abstract contract LPoolApproved is LPoolCore {
     using SafeMath for uint256; 
     using SafeERC20 for IERC20;
+    using SafeERC20 for LPoolToken;
 
-    mapping(IERC20 => bool) private _approved;
-    mapping(IERC20 => IERC20) private _tokenToLPToken;
-    mapping(IERC20 => IERC20) private _LPTokenToToken;
+    mapping(IERC20 => bool) private _approvedTokens;
+    mapping(LPoolToken => bool) private _approvedLPTokens;
 
-    modifier approvedOnly(IERC20 _token) {
-        require(isApproved(_token), "Only approved tokens are allowed");
+    mapping(IERC20 => LPoolToken) private _tokenToLPToken;
+    mapping(LPoolToken => IERC20) private _LPTokenToToken;
+
+    modifier approvedTokenOnly(IERC20 _token) {
+        require(isApprovedToken(_token), "Only approved tokens may be used");
         _;
     }
 
-    // Check if a token is approved
-    function isApproved(IERC20 _token) public view returns (bool) {
-        return _approved[_token];
+    modifier approvedLPTokenOnly(LPoolToken _token) {
+        require(isLPToken(_token), "Only LP tokens may be used");
+        _;
+    }
+
+    // Check if a token regular token is approved
+    function isApprovedToken(IERC20 _token) public view returns (bool) {
+        return _approvedTokens[_token];
+    }
+
+    // Check if a given token is an LP token
+    function isLPToken(LPoolToken _token) public view returns (bool) {
+        return _approvedLPTokens[_token];
     }
 
     // Approve a token for use with the pool and create a new LP token
-    function approve(IERC20 _token) external onlyRole(POOL_ADMIN) {
-        require(!isApproved(_token), "This token has already been approved");
-        _approved[_token] = true;
+    function approve(IERC20 _token, string memory _name, string memory _symbol) external onlyRole(POOL_ADMIN) {
+        require(!isApprovedToken(_token) && !isLPToken(LPoolToken(address(_token))), "This token is already in use with the pool");
+        _approvedTokens[_token] = true;
 
-        address newFarmToken = address(new PoolToken(_ltName, _ltSymbol, 0)); 
+        LPoolToken LPToken = new LPoolToken(_name, _symbol); 
+        _approvedLPTokens[LPToken] = true;
 
-        emit TokenApproved(_token);
+        _tokenToLPToken[_token] = LPToken;
+        _LPTokenToToken[LPToken] = _token;
+
+        emit TokenApproved(_token, LPToken);
     } 
 
-    // Get the equivalent token
+    // Get the LP token equivalent of a given token
+    function getLPTokenFromToken(IERC20 _token) public view approvedTokenOnly(_token) returns (LPoolToken) {
+        return _tokenToLPToken[_token];
+    }
 
-    event TokenApproved(IERC20 token);
+    function getTokenFromLPToken(LPoolToken _token) public view approvedLPTokenOnly(_token) returns (IERC20) {
+        return _LPTokenToToken[_token];
+    }
+
+    event TokenApproved(IERC20 token, LPoolToken LPToken);
 }
