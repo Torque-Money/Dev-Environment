3924a10740c3076ae19240954f76915a6d0ddacb
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-28 12:24:48 +1100

    🎉 Update

--- contracts/LPool/LPoolStake.sol
+++ contracts/LPool/LPoolStake.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./LPoolApproved.sol";
-import "./LPoolTax.sol";
 
 abstract contract LPoolStake is LPoolApproved {
 
