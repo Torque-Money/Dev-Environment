51cd72033a05bafede95a884ded4c575bf818b69
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-28 11:06:20 +1100

    🎉 Update

--- contracts/LPool/LPoolManipulation.sol
+++ contracts/LPool/LPoolManipulation.sol
@@ -6,4 +6,24 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./LPoolApproved.sol";
 
-abstract contract LPoolManipulation is LPoolApproved {}
+abstract contract LPoolManipulation is LPoolApproved {
+    // Claim an amount of a given token
+    function claim(IERC20 _token) external onlyRole(POOL_APPROVED) {
+
+    }
+
+    // Unclaim an amount of a given token
+    function unclaim(IERC20 _token) external onlyRole(POOL_APPROVED) {
+
+    }
+
+    // Deposit a given amount of collateral into the pool
+    function deposit(IERC20 _token, uint256 _amount) external onlyRole(POOL_APPROVED) {
+
+    }
+
+    // Withdraw a given amount of collateral from the pool
+    function withdraw(IERC20 _token, uint256 _amount) external onlyRole(POOL_APPROVED) {
+        
+    }
+}
