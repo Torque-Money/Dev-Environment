680880b13209a47f1609972d3e16303636ce50e9
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-28 21:26:19 +1100

    🎉 Update

--- contracts/MarketLink/MarketLinkPrice.sol
+++ contracts/MarketLink/MarketLinkPrice.sol
@@ -8,10 +8,9 @@ import "./MarketLinkRouter.sol";
 abstract contract MarketLinkPrice is MarketLinkRouter {
     using SafeMath for uint256;
 
-    // Get the price between an LP token or regular token and another token
+    // Get the price between an LP token or regular token and another LP token or regular token
     function swapPrice(IERC20 _token1, uint256 _amount, IERC20 _token2) external view returns (uint256) {
         address[] memory path = new address[](2);
-        path[1] = address(_token2);
 
         if (pool.isLPToken(_token1)) {
             _amount = pool.redeemValue(_token1, _amount);
@@ -19,6 +18,10 @@ abstract contract MarketLinkPrice is MarketLinkRouter {
         } else {
             path[0] = address(_token1);
         }
+
+        path[1] = address(_token2);
+
+        // Now we need to consider what happens if the second token is also an LP token - do we support this or not ?
 
         return router.getAmountsOut(_amount, path)[1];
     }
