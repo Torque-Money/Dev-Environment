5b40dfaecbb4292e8ad2d6c6c2546ae03575f6bc
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-28 12:24:40 +1100

    🎉 Update

---
+++ contracts/LPool/LPoolStake.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./LPoolApproved.sol";
+import "./LPoolTax.sol";
+
+abstract contract LPoolStake is LPoolApproved {
+
+}
