01aa20d110df29638987f385b2959bcc55dd1e3e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-29 08:28:13 +1100

    🎉 Update

---
+++ contracts/IsoMargin/IsoMarginInterest.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./IsoMarginCore.sol";
+
+abstract contract IsoMarginInterest is IsoMarginCore {
+    using SafeMath for uint256;
+
+    uint256 public maxInterestRate;
+
+    constructor(uint256 maxInterestRate_) {
+        maxInterestRate = maxInterestRate_;
+    }
+
+    // Set the max interest rate
+    function setMaxInterestRate(uint256 _maxInterestRate) external onlyOwner {
+        maxInterestRate = _maxInterestRate;
+    }
+}
