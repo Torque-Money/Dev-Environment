0654900b74536ff126b3600081c8d0e412d02d5b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-28 20:56:39 +1100

    🎉 Update

--- contracts/MarketLink/MarketLinkPrice.sol
+++ contracts/MarketLink/MarketLinkPrice.sol
@@ -4,16 +4,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./MarketLinkRouter.sol";
-import "../lib/Median.sol";
 
 abstract contract MarketLinkPrice is MarketLinkRouter {
     using SafeMath for uint256;
-    using Median for uint256[];
 
     // Get the price between any 
-    function pairPrice(
+    function swapPrice(
         IERC20 _token1, uint256 _amount, IERC20 _token2
     ) external view onlyApprovedOrLPToken(_token1) onlyApprovedOrLPToken(_token2) returns (uint256) {
-        // First we need to check if we are dealing with a regular approved token or an LP token for both token1 and token2
+        bool token1IsLP = pool.isLPToken(_token1);
+        bool token2IsLP = pool.isLPToken(_token2);
+
+        address[] memory pair = new address[](2);
+
+        // **** Now we want to be able to get the prices of the converted tokens - if they are LP tokens then reconvert to the price in terms of them
     }
 }
--- contracts/MarketLink/MarketLinkRouter.sol
+++ contracts/MarketLink/MarketLinkRouter.sol
@@ -1,30 +1,13 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./MarketLinkCore.sol";
 import "../lib/UniswapV2Router02.sol";
 
 abstract contract MarketLinkRouter is MarketLinkCore {
-    using SafeMath for uint256;
+    UniswapV2Router02 public router;
 
-    UniswapV2Router02[] private _routers;
-    mapping(UniswapV2Router02 => bool) private _addedRouters;
-
-    // Add a new router
-    function addRouter(UniswapV2Router02 _router) external onlyOwner {
-        require(!_addedRouters[_router], "Router has already been added");
-        _routers.push(_router);
-        _addedRouters[_router] = true;
-        emit AddRouter(_router);
+    function setRouter(UniswapV2Router02 _router) external onlyOwner {
+        router = _router;
     }
-
-    // Get a pseudo-random router
-    function router() external view returns (UniswapV2Router02) {
-        uint256 index = block.timestamp.mod(_routers.length);
-        return _routers[index];
-    }
-
-    event AddRouter(UniswapV2Router02 router);
 }
