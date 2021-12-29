3a616e655299ef8df758bd1f02c11e2ea118119b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-29 21:30:18 +1100

    🎉 Update

--- contracts/IsoMargin/IsoMarginRepay.sol
+++ contracts/IsoMargin/IsoMarginRepay.sol
@@ -6,5 +6,15 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IsoMarginMargin.sol";
 
 abstract contract IsoMarginRepay is IsoMarginMargin {
+    function collateralAfterRepay() public view returns (uint256) {
+        
+    }
 
+    function _repayGreater() internal {
+
+    }
+
+    function _repayLessOrEqual() internal {
+
+    }
 }
