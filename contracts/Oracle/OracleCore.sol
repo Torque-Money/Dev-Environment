2de76cf06049bd0e8da4ef27730444e8677b369a
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-30 10:56:00 +1100

    🎉 Update

--- contracts/Oracle/OracleCore.sol
+++ contracts/Oracle/OracleCore.sol
@@ -7,15 +7,22 @@ import "../LPool/LPool.sol";
 
 contract OracleCore is Ownable {
     AggregatorV3Interface public priceFeed;
+    AggregatorV3Interface public reservePriceFeed;
     LPool public pool;
 
-    constructor(AggregatorV3Interface priceFeed_, LPool pool_) {
+    constructor(AggregatorV3Interface priceFeed_, AggregatorV3Interface reservePriceFeed_, LPool pool_) {
         priceFeed = priceFeed_;
+        reservePriceFeed = reservePriceFeed_;
         pool = pool_;
     }
 
     // Set the price feed
     function setPriceFeed(AggregatorV3Interface priceFeed_) external onlyOwner {
+        priceFeed = priceFeed_;
+    }
+
+    // Set the price feed
+    function setReservePriceFeed(AggregatorV3Interface priceFeed_) external onlyOwner {
         priceFeed = priceFeed_;
     }
 
