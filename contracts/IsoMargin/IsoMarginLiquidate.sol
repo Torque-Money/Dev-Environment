4927ad96ba3232a09918cc73f0484ca54851a986
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-30 08:03:14 +1100

    🎉 Update

--- contracts/IsoMargin/IsoMarginLiquidate.sol
+++ contracts/IsoMargin/IsoMarginLiquidate.sol
@@ -2,9 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IsoMarginRepay.sol";
 
 abstract contract IsoMarginLiquidate is IsoMarginRepay {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
+    // Get the fee for liquidating an undercollateralized account
+    function liquidationFee(IERC20 collateral_, IERC20 borrowed_, address account_) public view returns (uint256) {
+
+    }
+
+    // Liquidate an undercollateralized account
+    function liquidate(IERC20 collateral_, IERC20 borrowed_, address account_) external {
+        
+    }
+
+    event Liquidated();
 }
