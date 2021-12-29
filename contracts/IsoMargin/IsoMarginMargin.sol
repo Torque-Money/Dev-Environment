24cfc356af5c8b14f2eed48adf6f5460d3a52451
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-29 15:20:44 +1100

    🎉 Update

--- contracts/IsoMargin/IsoMarginMargin.sol
+++ contracts/IsoMargin/IsoMarginMargin.sol
@@ -2,13 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IsoMarginAccount.sol";
 
-abstract contract IsoMarginBorrow is IsoMarginAccount {
+abstract contract IsoMarginMargin is IsoMarginAccount {
     using SafeMath for uint256;
-    using SafeERC20 for IERC20;
 
-    // **** Perform the borrowing here
+    // **** Get the margin levels and such here
 }
