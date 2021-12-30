338c2ed3b32bd9e069fe4a99157db55a93a4f8c2
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-30 17:40:42 +1100

    🎉 Update

--- contracts/MarginCore/MarginCoreCollateral.sol
+++ contracts/MarginCore/MarginCoreCollateral.sol
@@ -2,29 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./MarginCore.sol";
+import "./MarginCorePool.sol";
 
-abstract contract IsoMarginPool is IsoMarginCore {
-    mapping(IERC20 => uint256) private _totalBorrowed;
-    mapping(IERC20 => uint256) private _totalCollateral;
-
-    // Get the total borrowed of a given asset
-    function totalBorrowed(IERC20 token_) public view returns (uint256) {
-        return _totalBorrowed[token_];
-    }
-
-    // Get the total collateral of a given asset
-    function totalCollateral(IERC20 token_) public view returns (uint256) {
-        return _totalCollateral[token_];
-    }
-
-    // Set the total borrowed of a given asset
-    function setTotalBorrowed(IERC20 token_, uint256 amount_) public {
-        _totalBorrowed[token_] = amount_;
-    }
-
-    // Set the total collateral of a given asset
-    function setTotalCollateral(IERC20 token_, uint256 amount_) public {
-        _totalCollateral[token_] = amount_;
-    }
+abstract contract MarginCoreCollateral is MarginCorePool {
 }
--- contracts/MarginCore/MarginCorePool.sol
+++ contracts/MarginCore/MarginCorePool.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./MarginCore.sol";
 
-abstract contract IsoMarginPool is IsoMarginCore {
+abstract contract MarginCorePool is MarginCore {
     mapping(IERC20 => uint256) private _totalBorrowed;
     mapping(IERC20 => uint256) private _totalCollateral;
 
