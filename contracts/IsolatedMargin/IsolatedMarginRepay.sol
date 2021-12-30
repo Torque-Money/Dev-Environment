5b5c17c7f1cef5b96a0b160bb35e6458e9548f40
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-31 08:19:29 +1100

    🎉 Update

--- contracts/IsolatedMargin/IsolatedMarginRepay.sol
+++ contracts/IsolatedMargin/IsolatedMarginRepay.sol
@@ -10,6 +10,11 @@ abstract contract IsolatedMarginRepay is IsolatedMarginLevel {
 
     // Get the accounts collateral price after repay
     function collateralAfterRepay(IERC20 borrowed_, address account_) public view returns (uint256) {
+        uint256 _collateral = collateral(borrowed_, account_);
+        uint256 initialBorrowPrice = _initialBorrowPrice(borrowed_, account_);
+        uint256 currentBorrowPrice = borrowedPrice(borrowed_, account_);
+        uint256 interest = pool.interest(borrowed_, initialBorrowPrice, _initialBorrowBlock(borrowed_, account_));
 
+        return _collateral.add(currentBorrowPrice).sub(initialBorrowPrice).sub(interest);
     }
 }
