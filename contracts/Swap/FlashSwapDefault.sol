7850450cb927513ad8ab0af7848657df2db9f938
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-30 16:40:09 +1100

    🎉 Update

--- contracts/Swap/FlashSwapDefault.sol
+++ contracts/Swap/FlashSwapDefault.sol
@@ -6,14 +6,17 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./IFlashSwap.sol";
 import "../lib/UniswapV2Router02.sol";
+import "../LPool/LPool.sol";
 
 contract FlashSwapDefault is IFlashSwap, Ownable {
     using SafeERC20 for IERC20;
 
     UniswapV2Router02 public router; 
+    LPool public pool;
 
-    constructor(UniswapV2Router02 router_) {
+    constructor(UniswapV2Router02 router_, LPool pool_) {
         router = router_;
+        pool = pool_;
     }
 
     // Set the router to be used for the swap
@@ -21,17 +24,49 @@ contract FlashSwapDefault is IFlashSwap, Ownable {
         router = router_;
     }
 
+    // Set the pool
+    function setPool(LPool pool_) external onlyOwner {
+        pool = pool_;
+    }
+
     // Callback for swapping from one asset to another and return the amount of the asset swapped out for
     function flashSwap(
-        address, IERC20 tokenIn_, uint256 amountIn_, IERC20 tokenOut_, uint256 minAmountOut_, bytes calldata
+        address,
+        IERC20 tokenIn_, uint256 amountIn_, IERC20 tokenOut_,
+        uint256, bytes calldata
     ) external override returns (uint256) {
+        tokenIn_.safeTransferFrom(_msgSender(), address(this), amountIn_);
+
         address[] memory path = new address[](2);
-        path[0] = address(tokenIn_);
-        path[1] = address(tokenOut_);
 
-        tokenIn_.safeApprove(address(router), amountIn_);
-        return router.swapExactTokensForTokens(amountIn_, 0, path, _msgSender(), block.timestamp + 1 hours)[1];
+        if (pool.isLPToken(tokenIn_)) {
+            amountIn_ = pool.redeem(tokenIn_, amountIn_);
+            path[0] = address(pool.tokenFromLPToken(tokenIn_));
+        } else {
+            path[0] = address(tokenIn_);
+        }
 
-        // **** I NEED TO ADD THE OTHER STUFF IN AS WELL INCLUDING THE LP TOKENS (what do we even do for this...?)
+        bool tokenOutIsLP = pool.isLPToken(tokenOut_);
+        if (tokenOutIsLP) {
+            path[1] = address(pool.tokenFromLPToken(tokenOut_));
+        } else {
+            path[1] = address(tokenOut_);
+        }
+
+        uint256 amountOut;
+        if (path[0] == path[1]) {
+            amountOut = amountIn_;
+        } else {
+            IERC20(path[0]).safeApprove(address(router), amountIn_);
+            amountOut = router.swapExactTokensForTokens(amountIn_, 0, path, address(this), block.timestamp + 1 hours)[1];
+        }
+
+        if (tokenOutIsLP) {
+            amountOut = pool.stake(IERC20(path[1]), amountOut);
+        }
+
+        tokenOut_.safeTransfer(_msgSender(), amountOut);
+
+        return amountOut;
     }
 }
