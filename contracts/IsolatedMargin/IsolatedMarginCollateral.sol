021844e0e9c0a294af787d87163575cb71b4fb86
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-30 21:42:01 +1100

    🎉 Update

--- contracts/IsolatedMargin/IsolatedMarginCollateral.sol
+++ contracts/IsolatedMargin/IsolatedMarginCollateral.sol
@@ -9,4 +9,13 @@ import "./IsolatedMarginLevel.sol";
 abstract contract IsolatedMarginCollateral is IsolatedMarginLevel {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
+
+    // Deposit collateral into the specified account
+    function depositCollateral(IERC20 borrowed_, IERC20 collateral_, uint256 amount_) external onlyLPOrApprovedToken(collateral_) onlyApprovedToken(borrowed_) {
+        collateral_.safeTransferFrom(_msgSender(), address(this), amount_);
+        _setCollateral(borrowed_, collateral_, amount_, _msgSender());
+        emit DepositCollateral(_msgSender(), borrowed_, collateral_, amount_);
+    }
+
+    event DepositCollateral(address indexed account, IERC20 borrowed, IERC20 collateral, uint256 amount);
 }
