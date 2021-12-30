809782c62d8ed8162f2865a4e95a7cc912a62d3a
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-30 22:22:39 +1100

    🎉 Update

--- contracts/IsolatedMargin/IsolatedMarginBorrow.sol
+++ contracts/IsolatedMargin/IsolatedMarginBorrow.sol
@@ -8,12 +8,12 @@ import "./IsolatedMarginLevel.sol";
 abstract contract IsolatedMarginBorrow is IsolatedMarginLevel {
     using SafeMath for uint256;
 
-    uint256 public minCollateral; // **** Maybe this should go deeper too like make it so that you cant withdraw less than the collateral if borrowing
+    uint256 public minCollateral;
 
     // Set the minimum account collateral required to borrow against
     function setMinCollateral(uint256 minCollateral_) external onlyOwner {
         minCollateral = minCollateral_;
     }
 
-    
+
 }
