ed3d283887308c0d234d104c67584fefd4e2574d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-30 12:51:19 +1100

    🎉 Update

--- contracts/Swap/SwapTokens.sol
+++ contracts/Swap/SwapTokens.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./IFlashSwap.sol";
 import "./SwapCore.sol";
 
@@ -14,7 +15,7 @@ abstract contract SwapAssets is SwapCore, ReentrancyGuard {
     // Swap one asset for another using an external function and allow the transaction as long as the minimum amount is satisfied - returns the amount of the asset out
     function flashSwap(
         IERC20 tokenIn_, uint256 amountIn_, IERC20 tokenOut_, uint256 minAmountOut_, ISwap flashSwap_, bytes calldata data_
-    ) external returns (uint256) {
+    ) external nonReentrant returns (uint256) {
         tokenIn_.safeTransferFrom(_msgSender(), address(flashSwap_), amountIn_);
 
         uint256 amountOut = flashSwap_.flashSwap(_msgSender(), tokenIn_, amountIn_, tokenOut_, minAmountOut_, data_);
