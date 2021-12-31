79f42a4052f8a8feacd7b3319948eec8d091fdb5
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-31 20:58:56 +1100

    🎉 Update

--- contracts/IsolatedMargin/IsolatedMarginAccount.sol
+++ contracts/IsolatedMargin/IsolatedMarginAccount.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../Margin/MarginPool.sol";
+import "../Margin/Margin.sol";
 
-abstract contract IsolatedMarginAccount is MarginPool {
+abstract contract IsolatedMarginAccount is Margin {
     using SafeMath for uint256;
 
     struct Account {
