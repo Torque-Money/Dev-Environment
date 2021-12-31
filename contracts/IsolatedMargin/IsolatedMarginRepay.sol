cf2e5670ac2e7054a1af93e363632ee57e5eebfa
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-31 12:07:09 +1100

    🎉 Update

--- contracts/IsolatedMargin/IsolatedMarginRepay.sol
+++ contracts/IsolatedMargin/IsolatedMarginRepay.sol
@@ -13,6 +13,7 @@ abstract contract IsolatedMarginRepay is IsolatedMarginLevel {
     mapping(uint256 => uint256) private _swapTokenAmounts;
     uint256 private _swapTokensLength;
 
+    // **** This is not even being properly considered at all - how can I make it so that it is ?
     // Get the accounts collateral price after repay
     function collateralPriceAfterRepay(IERC20 borrowed_, address account_) public view returns (uint256) {
         uint256 _collateral = collateral(borrowed_, account_);
@@ -64,20 +65,23 @@ abstract contract IsolatedMarginRepay is IsolatedMarginLevel {
 
         uint256 borrowedTokenPrice = oracle.price(borrowed_, 10 ** oracle.decimals(borrowed_));
         uint256 minAmountOut = repayPrice.mul(10 ** oracle.decimals(borrowed_)).div(borrowedTokenPrice);
-        _flashSwap(swapTokens, swapTokenAmounts, borrowed_, minAmountOut, flashSwap_, data_);
+
+        uint256 swappedOut = _flashSwap(swapTokens, swapTokenAmounts, borrowed_, minAmountOut, flashSwap_, data_);
+        pool.deposit(borrowed_, swappedOut);        
     }
 
     // Repay when the collateral price is higher
     function _repayGreater(IERC20 borrowed_, address account_) internal {
-        // **** We are straight up just going to give the user the portion of the asset they are entitled to to avoid problems
-
         uint256 initialBorrowPrice = _initialBorrowPrice(borrowed_, account_);
         uint256 currentBorrowPrice = borrowedPrice(borrowed_, account_);
         uint256 interest = pool.interest(borrowed_, initialBorrowPrice, _initialBorrowBlock(borrowed_, account_));
         pool.unclaim(borrowed_, borrowed(borrowed_, account_));
 
-        uint256 payoutAmount = currentBorrowPrice.sub(initialBorrowPrice).sub(interest;
+        uint256 payoutAmount = currentBorrowPrice.sub(initialBorrowPrice).sub(interest);
+        uint256 borrowedTokenPrice = oracle.price(borrowed_, 10 ** oracle.decimals(borrowed_));
 
+        uint256 amountOut = payoutAmount.mul(10 ** oracle.decimals(borrowed_)).div(borrowedTokenPrice);
+        pool.withdraw(borrowed_, amountOut);
     }
 
     // Repay a users account with custom flash swap
