b7f3d0a3c17bce3e4c4caef3d5c2c7686f8c2f42
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-31 20:58:28 +1100

    🎉 Update

--- contracts/Margin/Margin.sol
+++ contracts/Margin/Margin.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.0;
 
 import "../Oracle/Oracle.sol";
 import "../FlashSwap/FlashSwap.sol";
-import "../FlashSwap/IFlashSwap.sol";
 import "../LPool/LPool.sol";
 import "./MarginApproved.sol";
 import "./MarginPool.sol";
 
-abstract contract Margin is MarginApproved, MarginPool {}
+abstract contract Margin is MarginApproved, MarginPool {
+    constructor(LPool pool_, Oracle oracle_, FlashSwap flashSwap_, uint256 swapTolerance_)
+        MarginCore(pool_, oracle_, flashSwap_, swapTolerance_)
+    {}
+}
