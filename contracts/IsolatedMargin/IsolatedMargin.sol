eaaced674233e1f5e0a4feb4e318e767e6ab4503
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-01 08:40:53 +1100

    🎉 Update

--- contracts/IsolatedMargin/IsolatedMargin.sol
+++ contracts/IsolatedMargin/IsolatedMargin.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../Oracle/Oracle.sol";
 import "../FlashSwap/FlashSwap.sol";
 import "../LPool/LPool.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IsolatedMarginBorrow.sol";
 import "./IsolatedMarginRepay.sol";
 import "./IsolatedMarginLiquidate.sol";
