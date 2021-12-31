d7085fb08b477c1fb84f457b794b3be5aa7941d0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-01 08:31:34 +1100

    🎉 Update

---
+++ contracts/Yield/YieldApproved.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../LPool/LPool.sol";
+import "../Governance/Token.sol";
+import "./YieldCore.sol";
+
+abstract contract YieldRates is YieldCore {
+    mapping(IERC20 => uint256) private _ratesNumerator; 
+    mapping(IERC20 => uint256) private _ratesDenominator; 
+
+    // Set the yield rates for tokens
+    function setRates(IERC20[] memory tokens_, uint256[] memory ratesNumerator_, uint256[] memory ratesDenominator_) external onlyOwner {
+        for (uint i = 0; i < tokens_.length; i++) {
+            _ratesNumerator[tokens_[i]] = ratesNumerator_[i];
+            _ratesDenominator[tokens_[i]] = ratesDenominator_[i];
+            emit RateChange(tokens_[i], ratesNumerator_[i], ratesDenominator_[i]);
+        }
+    }
+
+    // Get the yield rate numerator and denominator for the given token
+    function getRate(IERC20 token_) public view returns (uint256, uint256) {
+        return (_ratesNumerator[token_], _ratesDenominator[token_]);
+    }
+
+    event RateChange(IERC20 token, uint256 rateNumerator, uint256 rateDenominator);
+}
