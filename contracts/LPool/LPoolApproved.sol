62a92c1d58ab5eb54f7b59e27466b3297d39e762
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-01 19:26:26 +1100

    🎉 Update

--- contracts/LPool/LPoolApproved.sol
+++ contracts/LPool/LPoolApproved.sol
@@ -6,11 +6,11 @@ import "./LPoolCore.sol";
 import "./LPoolToken.sol";
 
 abstract contract LPoolApproved is LPoolCore {
-    mapping(IERC20 => bool) private _approvedTokens;
-    mapping(IERC20 => bool) private _approvedLPTokens;
+    mapping(IERC20 => bool) private _PATokens;
+    mapping(IERC20 => bool) private _LPTokens;
 
-    mapping(IERC20 => IERC20) private _tokenToLPToken;
-    mapping(IERC20 => IERC20) private _LPTokenToToken;
+    mapping(IERC20 => IERC20) private _PAToLP;
+    mapping(IERC20 => IERC20) private _LPToPA;
 
     modifier onlyPA(IERC20 token_) {
         require(isPA(token_), "Only pool approved tokens may be used");
@@ -24,25 +24,25 @@ abstract contract LPoolApproved is LPoolCore {
 
     // Check if a token regular token is approved
     function isPA(IERC20 token_) public view returns (bool) {
-        return _approvedTokens[token_];
+        return _PATokens[token_];
     }
 
     // Check if a given token is an LP token
     function isLP(IERC20 token_) public view returns (bool) {
-        return _approvedLPTokens[token_];
+        return _LPTokens[token_];
     }
 
     // Approve a token for use with the pool and create a new LP token
     function approve(IERC20[] memory token_, string[] memory name_, string[] memory symbol_) external onlyRole(POOL_ADMIN) {
         for (uint i = 0; i < token_.length; i++) {
             if (!isPA(token_[i]) && !isLP(token_[i])) {
-                _approvedTokens[token_[i]] = true;
+                _PATokens[token_[i]] = true;
 
                 IERC20 LPToken = IERC20(address(new LPoolToken(name_[i], symbol_[i]))); 
-                _approvedLPTokens[LPToken] = true;
+                _LPTokens[LPToken] = true;
 
-                _tokenToLPToken[token_[i]] = LPToken;
-                _LPTokenToToken[LPToken] = token_[i];
+                _PAToLP[token_[i]] = LPToken;
+                _LPToPA[LPToken] = token_[i];
 
                 emit TokenApproved(token_[i], LPToken);
             }
@@ -51,12 +51,12 @@ abstract contract LPoolApproved is LPoolCore {
 
     // Get the LP token that corresponds to the given token
     function LPFromPA(IERC20 token_) public view onlyPA(token_) returns (IERC20) {
-        return _tokenToLPToken[token_];
+        return _PAToLP[token_];
     }
 
     // Get the token that corresponds to the given LP token
     function PAFromLP(IERC20 token_) public view onlyLP(token_) returns (IERC20) {
-        return _LPTokenToToken[token_];
+        return _LPToPA[token_];
     }
 
     event TokenApproved(IERC20 token, IERC20 LPToken);
