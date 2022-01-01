a645aad521cc56384a4f4e14c0304bf286b313dd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-01 13:05:25 +1100

    🎉 Update

---
+++ contracts/Yield/Yield.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "../LPool/LPool.sol";
+import "../Governance/Token.sol";
+import "./YieldStake.sol";
+
+contract Yield is YieldStake {
+    constructor(Token token_, LPool pool_)
+        YieldCore(token_, pool_)
+    {}
+}
