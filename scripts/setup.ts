a12d68aefc7702e3e6f38614c0c4d0da382c13dd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-01 22:21:42 +1100

    🎉 Update

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -2,8 +2,25 @@ import hre from "hardhat";
 import fs from "fs";
 import config from "../config.json";
 
+import deployPool from "./deployPool";
+import deployOracle from "./deployOracle";
+import deployFlashSwap from "./deployFlashSwap";
+import deployIsolatedMargin from "./deployIsolatedMargin";
+import deployGovernance from "./deployGovernance";
+import deployYield from "./deployYield";
+
 export default async function main() {
-    // Call all of the setup functions, approve tokens, and handover to DAO
+    // Deploy contracts
+    await deployPool();
+    await deployOracle();
+    await deployFlashSwap();
+    await deployIsolatedMargin();
+    await deployGovernance();
+    await deployYield();
+
+    // Approve tokens for use with the contracts
+
+    // Remove ownership of the contracts
 }
 
 if (require.main === module)
