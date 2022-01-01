8c55e3c731fe79de251676a4a7bddf6f7732f313
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-01 14:37:42 +1100

    🎉 Update

--- contracts/Governance/TimelockTax.sol
+++ contracts/Governance/TimelockTax.sol
@@ -5,26 +5,33 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../lib/FractionMath.sol";
 
 contract TimelockTax is Context {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
     address public taxAccount;
-    uint256 public immutable taxPercentage;
+    FractionMath.Fraction private _taxPercentage;
 
     uint256 public immutable taxCooldown;
     uint256 public lastTax;
 
-    constructor(uint256 taxPercentage_, uint256 taxCooldown_) {
+    constructor(uint256 taxPercentageNumerator_, uint256 taxPercentageDenominator_, uint256 taxCooldown_) {
         taxAccount = _msgSender();
-        taxPercentage = taxPercentage_;
+        _taxPercentage.numerator = taxPercentageNumerator_;
+        _taxPercentage.denominator = taxPercentageDenominator_;
         taxCooldown = taxCooldown_;
     }
 
     modifier onlyTax {
         require(_msgSender() == taxAccount, "Only the tax account may call this");
         _;
+    }
+
+    // Get the tax percentage
+    function taxPercentage() external view returns (uint256, uint256) {
+        return (_taxPercentage.numerator, _taxPercentage.denominator);
     }
 
     /** @dev Transfer the tax account */
