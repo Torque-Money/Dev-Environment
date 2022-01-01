80af32990a3d3599c13f4ae4a4cf548be89701af
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-01 21:35:56 +1100

    🎉 Update

--- scripts/deployFlashSwap.ts
+++ scripts/deployFlashSwap.ts
@@ -3,15 +3,27 @@ import fs from "fs";
 import config from "../config.json";
 
 export default async function main() {
-    const constructorArgs = {
+    const constructorArgs1 = {
         // @ts-ignore
         pool: config.poolAddress,
     };
     const FlashSwap = await hre.ethers.getContractFactory("FlashSwap");
-    const flashSwap = await FlashSwap.deploy(...Object.values(constructorArgs));
+    const flashSwap = await FlashSwap.deploy(...Object.values(constructorArgs1));
     // @ts-ignore
     config.flashSwapAddress = flashSwap.address;
     console.log("Deployed: Flash swap");
+    fs.writeFileSync("config.json", JSON.stringify(config));
+
+    const constructorArgs2 = {
+        router: config.defaultRouterAddress,
+        // @ts-ignore
+        pool: config.poolAddress,
+    };
+    const FlashSwapDefault = await hre.ethers.getContractFactory("FlashSwapDefault");
+    const flashSwapDefault = await FlashSwapDefault.deploy(...Object.values(constructorArgs2));
+    // @ts-ignore
+    config.flashSwapDefaultAddress = flashSwapDefault.address;
+    console.log("Deployed: Flash swap default");
     fs.writeFileSync("config.json", JSON.stringify(config));
 }
 
