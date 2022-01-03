bd75729086c31fcf7e3a13e757125cc7fc88ba2b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-04 10:51:48 +1100

    🎉 Update

--- scripts/setup/setup.ts
+++ scripts/setup/setup.ts
@@ -1,82 +1,20 @@
-import hre from "hardhat";
-import config from "../../config.json";
+import setupPool from "./setupPool";
+import setupOracle from "./setupOracle";
+import setupFlashswap from "./setupFlashswap";
+import setupIsolatedMargin from "./setupIsolatedMargin";
+import setupToken from "./setupToken";
+import setupTimelock from "./setupTimelock";
+import setupYield from "./setupYield";
 
 export default async function main() {
-    // Get the deployer contracts
-    const signer = hre.ethers.provider.getSigner();
-    const signerAddress = await signer.getAddress();
-
-    const leveragePool = await hre.ethers.getContractAt("LPool", config.leveragePoolAddress);
-    const oracle = await hre.ethers.getContractAt("Oracle", config.oracleAddress);
-    const flashSwapDefault = await hre.ethers.getContractAt("FlashSwapDefault", config.flashSwapDefaultAddress);
-    const isolatedMargin = await hre.ethers.getContractAt("IsolatedMargin", config.isolatedMarginAddress);
-    const token = await hre.ethers.getContractAt("Token", config.tokenAddress);
-    const timelock = await hre.ethers.getContractAt("Timelock", config.timelockAddress);
-    const _yield = await hre.ethers.getContractAt("Yield", config.yieldAddress);
-
-    // ======== Setup the pool ========
-    const leveragePoolApprovedTokens = config.approved.filter((approved) => approved.leveragePool).map((approved) => approved.address);
-    const approvedNames = config.approved.map((approved) => "Torque Leveraged " + approved.name);
-    const approvedSymbols = config.approved.map((approved) => "tl" + approved.symbol);
-    await leveragePool.approve(leveragePoolApprovedTokens, approvedNames, approvedSymbols);
-    const maxInterestMinNumerator = Array(leveragePoolApprovedTokens.length).fill(15);
-    const maxInterestMinDenominator = Array(leveragePoolApprovedTokens.length).fill(100);
-    await leveragePool.setMaxInterestMin(leveragePoolApprovedTokens, maxInterestMinNumerator, maxInterestMinDenominator);
-    const maxInterestMaxNumerator = Array(leveragePoolApprovedTokens.length).fill(45);
-    const maxInterestMaxDenominator = Array(leveragePoolApprovedTokens.length).fill(100);
-    await leveragePool.setMaxInterestMax(leveragePoolApprovedTokens, maxInterestMaxNumerator, maxInterestMaxDenominator);
-    const maxUtilizationNumerator = Array(leveragePoolApprovedTokens.length).fill(60);
-    const maxUtilizationDenominator = Array(leveragePoolApprovedTokens.length).fill(100);
-    await leveragePool.setMaxUtilization(leveragePoolApprovedTokens, maxUtilizationNumerator, maxUtilizationDenominator);
-    console.log("Setup pool: Finished setting tokens up");
-
-    await leveragePool.grantRole(hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("POOL_APPROVED_ROLE")), isolatedMargin.address);
-    await leveragePool.grantRole(hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("POOL_ADMIN_ROLE")), timelock.address);
-    await leveragePool.setTaxAccount(timelock.address);
-    await leveragePool.renounceRole(hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("POOL_ADMIN_ROLE")), signerAddress);
-    console.log("Setup pool: Finishing assigning roles");
-
-    // ======== Setup the oracle ========
-    const oracleApproved = config.approved.map((approved) => approved.address);
-    const priceFeeds = config.approved.map((approved) => approved.priceFeed);
-    const reservePriceFeeds = config.approved.map((approved) => approved.reservePriceFeed);
-    const correctDecimals = config.approved.map((approved) => approved.decimals);
-    const oracleSupported = Array(oracleApproved.length).fill(true);
-    await oracle.setPriceFeed(oracleApproved, priceFeeds, reservePriceFeeds, correctDecimals, oracleSupported);
-    await oracle.setDefaultStablecoin(oracleApproved[0]);
-    console.log("Setup oracle: Finished adding supported tokens");
-
-    await oracle.transferOwnership(timelock.address);
-    console.log("Setup oracle: Finished transferring ownership");
-
-    // ======== Setup the flash swap default ========
-    await flashSwapDefault.transferOwnership(timelock.address);
-    console.log("Setup flash swap default: Finished transferring ownership");
-
-    // ======== Setup the isolated margin ========
-    const marginApproved = config.approved.filter((approved) => approved.margin).map((approved) => approved.address);
-    const marginSupported = Array(marginApproved.length).fill(true);
-    await isolatedMargin.approve(marginApproved, marginSupported);
-    console.log("Setup isolated margin: Finished approving collateral tokens");
-
-    await isolatedMargin.transferOwnership(timelock.address);
-    console.log("Setup isolated margin: Finished transferring ownership");
-
-    // ======== Setup the token ========
-    await token.grantRole(hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("TOKEN_ADMIN_ROLE")), timelock.address);
-    await token.grantRole(hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("TOKEN_MINTER_ROLE")), _yield.address);
-    await token.renounceRole(hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("TOKEN_ADMIN_ROLE")), signerAddress);
-    console.log("Setup token: Finishing assigning roles");
-
-    // ======== Setup the timelock ========
-    await timelock.renounceRole(hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("TIMELOCK_ADMIN_ROLE")), signerAddress);
-    console.log("Setup timelock: Finishing assigning roles");
-
-    // ======== Setup the yield ========
-    const lpTokens = await Promise.all(leveragePoolApprovedTokens.map((approved) => leveragePool.LPFromPA(approved)));
-    const rateNumerators = Array(lpTokens.length).fill(10);
-    const rateDenominators = Array(lpTokens.length).fill(100);
-    await _yield.setRates(lpTokens, rateNumerators, rateDenominators);
+    // Setup the contracts
+    await setupPool();
+    await setupOracle();
+    await setupFlashswap();
+    await setupIsolatedMargin();
+    await setupToken();
+    await setupTimelock();
+    await setupYield();
 }
 
 if (require.main === module)
