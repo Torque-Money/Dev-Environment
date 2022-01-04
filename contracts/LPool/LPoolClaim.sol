d53d91976c463010a863e3e54a20633a5bec1d56
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-04 19:42:04 +1100

    🎉 Update

--- contracts/LPool/LPoolClaim.sol
+++ contracts/LPool/LPoolClaim.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./LPoolApproved.sol";
 
-abstract contract LPoolManipulation is LPoolApproved  {
+abstract contract LPoolClaim is LPoolApproved  {
     using SafeMath for uint256;
 
     mapping(address => mapping(IERC20 => uint256)) private _claimed;
@@ -25,6 +25,16 @@ abstract contract LPoolManipulation is LPoolApproved  {
         _claimed[_msgSender()][token_] = _claimed[_msgSender()][token_].sub(amount_);
         _totalClaimed[token_] = _totalClaimed[token_].sub(amount_);
         emit Unclaim(_msgSender(), token_, amount_);
+    }
+
+    // Get the amount an account has claimed
+    function claimed(IERC20 token_, address account_) external view returns (uint256) {
+        return _claimed[account_][token_];
+    }
+
+    // Get the total amount claimed
+    function totalClaimed(IERC20 token_) public view returns (uint256) {
+        return _totalClaimed[token_];
     }
 
     function liquidity(IERC20 token_) public view virtual returns (uint256);
--- contracts/LPool/LPoolManipulation.sol
+++ contracts/LPool/LPoolManipulation.sol
@@ -6,13 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./LPoolApproved.sol";
 import "./LPoolTax.sol";
+import "./LPoolClaim.sol";
 
-abstract contract LPoolManipulation is LPoolApproved, LPoolTax {
+abstract contract LPoolManipulation is LPoolApproved, LPoolTax, LPoolClaim {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
-
-    mapping(address => mapping(IERC20 => uint256)) private _claimed;
-    mapping(IERC20 => uint256) private _totalClaimed;
 
     // Return the total value locked of a given asset
     function tvl(IERC20 token_) public view returns (uint256) {
@@ -20,25 +18,9 @@ abstract contract LPoolManipulation is LPoolApproved, LPoolTax {
     }
 
     // Get the available liquidity of the pool
-    function liquidity(IERC20 token_) public view returns (uint256) {
-        uint256 claimed = _totalClaimed[token_];
+    function liquidity(IERC20 token_) public view override returns (uint256) {
+        uint256 claimed = totalClaimed(token_);
         return tvl(token_).sub(claimed);
-    }
-
-    // Claim an amount of a given token
-    function claim(IERC20 token_, uint256 amount_) external onlyRole(POOL_APPROVED) onlyPA(token_) {
-        require(amount_ <= liquidity(token_), "Cannot claim more than total liquidity");
-        _claimed[_msgSender()][token_] = _claimed[_msgSender()][token_].add(amount_);
-        _totalClaimed[token_] = _totalClaimed[token_].add(amount_);
-        emit Claim(_msgSender(), token_, amount_);
-    }
-
-    // Unclaim an amount of a given token
-    function unclaim(IERC20 token_, uint256 amount_) external onlyRole(POOL_APPROVED) onlyPA(token_) {
-        require(amount_ <= _claimed[_msgSender()][token_], "Cannot unclaim more than your claim");
-        _claimed[_msgSender()][token_] = _claimed[_msgSender()][token_].sub(amount_);
-        _totalClaimed[token_] = _totalClaimed[token_].sub(amount_);
-        emit Unclaim(_msgSender(), token_, amount_);
     }
 
     // Deposit a given amount of collateral into the pool and transfer a portion as a tax to the tax account
@@ -58,8 +40,6 @@ abstract contract LPoolManipulation is LPoolApproved, LPoolTax {
         emit Withdraw(_msgSender(), token_, amount_);
     }
 
-    event Claim(address indexed account, IERC20 token, uint256 amount);
-    event Unclaim(address indexed account, IERC20 token, uint256 amount);
     event Deposit(address indexed account, IERC20 token, uint256 amount, uint256 tax, address taxAccount);
     event Withdraw(address indexed account, IERC20 token, uint256 amount);
 }
