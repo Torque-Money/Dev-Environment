634daab83a2cff88a7dc41a90f581b076f45c6b1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-04 21:43:20 +1100

    🎉 Update

--- contracts/FlashSwap/FlashSwapDefault.sol
+++ contracts/FlashSwap/FlashSwapDefault.sol
@@ -43,45 +43,45 @@ contract FlashSwapDefault is IFlashSwap, Ownable {
         IERC20[] memory tokenIn_, uint256[] memory amountIn_, IERC20[] memory tokenOut_,
         uint256[] memory minTokenOut_, bytes memory data_
     ) external override returns (uint256[] memory) {
-        address[] memory path = new address[](2);
-        bool tokenOutIsLP = pool.isLP(tokenOut_);
-        uint256 amountOut = 0;
+    //     address[] memory path = new address[](2);
+    //     bool tokenOutIsLP = pool.isLP(tokenOut_);
+    //     uint256 amountOut = 0;
 
-        for (uint i = 0; i < tokenIn_.length; i++) {
-            if (pool.isLP(tokenIn_[i])) {
-                amountIn_[i] = pool.redeem(tokenIn_[i], amountIn_[i]);
-                path[0] = address(pool.PAFromLP(tokenIn_[i]));
+    //     for (uint i = 0; i < tokenIn_.length; i++) {
+    //         if (pool.isLP(tokenIn_[i])) {
+    //             amountIn_[i] = pool.redeem(tokenIn_[i], amountIn_[i]);
+    //             path[0] = address(pool.PAFromLP(tokenIn_[i]));
 
-            } else {
-                path[0] = address(tokenIn_[i]);
-            }
+    //         } else {
+    //             path[0] = address(tokenIn_[i]);
+    //         }
 
-            if (tokenOutIsLP) {
-                path[1] = address(pool.PAFromLP(tokenOut_));
+    //         if (tokenOutIsLP) {
+    //             path[1] = address(pool.PAFromLP(tokenOut_));
 
-            } else {
-                path[1] = address(tokenOut_);
-            }
+    //         } else {
+    //             path[1] = address(tokenOut_);
+    //         }
 
-            if (path[0] == path[1]) {
-                amountOut = amountOut.add(amountIn_[i]);
+    //         if (path[0] == path[1]) {
+    //             amountOut = amountOut.add(amountIn_[i]);
 
-            } else {
-                IERC20(path[0]).safeApprove(address(router), amountIn_[i]);
-                amountOut = amountOut.add(router.swapExactTokensForTokens(amountIn_[i], 0, path, address(this), block.timestamp + 1 hours)[1]);
-            }
-        }
+    //         } else {
+    //             IERC20(path[0]).safeApprove(address(router), amountIn_[i]);
+    //             amountOut = amountOut.add(router.swapExactTokensForTokens(amountIn_[i], 0, path, address(this), block.timestamp + 1 hours)[1]);
+    //         }
+    //     }
 
-        if (tokenOutIsLP) amountOut = pool.stake(IERC20(path[1]), amountOut);
+    //     if (tokenOutIsLP) amountOut = pool.stake(IERC20(path[1]), amountOut);
 
-        address rewarded = _bytesToAddress(data_);
-        if (rewarded != _bytesToAddress("")) {
-            tokenOut_.safeTransfer(rewarded, amountOut.sub(minTokenOut_));
-            amountOut = minTokenOut_;
-        }
+    //     address rewarded = _bytesToAddress(data_);
+    //     if (rewarded != _bytesToAddress("")) {
+    //         tokenOut_.safeTransfer(rewarded, amountOut.sub(minTokenOut_));
+    //         amountOut = minTokenOut_;
+    //     }
 
-        tokenOut_.safeTransfer(_msgSender(), amountOut);
+    //     tokenOut_.safeTransfer(_msgSender(), amountOut);
 
-        return amountOut;
+    //     return amountOut;
     }
 }
