4c67373afb184879a831cec918e04e93d751de37
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-05 12:10:45 +1100

    🎉 Update

---
+++ contracts/Margin/MarginAccount.sol
@@ -0,0 +1,101 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../lib/Set.sol";
+import "../Margin/Margin.sol";
+
+abstract contract MarginAccount is Margin {
+    using SafeMath for uint256;
+    using TokenSet for TokenSet.Set;
+
+    struct Account {
+        TokenSet.Set collateral;
+        mapping(IERC20 => uint256) collateralAmounts;
+
+        TokenSet.Set borrowed;
+        mapping(IERC20 => uint256) borrowedAmounts;
+
+        uint256 initialBorrowPrice;
+        uint256 initialBorrowBlock;
+    }
+
+    mapping(address => Account) private _accounts;
+
+    // Set the collateral for a given asset
+    function _setCollateral(IERC20 collateral_, uint256 amount_, address account_) internal {
+        Account storage account = _accounts[account_];
+
+        if (account.collateralAmounts[collateral_] == 0 && amount_ != 0) account.collateral.insert(collateral_);
+        else if (account.collateralAmounts[collateral_] != 0 && amount_ == 0) account.collateral.remove(collateral_);
+
+        setTotalCollateral(collateral_, totalCollateral(collateral_).sub(account.collateralAmounts[collateral_]).add(amount_));
+        account.collateralAmounts[collateral_] = amount_;
+    }
+
+    // Get the collateral for a given asset for a given account
+    function collateral(IERC20 collateral_, address account_) public view returns (uint256) {
+        Account storage account = _accounts[account_];
+        return account.collateralAmounts[collateral_];
+    }
+
+    // Get the price of a token used as collateral for the asset borrowed for an account
+    function collateralPrice(IERC20 collateral_, address account_) public view returns (uint256) {
+        Account storage account = _accounts[account_];
+        return oracle.price(collateral_, account.collateralAmounts[collateral_]);
+    }
+
+    // Get the total collateral price for a given account and asset borrowed
+    function collateralPrice(address account_) public view returns (uint256) {
+        Account storage account = _accounts[account_];
+        uint256 totalPrice = 0;
+        for (uint i = 0; i < account.collateral.count(); i++)
+            totalPrice = totalPrice.add(collateralPrice(account.collateral.keyAtIndex(i), account_));
+        return totalPrice;
+    }
+
+    // Set the amount the user has borrowed
+    function _setBorrowed(IERC20 borrowed_, uint256 amount_, address account_) internal {
+        Account storage account = _accounts[borrowed_][account_];
+
+        setTotalBorrowed(borrowed_, totalBorrowed(borrowed_).sub(account.borrowed).add(amount_));
+        account.borrowed = amount_;
+    }
+
+    // Get the borrowed for a given account
+    function borrowed(IERC20 borrowed_, address account_) public view returns (uint256) {
+        Account storage account = _accounts[borrowed_][account_];
+        return account.borrowed;
+    }
+
+    // Get the total price of the assets borrowed
+    function borrowedPrice(IERC20 borrowed_, address account_) public view returns (uint256) {
+        Account storage account = _accounts[borrowed_][account_];
+        return oracle.price(borrowed_, account.borrowed);
+    }
+
+    // Get the initial borrow price for an account
+    function initialBorrowPrice(IERC20 borrowed_, address account_) public view returns (uint256) {
+        Account storage account = _accounts[borrowed_][account_];
+        return account.initialBorrowPrice;
+    }
+
+    // Set the initial borrow price for an account
+    function _setInitialBorrowPrice(IERC20 borrowed_, uint256 price_, address account_) internal {
+        Account storage account = _accounts[borrowed_][account_];
+        account.initialBorrowPrice = price_;
+    }
+
+    // Get the initial borrow block for an ccount
+    function initialBorrowBlock(IERC20 borrowed_, address account_) public view returns (uint256) {
+        Account storage account = _accounts[borrowed_][account_];
+        return account.initialBorrowBlock;
+    }
+
+    // Set the initial borrow price for an account
+    function _setInitialBorrowBlock(IERC20 borrowed_, uint256 block_, address account_) internal {
+        Account storage account = _accounts[borrowed_][account_];
+        account.initialBorrowBlock = block_;
+    }
+}
