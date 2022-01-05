80791b72b57bfff39041ac08bfa1a5ebec594ebe
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-05 15:00:46 +1100

    🎉 Update

--- contracts/Margin/MarginCollateral.sol
+++ contracts/Margin/MarginCollateral.sol
@@ -12,6 +12,17 @@ abstract contract MarginCollateral is MarginBorrow, MarginLevel {
     using SafeERC20 for IERC20;
 
     // Deposit collateral into the account
+    function addCollateral(IERC20 collateral_, uint256 amount_) external {
+        collateral_.safeTransferFrom(_msgSender(), address(this), amount_);
+        _setCollateral(collateral_, collateral(collateral_, _msgSender()).add(amount_), _msgSender());
+        emit AddCollateral(_msgSender(), collateral, amount_);
+    }
 
     // Withdraw collateral from the account
+    function removeCollateral(IERC20 collateral_, uint256 amount_) external {
+
+    }
+
+    event AddCollateral(address indexed account, IERC20 collateral, uint256 amount);
+    event RemoveCollateral(address indexed account, IERC20 collateral, uint256 amount);
 }
