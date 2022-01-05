898865ae1d86de766f4054042505ba1702a98de0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-05 14:14:47 +1100

    🎉 Update

--- contracts/Oracle/OracleTokens.sol
+++ contracts/Oracle/OracleTokens.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./OracleCore.sol";
 
-abstract contract OracleTokens is OracleCore {
+abstract contract OracleTokens is Ownable {
     struct Token {
         AggregatorV3Interface priceFeed;
         AggregatorV3Interface reservePriceFeed;
