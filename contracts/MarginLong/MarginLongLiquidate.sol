f639d6c09671fbc2b00fcd71562f617b3eaa1739
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-07 09:36:34 +1100

    🎉 Update

--- contracts/MarginLong/MarginLongLiquidate.sol
+++ contracts/MarginLong/MarginLongLiquidate.sol
@@ -4,31 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../lib/FractionMath.sol";
 import "../FlashSwap/IFlashSwap.sol";
 import "./MarginLongRepay.sol";
 
 abstract contract MarginLongLiquidate is MarginLongRepay {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
-
-    FractionMath.Fraction private _liquidationFeePercent;
-
-    constructor(uint256 liquidationFeePercentNumerator_, uint256 liquidationFeePercentDenominator_) {
-        _liquidationFeePercent.numerator = liquidationFeePercentNumerator_;
-        _liquidationFeePercent.denominator = liquidationFeePercentDenominator_;
-    }
-
-    // Get the liquidation fee percent
-    function liquidationFeePercent() public view returns (uint256, uint256) {
-        return (_liquidationFeePercent.numerator, _liquidationFeePercent.denominator);
-    }
-
-    // Set the liquidation fee percent
-    function setLiquidationFeePercent(uint256 liquidationFeePercentNumerator_, uint256 liquidationFeePercentDenominator_) external onlyOwner {
-        _liquidationFeePercent.numerator = liquidationFeePercentNumerator_;
-        _liquidationFeePercent.denominator = liquidationFeePercentDenominator_;
-    }
 
     // Liquidate all accounts that have not been repaid by the repay greater
     function _liquidate(
