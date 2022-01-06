b6a22336f9fb352d5e00b0e02e48b2cc4141cc55
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-07 10:52:31 +1100

    🎉 Update

--- contracts/Yield/YieldCore.sol
+++ contracts/Yield/YieldCore.sol
@@ -3,16 +3,24 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../Governance/Token.sol";
+import "../Oracle/Oracle.sol";
 
 contract YieldCore is Ownable {
     Token public token;
+    Oracle public oracle;
 
-    constructor(Token token_) {
+    constructor(Token token_, Oracle oracle_) {
         token = token_;
+        oracle = oracle_;
     }
 
     // Set the yield distribution token
     function setToken(Token token_) external onlyOwner {
         token = token_;
+    }
+
+    // Set the oracle
+    function setOracle(Oracle oracle_) external onlyOwner {
+        oracle = oracle_;
     }
 }
