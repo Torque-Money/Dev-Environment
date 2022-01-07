4ead9152fe0dd5f5a447f985d39b6cc0b30ff056
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-08 09:09:35 +1100

    🎉 Update

--- scripts/util/updateFIles.ts
+++ scripts/util/updateFIles.ts
@@ -1,10 +1,24 @@
 import fs from "fs";
 
+function move() {}
+
 export default async function main() {
     const compiledContracts = "artifacts/contracts";
     const config = "config.json";
-    const abis = [];
+    const abis = [
+        "FlashSwap/FlashSwap.sol/FlashSwap.json",
+        "FlashSwap/FlashSwapDefault.sol/FlashSwapDefault.json",
+        "Governance/Governor.sol/Governance.json",
+        "Governance/Timelock.sol/Timelock.json",
+        "Governance/Token.sol/Token.json",
+        "LPool/LPool.sol/LPool.json",
+        "MarginLong/MarginLong.sol/MarginLong.json",
+        "Oracle/Oracle.sol/Oracle.json",
+        "Reserve/Reserve.sol/Reserve.json",
+    ];
     const outDir = "../Torque-Frontend/src/config";
+
+    // Here we are going to loop through each ABI, take its file name from the path and move it into the outDir
 
     console.log("Util: Copied files");
 }
