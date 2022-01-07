ffa12c156df5efebe90de323240cb89a3333eed1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-07 11:56:18 +1100

    🎉 Update

---
+++ contracts/Reserve/ReserveRedeem.sol
@@ -0,0 +1,4 @@
+abstract contract ReserveRedeem {
+    // **** Here we will somehow look at the total price of the reserve and will then allocate tokens in proportion to what the treasury is worth (HOW DO WE TRACK TOKENS - IS THIS VALID WAY OF DOING SO ?)
+    // **** Tokens also get burned on redemption
+}
