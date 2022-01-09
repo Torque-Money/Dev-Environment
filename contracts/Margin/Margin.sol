c7c6a59ee31c8c6503fae2c1e6499252ffa27da4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-09 21:15:32 +1100

    🎉 Update

--- contracts/Margin/Margin.sol
+++ contracts/Margin/Margin.sol
@@ -2,5 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "./MarginCollateral.sol";
+import "./MarginBorrowers.sol";
 
-abstract contract Margin is MarginCollateral {}
+abstract contract Margin is MarginCollateral, MarginBorrowers {}
