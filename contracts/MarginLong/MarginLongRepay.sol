502c1ec43cfb9d7c19bd8358b8630a864562966a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-11 13:35:05 +1100

    🎉 Update

--- contracts/MarginLong/MarginLongRepay.sol
+++ contracts/MarginLong/MarginLongRepay.sol
@@ -30,31 +30,46 @@ abstract contract MarginLongRepayOLD is Margin {
         return (repayTax.numerator, repayTax.denominator);
     }
 
-    // Get the repay amount and whether it is a repay payout or loss
-    function _repayPrice(IERC20 token_, address account_) internal view returns (uint256, bool) {
+    // Check whether or not a given borrowed asset is at a loss or profit
+    function _repayIsPayout(IERC20 token_, address account_) internal view returns (bool) {
+        uint256 currentPrice = _borrowedPrice(token_, account_);
+        uint256 initialPrice = initialBorrowPrice(token_, account_);
+        uint256 interest = pool.interest(token_, initialPrice, initialBorrowBlock(token_, account_));
+        return (currentPrice > initialPrice.add(interest));
+    }
+
+    // Get the repay amount when there is a payout
+    function _repayPayoutAmount(IERC20 token_, address account_) internal view returns (uint256) {
         uint256 currentPrice = _borrowedPrice(token_, account_);
         uint256 initialPrice = initialBorrowPrice(token_, account_);
         uint256 interest = pool.interest(token_, initialPrice, initialBorrowBlock(token_, account_));
 
-        if (currentPrice > initialPrice.add(interest)) return (currentPrice.sub(initialPrice).sub(interest), true);
-        else return (initialPrice.add(interest).sub(currentPrice), false);
+        return oracle.amount(token_, currentPrice.sub(initialPrice).sub(interest));
+    }
+
+    // Get the repay price when there is a loss
+    function _repayLossesPrice(IERC20 token_, address account_) internal view returns (uint256) {
+        uint256 currentPrice = _borrowedPrice(token_, account_);
+        uint256 initialPrice = initialBorrowPrice(token_, account_);
+        uint256 interest = pool.interest(token_, initialPrice, initialBorrowBlock(token_, account_));
+
+        return oracle.amount(token_, initialPrice.add(interest).sub(currentPrice));
     }
 
     // Get the borrowed assets that were above the price and how much they were repaid
-    function _repayPayoutPrices(address account_) internal view returns (uint256[] memory) {
+    function _repayPayoutAmounts(address account_) internal view returns (uint256[] memory) {
         IERC20[] memory borrowTokens = _borrowTokens(account_);
         uint256[] memory borrowedRepays = new uint256[](borrowTokens.length);
 
         for (uint256 i = 0; i < borrowedTokens.length; i++) {
-            (uint256 repayPrice, bool payout) = _repayPrice(borrowedTokens[i], account_);
-            if (payout) borrowedRepays[i] = repayPrice;
+            if (_repayIsPayout(borrowedTokens[i], account_)) borrowedRepays[i] = _repayPayoutAmount(borrowedTokens[i], account_);
         }
 
         return borrowedRepays;
     }
 
     // Calculate the repay amounts to be paid out from the collateral
-    function _repayLossesPrices(uint256[] memory payoutPrices_, address account_) internal view returns (uint256[] memory) {
+    function _repayLossesPrices(uint256[] memory payoutAmounts_, address account_) internal view returns (uint256[] memory) {
         IERC20[] memory borrowTokens = _borrowTokens(account_);
         uint256[] memory borrowDebt = new uint256[](borrowTokens.length);
 
@@ -67,20 +82,24 @@ abstract contract MarginLongRepayOLD is Margin {
 
         for (uint256 i = 0; i < borrowTokens.length; i++) {
             if (payoutAmounts_[i] <= 0) {
-                (uint256 debt, ) = _repayPrice(borrowTokens[i], account_);
+                (uint256 debt, ) = _repayLossesPrice(borrowTokens[i], account_);
 
                 while (debt > 0) {
                     if (collateralIndex < collateralTokens.length) {
-                        uint256 _collateralPrice = oracle.price(collateralTokens[i], collateralAmounts[i]);
+                        uint256 _collateralPrice = oracle.price(collateralTokens[collateralIndex], collateralAmounts[collateralIndex]);
 
-                        uint256 _collateralAmount = collateralAmounts[i];
+                        uint256 _collateralAmount = collateralAmounts[collateralIndex];
                         if (_collateralPrice > debt) {
-                            _collateralAmount = oracle.minAmount(collateralTokens[i], debt);
-                            collateralAmounts[i] = collateralAmounts[i].sub(_collateralAmount);
-                            collateralDebt[i] = collateralDebt[i].add(_collateralAmount);
+                            _collateralAmount = oracle.minAmount(collateralTokens[collateralIndex], debt);
+
+                            collateralAmounts[collateralIndex] = collateralAmounts[collateralIndex].sub(_collateralAmount);
+                            collateralDebt[collateralIndex] = collateralDebt[collateralIndex].add(_collateralAmount);
+
                             debt = 0;
                         } else {
-                            collateralDebt[i] = collateralDebt[i].add(_collateralAmount);
+                            collateralAmounts[collateralIndex] = 0;
+                            collateralDebt[collateralIndex] = collateralDebt[collateralIndex].add(_collateralAmount);
+
                             debt = debt.sub(_collateralPrice);
                             collateralIndex = collateralIndex.add(1);
                         }
@@ -89,7 +108,23 @@ abstract contract MarginLongRepayOLD is Margin {
 
                         if (payoutAmounts_[borrowIndex] <= 0) borrowIndex = borrowIndex.add(1);
                         else {
-                            uint256 _collateralPrice = oracle.price(borrowedTokens[i], payoutAmounts_[i]); // **** We want the amounts but we have been given the prices - it should be amounts shouldnt it
+                            uint256 _collateralPrice = oracle.price(borrowedTokens[borrowIndex], payoutAmounts_[borrowIndex]);
+
+                            uint256 _collateralAmount = payoutAmounts_[borrowIndex];
+                            if (_collateralPrice > debt) {
+                                _collateralAmount = oracle.minAmount(borrowedTokens[borrowIndex], debt);
+
+                                payoutAmounts_[borrowIndex] = payoutAmounts_[borrowIndex].sub(_collateralAmount);
+                                borrowDebt[borrowIndex] = borrowDebt[borrowIndex].add(_collateralAmount);
+
+                                debt = 0;
+                            } else {
+                                payoutAmounts_[borrowIndex] = 0;
+                                borrowDebt[borrowIndex] = borrowDebt[borrowIndex].add(_collateralAmount);
+
+                                debt = debt.sub(_collateralPrice);
+                                borrowIndex = borrowIndex.add(1);
+                            }
                         }
                     }
                 }
