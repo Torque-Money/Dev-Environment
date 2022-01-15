af0b5686649c40d4a776f8f7278d4d0bc7c51353
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-16 08:23:42 +1100

    🎉 Update

--- contracts/LPool/LPoolDeposit.sol
+++ contracts/LPool/LPoolDeposit.sol
@@ -23,23 +23,34 @@ abstract contract LPoolDeposit is LPoolApproved, LPoolTax {
         converter = converter_;
     }
 
-    // Get a pseudo random token to convert the deposited asset to for a uniform distribution of fees
-    function _pseudoRandomPT() internal view returns (IERC20) {
-        // **** Move this to a weighted distribution using the utilization rates
-        // **** Perhaps instead of doing a random distribution so that the interest rates are artificial and the distribution is fair, have a manual allocation mode ?
-        // **** Try and intuitively understand this ???
+    // Get a pseudo random token to convert the deposited asset to for a weighted distribution based on utilization rates of fees
+    function _pseudoRandomWeightedPT() internal view returns (IERC20) {
+        uint256 multiplier = 10000;
 
-        // **** We are going to use a cache which will be notified whenever the value changes via the TVL or something else changing
+        IERC20[] memory poolTokens = _poolTokens();
+        uint256[] memory weights = new uint256[](poolTokens.length);
+        uint256 totalWeightSize;
+        for (uint256 i = 0; i < poolTokens.length; i++) {
+            (uint256 utilNumerator, uint256 utilDenominator) = utilizationRate(poolTokens[i]);
+            uint256 weightSize = multiplier.mul(utilNumerator).div(utilDenominator);
 
-        IERC20[] memory tokens = _poolTokens();
-        uint256 index = uint256(keccak256(abi.encodePacked(block.difficulty, block.number, _msgSender()))).mod(tokens.length);
-        return tokens[index];
+            weights[i] = weightSize;
+            totalWeightSize = totalWeightSize.add(weightSize);
+        }
+
+        uint256 randomSample = uint256(keccak256(abi.encodePacked(block.difficulty, block.number, _msgSender()))).mod(totalWeightSize);
+
+        uint256 cumulative = 0;
+        for (uint256 i = 0; i < poolTokens.length; i++) {
+            cumulative = cumulative.add(weights[i]);
+            if (randomSample <= cumulative) return poolTokens[i];
+        }
     }
 
     // Deposit a given amount of collateral into the pool and transfer a portion as a tax to the tax account
     function deposit(IERC20 token_, uint256 amount_) external onlyRole(POOL_APPROVED) {
         token_.safeTransferFrom(_msgSender(), address(this), amount_);
-        IERC20 convertedToken = _pseudoRandomPT();
+        IERC20 convertedToken = _pseudoRandomWeightedPT();
         uint256 convertedAmount = converter.swapMaxOut(token_, amount_, convertedToken);
 
         address[] memory _taxAccounts = _taxAccounts();
@@ -61,6 +72,8 @@ abstract contract LPoolDeposit is LPoolApproved, LPoolTax {
     }
 
     function liquidity(IERC20 token_) public view virtual returns (uint256);
+
+    function utilizationRate(IERC20 token_) public view virtual returns (uint256, uint256);
 
     event Deposit(address indexed account, IERC20 tokenIn, uint256 amountIn, IERC20 convertedToken, uint256 convertedAmount);
     event Withdraw(address indexed account, IERC20 token, uint256 amount);
