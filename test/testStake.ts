eb28652e7b04b84c62dd98d6db469de60a24db13
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-18 20:55:51 +1100

    🎉 Update

--- test/testStake.ts
+++ test/testStake.ts
@@ -3,7 +3,7 @@ import deployFast from "../scripts/fast";
 import config from "../config.json";
 
 describe("Stake", function () {
-    beforeEach(async () => await deployFast());
+    beforeEach(async () => await deployFast(false));
 
     it("should stake tokens for an equal amount of LP tokens", async function () {
         // Do something with the accounts
