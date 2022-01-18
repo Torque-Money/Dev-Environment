94fe7f215af7889dbc2c42060b84b98fd3159d27
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-18 20:11:45 +1100

    🎉 Update

---
+++ test/testStake.ts
@@ -0,0 +1,11 @@
+import {ethers} from "hardhat";
+import deployFast from "../scripts/fast";
+import config from "../config.json";
+
+describe("Stake", function () {
+    beforeEach(async () => await deployFast());
+
+    it("should stake tokens for an equal amount of LP tokens", async function () {
+        // Do something with the accounts
+    });
+});
