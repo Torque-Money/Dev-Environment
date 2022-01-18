0af0cdcea7cf50e33133ec5cdc98677c26e8a257
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-18 22:02:20 +1100

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,9 @@ import fast from "./scripts/fast";
 import dotenv from "dotenv";
 dotenv.config();
 
+// **** The fast isnt going to work for everything - I dont get to spend others money on live or testnet
+// **** In addition we should remove the need for whales on testnet and the ability to choose the config on testnet
+
 task("deploy-main", "Deploy contracts onto mainnet", async (args, hre) => await fast(false, hre));
 
 task("deploy-test", "Deploy contracts onto testnet", async (args, hre) => await fast(true, hre));
@@ -13,7 +16,7 @@ task("deploy-test", "Deploy contracts onto testnet", async (args, hre) => await 
 task("deploy-fork", "Deploy contracts onto forked network", async (args, hre) => await fast(false, hre));
 
 const NETWORK_URL = "https://rpc.ftm.tools/";
-const NETWORK_URL_TEST = "";
+const NETWORK_URL_TEST = process.env.NETWORK_URL; // Rinkeby
 
 export default {
     solidity: {
