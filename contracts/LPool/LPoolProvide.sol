867cabf9ec168db41786ca66cf307d4e530019b0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-20 10:40:41 +1100

    🎉 Update

--- contracts/LPool/LPoolProvide.sol
+++ contracts/LPool/LPoolProvide.sol
@@ -7,12 +7,12 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./LPoolLiquidity.sol";
 import "./LPoolToken.sol";
 
-abstract contract LPoolStake is LPoolLiquidity {
+abstract contract LPoolProvide is LPoolLiquidity {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    // Return the amount of tokens received for staking a given amount of tokens
-    function stakeValue(IERC20 token_, uint256 amount_) public view returns (uint256) {
+    // Return the amount of tokens received for adding a given amount of tokens as liquidity
+    function provideLiquidityValue(IERC20 token_, uint256 amount_) public view returns (uint256) {
         LPoolToken LPToken = LPoolToken(address(LPFromPT(token_)));
         uint256 totalSupply = LPToken.totalSupply();
         uint256 totalValue = tvl(token_);
@@ -20,23 +20,23 @@ abstract contract LPoolStake is LPoolLiquidity {
         return amount_.mul(totalSupply).div(totalValue);
     }
 
-    // Stake tokens and receive LP tokens that represent the users share in the pool
-    function stake(IERC20 token_, uint256 amount_) external onlyApprovedPT(token_) returns (uint256) {
+    // Add tokens to the liquidity pool and receive LP tokens that represent the users share in the pool
+    function provideLiquidity(IERC20 token_, uint256 amount_) external onlyApprovedPT(token_) returns (uint256) {
         LPoolToken LPToken = LPoolToken(address(LPFromPT(token_)));
 
-        uint256 value = stakeValue(token_, amount_);
-        require(value > 0, "LPoolStake: Not enough tokens staked");
+        uint256 value = provideLiquidityValue(token_, amount_);
+        require(value > 0, "LPoolProvide: Not enough tokens provided");
 
         token_.safeTransferFrom(_msgSender(), address(this), amount_);
         LPToken.mint(_msgSender(), value);
 
-        emit Stake(_msgSender(), token_, amount_, value);
+        emit ProvideLiquidity(_msgSender(), token_, amount_, value);
 
         return value;
     }
 
     // Get the value for redeeming LP tokens for the underlying asset
-    function redeemValue(IERC20 token_, uint256 amount_) public view returns (uint256) {
+    function redeemLiquidityValue(IERC20 token_, uint256 amount_) public view returns (uint256) {
         LPoolToken LPToken = LPoolToken(address(token_));
         IERC20 approvedToken = PTFromLP(token_);
         uint256 totalSupply = LPToken.totalSupply();
@@ -45,21 +45,21 @@ abstract contract LPoolStake is LPoolLiquidity {
     }
 
     // Redeem LP tokens for the underlying asset
-    function redeem(IERC20 token_, uint256 amount_) external onlyLP(token_) returns (uint256) {
+    function redeemLiquidity(IERC20 token_, uint256 amount_) external onlyLP(token_) returns (uint256) {
         LPoolToken LPToken = LPoolToken(address(token_));
         IERC20 approvedToken = PTFromLP(token_);
 
-        uint256 value = redeemValue(LPToken, amount_);
-        require(value <= liquidity(approvedToken), "LPoolStake: Not enough liquidity to redeem at this time");
+        uint256 value = redeemLiquidityValue(LPToken, amount_);
+        require(value <= liquidity(approvedToken), "LPoolProvide: Not enough liquidity to redeem at this time");
 
         LPToken.burn(_msgSender(), amount_);
         approvedToken.safeTransfer(_msgSender(), value);
 
-        emit Redeem(_msgSender(), token_, amount_, value);
+        emit RedeemLiquidity(_msgSender(), token_, amount_, value);
 
         return value;
     }
 
-    event Stake(address indexed account, IERC20 token, uint256 amount, uint256 value);
-    event Redeem(address indexed account, IERC20 token, uint256 amount, uint256 value);
+    event ProvideLiquidity(address indexed account, IERC20 token, uint256 amount, uint256 value);
+    event RedeemLiquidity(address indexed account, IERC20 token, uint256 amount, uint256 value);
 }
