e4b9202a022d4d1571a31de4bc1f6c95943baa28
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-20 08:36:40 +1100

    🎉 Update

--- scripts/util/utilFund.ts
+++ scripts/util/utilFund.ts
@@ -2,13 +2,15 @@ import ERC20 from "@openzeppelin/contracts/build/contracts/ERC20.json";
 import {chooseConfig, ConfigType} from "./chooseConfig";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 
+import UniswapV2Router02 from "../../artifacts/contracts/lib/UniswapV2Router02.sol/UniswapV2Router02.json";
+
 export default async function main(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
     const config = chooseConfig(configType);
 
     const signer = hre.ethers.provider.getSigner();
     const signerAddress = await signer.getAddress();
 
-    // **** This will be changed to router swaps later
+    const router = new hre.ethers.Contract(config.routerAddress, UniswapV2Router02.abi, signer);
 
     for (const approved of config.approved) {
         await hre.network.provider.request({
