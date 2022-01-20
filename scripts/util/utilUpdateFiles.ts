c190dcefa35d14c8d989ff3ab8e0c1f1233fc607
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-21 08:10:41 +1100

    🎉 Update

--- scripts/util/utilUpdateFiles.ts
+++ scripts/util/utilUpdateFiles.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
+const fsExtra = require("fs-extra");
 
 export default async function main() {
     const compiledContracts = process.cwd() + "/artifacts/contracts";
     const configs = [process.cwd() + "/config.main.json", process.cwd() + "/config.test.json", process.cwd() + "/config.fork.json"];
     const abis = ["LPool/LPool.sol/LPool.json", "MarginLong/MarginLong.sol/MarginLong.json", "Oracle/Oracle.sol/Oracle.json"];
     const outDir = process.cwd() + "/../Torque-Frontend/src/config";
+    const typeChain = process.cwd() + "/typechain-types";
 
     for (const abi of abis) {
         const oldPath = compiledContracts + "/" + abi;
