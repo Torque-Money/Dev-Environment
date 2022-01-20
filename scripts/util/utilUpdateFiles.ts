fd98a45215147bd27bcf817331190f6826c18283
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-21 08:14:46 +1100

    🎉 Update

--- scripts/util/utilUpdateFiles.ts
+++ scripts/util/utilUpdateFiles.ts
@@ -1,33 +1,37 @@
-import fs from "fs";
-import fsExtra from "fs-extra";
+import fs from "fs-extra";
 
 export default async function main() {
     const compiledContracts = process.cwd() + "/artifacts/contracts";
     const configs = [process.cwd() + "/config.main.json", process.cwd() + "/config.test.json", process.cwd() + "/config.fork.json"];
     const abis = ["LPool/LPool.sol/LPool.json", "MarginLong/MarginLong.sol/MarginLong.json", "Oracle/Oracle.sol/Oracle.json"];
-    const outDir = process.cwd() + "/../Torque-Frontend/src/config";
     const typeChain = process.cwd() + "/typechain-types";
+
+    const outRoot = process.cwd() + "/../Torque-Frontend/src";
+    const outConfigDir = outRoot + "/config";
 
     for (const abi of abis) {
         const oldPath = compiledContracts + "/" + abi;
 
         const fileName = abi.split("/").at(-1);
-        const newPath = outDir + "/" + fileName;
+        const newPath = outConfigDir + "/" + fileName;
 
         fs.copyFile(oldPath, newPath, (err) => {
             if (err) throw err;
         });
 
-        console.log(`Moved '${oldPath}' to '${newPath}'`);
+        console.log(`Copied '${oldPath}' to '${newPath}'`);
     }
 
     for (const config of configs) {
-        const newConfigPath = outDir + "/" + config.split("/").at(-1);
+        const newConfigPath = outConfigDir + "/" + config.split("/").at(-1);
         fs.copyFile(config, newConfigPath, (err) => {
             if (err) throw err;
         });
-        console.log(`Moved '${config}' to '${newConfigPath}'`);
+        console.log(`Copied '${config}' to '${newConfigPath}'`);
     }
+
+    fs.copySync(typeChain, outRoot);
+    console.log(`Copied ${typeChain} to ${outRoot}`);
 
     console.log("Util: Copied files");
 }
