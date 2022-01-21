d94fd31f2df8c669876f3e9db4c6edd30a6e05b8
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-21 13:42:15 +1100

    🎉 Update

--- scripts/util/utilVerify.ts
+++ scripts/util/utilVerify.ts
@@ -1,8 +1,14 @@
 import fs from "fs";
+import {HardhatRuntimeEnvironment} from "hardhat/types";
 
 import tempConstructor from "../../temp.constructor.json";
+import {chooseConfig, ConfigType} from "./utilConfig";
 
 export function saveTempConstructor(key: string, constructorConfig: any) {
     (tempConstructor as any)[key] = constructorConfig;
     fs.writeFileSync("temp.constructor.json", JSON.stringify(tempConstructor));
+}
+
+export function verifyAll(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
+    const config = chooseConfig(configType);
 }
