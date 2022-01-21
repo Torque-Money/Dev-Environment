1f85aff5a6d2a609a9d768d051e7413ab91eac19
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-21 21:07:00 +1100

    🎉 Update

--- scripts/util/utilVerify.ts
+++ scripts/util/utilVerify.ts
@@ -4,11 +4,10 @@ import {HardhatRuntimeEnvironment} from "hardhat/types";
 import tempConstructor from "../../temp.constructor.json";
 
 export function saveTempConstructor(key: string, constructorConfig: any) {
-    (tempConstructor as any)[key] = constructorConfig;
+    (tempConstructor as any)[key] = Object.values(constructorConfig).map((item: any) => item.toString());
     fs.writeFileSync("temp.constructor.json", JSON.stringify(tempConstructor));
 }
 
 export async function verifyAll(hre: HardhatRuntimeEnvironment) {
-    // **** I need to save as an array + I need to convert to string first
     for (const [key, value] of Object.entries(tempConstructor)) await hre.run("verify:verify", {address: key, constructorArguments: value});
 }
