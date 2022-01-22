18f5b95e706e29efcc19a7a8794ad4fbdefda653
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-22 14:17:10 +1100

    🎉 Update

--- scripts/util/utilVerify.ts
+++ scripts/util/utilVerify.ts
@@ -1,13 +1,25 @@
 import fs from "fs";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 
-import tempConstructor from "../../temp.constructor.json";
+const TEMP_CONSTRUCTOR_NAME = "temp.constructor.json";
+
+export function loadTempConstructor() {
+    if (fs.existsSync(TEMP_CONSTRUCTOR_NAME)) {
+        const data = fs.readFileSync(TEMP_CONSTRUCTOR_NAME).toString();
+        return JSON.parse(data);
+    } else {
+        return {};
+    }
+}
 
 export function saveTempConstructor(key: string, constructorConfig: any) {
+    const tempConstructor = loadTempConstructor();
     (tempConstructor as any)[key] = Object.values(constructorConfig).map((item: any) => item.toString());
-    fs.writeFileSync("temp.constructor.json", JSON.stringify(tempConstructor));
+    fs.writeFileSync(TEMP_CONSTRUCTOR_NAME, JSON.stringify(tempConstructor));
 }
 
 export async function verifyAll(hre: HardhatRuntimeEnvironment) {
+    const tempConstructor = loadTempConstructor();
     for (const [key, value] of Object.entries(tempConstructor)) await hre.run("verify:verify", {address: key, constructorArguments: value});
+    fs.unlinkSync(TEMP_CONSTRUCTOR_NAME);
 }
