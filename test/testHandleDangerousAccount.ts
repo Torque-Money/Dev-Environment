73bd3786ebb0063e2dd26404409cb1824e493551
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-23 13:30:09 +1100

    🎉 Update

--- test/testHandleDangerousAccount.ts
+++ test/testHandleDangerousAccount.ts
@@ -2,21 +2,30 @@ import {expect} from "chai";
 import {ethers} from "hardhat";
 import config from "../config.fork.json";
 import {shouldFail} from "../scripts/util/utilsTest";
+import {ERC20, LPool, MarginLong, Resolver} from "../typechain-types";
 
 describe("Liquidate", async function () {
+    let marginLong: MarginLong;
     let pool: LPool;
-    let token: ERC20;
+    let resolver: Resolver;
+    let collateralToken: ERC20;
+    let borrowedToken: ERC20;
     let lpToken: ERC20;
     let signerAddress: string;
 
     beforeEach(async () => {
         pool = await ethers.getContractAt("LPool", config.leveragePoolAddress);
-        token = await ethers.getContractAt("ERC20", config.approved[1].address);
+        marginLong = await ethers.getContractAt("MarginLong", config.marginLongAddress);
+        resolver = await ethers.getContractAt("Resolver", config.resolverAddress);
 
-        lpToken = await ethers.getContractAt("ERC20", await pool.LPFromPT(token.address));
+        collateralToken = await ethers.getContractAt("ERC20", config.approved[0].address);
+        borrowedToken = await ethers.getContractAt("ERC20", config.approved[1].address);
+        lpToken = await ethers.getContractAt("ERC20", await pool.LPFromPT(borrowedToken.address));
 
         const signer = ethers.provider.getSigner();
         signerAddress = await signer.getAddress();
+
+        // **** Now here we want to deposit collateral and such into the account and see what happens and if it is liquidatable
     });
 
     it("should liquidate a dangerous account", async () => {});
