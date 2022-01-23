867721036dea424242c77d72ced5f54899f41b83
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-23 12:15:31 +1100

    🎉 Update

---
+++ contracts/Oracle/OracleTest.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IOracle.sol";
+
+contract OracleTest is IOracle {
+    function priceMin(IERC20 token_, uint256 amount_) external view returns (uint256);
+
+    function priceMax(IERC20 token_, uint256 amount_) external view returns (uint256);
+
+    function amountMin(IERC20 token_, uint256 price_) external view returns (uint256);
+
+    function amountMax(IERC20 token_, uint256 price_) external view returns (uint256);
+}
