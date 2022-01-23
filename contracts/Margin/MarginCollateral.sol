6d9d67c8e961172ff996b237bfa6da6bef4aff09
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-23 22:02:15 +1100

    🎉 Update

--- contracts/Margin/MarginCollateral.sol
+++ contracts/Margin/MarginCollateral.sol
@@ -25,7 +25,7 @@ abstract contract MarginCollateral is MarginApproved, MarginLevel, MarginLimits 
         require(amount_ <= collateral(collateral_, _msgSender()), "MarginCollateral: Cannot remove more than available collateral");
 
         _setCollateral(collateral_, collateral(collateral_, _msgSender()).sub(amount_), _msgSender());
-        require(!resettable(_msgSender() && !liquidatable(_msgSender()), "MarginCollateral: Withdrawing desired collateral puts account at risk");
+        require(!resettable(_msgSender()) && !liquidatable(_msgSender()), "MarginCollateral: Withdrawing desired collateral puts account at risk");
 
         collateral_.safeTransfer(_msgSender(), amount_);
 
