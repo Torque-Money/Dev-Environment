29e3ec9b3f3b8e1a58c07167aa8b0c3501e2ed3e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-24 18:37:13 +1100

    🎉 Update

--- contracts/MarginLong/MarginLongBorrow.sol
+++ contracts/MarginLong/MarginLongBorrow.sol
@@ -9,22 +9,22 @@ abstract contract MarginLongBorrow is Margin {
     using SafeMath for uint256;
 
     // Margin borrow against collateral
-    function borrow(IERC20 borrowed_, uint256 amount_) external onlyApprovedBorrowed(borrowed_) {
-        if (!isBorrowing(borrowed_, _msgSender())) {
-            _setInitialBorrowBlock(borrowed_, block.number, _msgSender());
+    function borrow(IERC20 token_, uint256 amount_) external onlyApprovedBorrowedToken(token_) {
+        if (!isBorrowing(token_, _msgSender())) {
+            _setInitialBorrowBlock(token_, block.number, _msgSender());
             _addAccount(_msgSender());
         }
 
-        pool.claim(borrowed_, amount_);
-        _setBorrowed(borrowed_, borrowed(borrowed_, _msgSender()).add(amount_), _msgSender());
+        pool.claim(token_, amount_);
+        _setBorrowed(token_, borrowed(token_, _msgSender()).add(amount_), _msgSender());
 
-        uint256 _initialBorrowPrice = oracle.priceMin(borrowed_, amount_);
-        _setInitialBorrowPrice(borrowed_, initialBorrowPrice(borrowed_, _msgSender()).add(_initialBorrowPrice), _msgSender());
+        uint256 _initialBorrowPrice = oracle.priceMin(token_, amount_);
+        _setInitialBorrowPrice(token_, initialBorrowPrice(token_, _msgSender()).add(_initialBorrowPrice), _msgSender());
 
         require(!resettable(_msgSender()) && !liquidatable(_msgSender()), "MarginLongBorrow: Borrowing desired amount puts account at risk");
 
-        emit Borrow(_msgSender(), borrowed_, amount_);
+        emit Borrow(_msgSender(), token_, amount_);
     }
 
-    event Borrow(address indexed account, IERC20 borrowed, uint256 amount);
+    event Borrow(address indexed account, IERC20 token, uint256 amount);
 }
