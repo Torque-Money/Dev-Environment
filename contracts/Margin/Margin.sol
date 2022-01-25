205e60d977758d6c2a9298b8b6cb1b6fb67b855d
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-25 17:32:07 +1100

    🎉 Update

--- contracts/Margin/Margin.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.0;
-
-import "./MarginCollateral.sol";
-import "./MarginBorrowers.sol";
-
-abstract contract Margin is MarginCollateral, MarginBorrowers {}
--- contracts/MarginLong/MarginLongRepayCore.sol
+++ contracts/MarginLong/MarginLongRepayCore.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../Margin/Margin.sol";
+import "./MarginLongCore.sol";
 
-abstract contract MarginLongRepayCore is Margin {
+abstract contract MarginLongRepayCore is MarginLongCore {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
