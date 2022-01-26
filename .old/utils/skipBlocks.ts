bd4634ad7a12018ebfac970609a8218f7fe4aa65
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-26 12:42:57 +1100

    🎉 Update

--- .old/utils/skipBlocks.ts
+++
@@ -1,5 +0,0 @@
-import hre from "hardhat";
-
-export default async function skipBlocks(blocks: number) {
-    for (let i = 0; i < blocks; i++) await hre.network.provider.send("evm_mine");
-}
--- .old/utils/timeTravel.ts
+++
@@ -1,5 +0,0 @@
-import hre from "hardhat";
-
-export default async function timeTravel(minutes: number) {
-    hre.run("time", { time: minutes.toString() });
-}
