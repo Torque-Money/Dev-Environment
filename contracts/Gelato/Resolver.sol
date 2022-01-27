40ea64562c0587952459d7fdff7544911ab8fd17
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-27 11:40:05 +1100

    🎉 Update

--- contracts/Gelato/Resolver.sol
+++ contracts/Gelato/Resolver.sol
@@ -12,15 +12,15 @@ contract Resolver is PokeMeReady {
     using SafeMath for uint256;
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    IConverter public converter;
-    MarginLong public marginLong;
-    LPool public pool;
+    address public converter;
+    address public marginLong;
+    address public pool;
 
     constructor(
         address pokeMe_,
-        MarginLong marginLong_,
-        LPool pool_,
-        IConverter converter_
+        address marginLong_,
+        address pool_,
+        address converter_
     ) PokeMeReady(pokeMe_) {
         marginLong = marginLong_;
         pool = pool_;
@@ -29,22 +29,23 @@ contract Resolver is PokeMeReady {
 
     // Repay debt for eth
     function _repayInEth(
-        IERC20Upgradeable[] memory repayToken_,
+        address[] memory repayToken_,
         uint256[] memory repayAmount_,
         uint256 outAmount_
     ) internal {
         for (uint256 i = 0; i < repayToken_.length; i++) {
-            uint256 amountOut = converter.maxAmountEthOut(repayToken_[i], repayAmount_[i]);
+            uint256 amountOut = IConverter(converter).maxAmountEthOut(repayToken_[i], repayAmount_[i]);
 
             if (amountOut > outAmount_) {
-                uint256 amountIn = converter.minAmountTokenInEthOut(repayToken_[i], outAmount_);
-                repayToken_[i].safeApprove(address(converter), amountIn);
-                converter.swapMaxEthOut(repayToken_[i], amountIn);
+                uint256 amountIn = IConverter(converter).minAmountTokenInEthOut(repayToken_[i], outAmount_);
+                IERC20Upgradeable(repayToken_[i]).safeApprove(converter, amountIn);
+                IConverter(converter).swapMaxEthOut(repayToken_[i], amountIn);
                 repayAmount_[i] = repayAmount_[i].sub(amountIn);
+
                 break;
             } else {
-                repayToken_[i].safeApprove(address(converter), repayAmount_[i]);
-                converter.swapMaxEthOut(repayToken_[i], repayAmount_[i]);
+                IERC20Upgradeable(repayToken_[i]).safeApprove(converter, repayAmount_[i]);
+                IConverter(converter).swapMaxEthOut(repayToken_[i], repayAmount_[i]);
                 repayAmount_[i] = 0;
             }
         }
@@ -52,51 +53,52 @@ contract Resolver is PokeMeReady {
 
     // Repay debt for tokens
     function _repayInToken(
-        IERC20Upgradeable[] memory repayToken_,
+        address[] memory repayToken_,
         uint256[] memory repayAmount_,
-        IERC20Upgradeable outToken_,
+        address outToken_,
         uint256 outAmount_
     ) internal {
         for (uint256 i = 0; i < repayToken_.length; i++) {
-            uint256 amountOut = converter.maxAmountTokenOut(repayToken_[i], repayAmount_[i], outToken_);
+            uint256 amountOut = IConverter(converter).maxAmountTokenOut(repayToken_[i], repayAmount_[i], outToken_);
 
             if (amountOut > outAmount_) {
-                uint256 amountIn = converter.minAmountTokenInTokenOut(repayToken_[i], outToken_, repayAmount_[i]);
-                repayToken_[i].safeApprove(address(converter), amountIn);
-                converter.swapMaxTokenOut(repayToken_[i], amountIn, outToken_);
+                uint256 amountIn = IConverter(converter).minAmountTokenInTokenOut(repayToken_[i], outToken_, repayAmount_[i]);
+                IERC20Upgradeable(repayToken_[i]).safeApprove(converter, amountIn);
+                IConverter(converter).swapMaxTokenOut(repayToken_[i], amountIn, outToken_);
                 repayAmount_[i] = repayAmount_[i].sub(amountIn);
+
                 break;
             } else {
-                repayToken_[i].safeApprove(address(converter), repayAmount_[i]);
-                converter.swapMaxTokenOut(repayToken_[i], repayAmount_[i], outToken_);
+                IConverter(repayToken_[i]).safeApprove(converter, repayAmount_[i]);
+                IConverter(converter).swapMaxTokenOut(repayToken_[i], repayAmount_[i], outToken_);
                 repayAmount_[i] = 0;
             }
         }
     }
 
     // Pay transaction
-    function _payTransaction(IERC20Upgradeable[] memory repayToken_, uint256[] memory repayAmount_) internal {
+    function _payTransaction(address[] memory repayToken_, uint256[] memory repayAmount_) internal {
         (uint256 fee, address feeToken) = IPokeMe(pokeMe).getFeeDetails();
 
         if (feeToken == ETH) _repayInEth(repayToken_, repayAmount_, fee);
-        else _repayInToken(repayToken_, repayAmount_, IERC20Upgradeable(feeToken), fee);
+        else _repayInToken(repayToken_, repayAmount_, feeToken, fee);
 
         _transfer(fee, feeToken);
 
         for (uint256 i = 0; i < repayToken_.length; i++) {
             if (repayAmount_[i] > 0) {
-                repayToken_[i].safeApprove(address(pool), repayAmount_[i]);
-                pool.deposit(repayToken_[i], repayAmount_[i]);
+                IERC20Upgradeable(repayToken_[i]).safeApprove(pool, repayAmount_[i]);
+                LPool(pool).deposit(repayToken_[i], repayAmount_[i]);
             }
         }
     }
 
     // Check if an account needs to be liquidated
     function checkLiquidate() external view returns (bool canExec, bytes memory execPayload) {
-        address[] memory accounts = marginLong.getBorrowingAccounts();
+        address[] memory accounts = MarginLong(marginLong).getBorrowingAccounts();
 
         for (uint256 i = 0; i < accounts.length; i++) {
-            if (marginLong.liquidatable(accounts[i])) {
+            if (MarginLong(marginLong).liquidatable(accounts[i])) {
                 canExec = true;
                 execPayload = abi.encodeWithSelector(this.executeLiquidate.selector, accounts[i]);
 
@@ -109,10 +111,10 @@ contract Resolver is PokeMeReady {
 
     // Check if an account needs to be reset
     function checkReset() external view returns (bool canExec, bytes memory execPayload) {
-        address[] memory accounts = marginLong.getBorrowingAccounts();
+        address[] memory accounts = MarginLong(marginLong).getBorrowingAccounts();
 
         for (uint256 i = 0; i < accounts.length; i++) {
-            if (marginLong.resettable(accounts[i])) {
+            if (MarginLong(marginLong).resettable(accounts[i])) {
                 canExec = true;
                 execPayload = abi.encodeWithSelector(this.executeReset.selector, accounts[i]);
 
@@ -125,13 +127,13 @@ contract Resolver is PokeMeReady {
 
     // Execute liquidate and repay
     function executeLiquidate(address account_) external onlyPokeMe {
-        (IERC20[] memory repayTokens, uint256[] memory repayAmounts) = marginLong.liquidateAccount(account_);
+        (address[] memory repayTokens, uint256[] memory repayAmounts) = MarginLong(marginLong).liquidateAccount(account_);
         _payTransaction(repayTokens, repayAmounts);
     }
 
     // Execute reset and repay
     function executeReset(address account_) external onlyPokeMe {
-        (IERC20[] memory repayTokens, uint256[] memory repayAmounts) = marginLong.resetAccount(account_);
+        (address[] memory repayTokens, uint256[] memory repayAmounts) = MarginLong(marginLong).resetAccount(account_);
         _payTransaction(repayTokens, repayAmounts);
     }
 }
