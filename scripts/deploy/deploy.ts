570bd9bc30b790ebb246eb468a5a5104fd3e46dd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-27 20:25:53 +1100

    🎉 Update

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -1,10 +1,12 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {ConfigType} from "../util/utilConfig";
+
 import deployPool from "./deployPool";
 import deployOracle from "./deployOracle";
 import deployConverter from "./deployConverter";
 import deployMarginLong from "./deployMarginLong";
 import deployResolver from "./deployResolver";
-import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {ConfigType} from "../util/utilConfig";
+import deployTimelock from "./deployTimelock";
 
 export default async function main(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
     await deployConverter(configType, hre);
@@ -12,4 +14,5 @@ export default async function main(configType: ConfigType, hre: HardhatRuntimeEn
     await deployPool(configType, hre);
     await deployMarginLong(configType, hre);
     await deployResolver(configType, hre);
+    await deployTimelock(configType, hre);
 }
--- scripts/setup/setup.ts
+++ scripts/setup/setup.ts
@@ -1,11 +1,14 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {ConfigType} from "../util/utilConfig";
+
 import setupPool from "./setupPool";
 import setupOracle from "./setupOracle";
 import setupMarginLong from "./setupMarginLong";
-import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {ConfigType} from "../util/utilConfig";
+import setupTimelock from "./setupTimelock";
 
 export default async function main(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
     await setupPool(configType, hre);
     await setupOracle(configType, hre);
     await setupMarginLong(configType, hre);
+    await setupTimelock(configType, hre);
 }
