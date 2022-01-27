346207cf33a241ffcd20d051f9d6e30047e63a54
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-27 20:34:52 +1100

    🎉 Update

---
+++ test/testTimelock.ts
@@ -0,0 +1,10 @@
+import {expect} from "chai";
+import {ethers} from "hardhat";
+import config from "../config.fork.json";
+import {shouldFail} from "../scripts/util/utilsTest";
+
+// **** I need a seperate function to delay the time
+
+describe("Timelock", async function () {
+    it("should execute an admin only request to the converter", async () => {});
+});
