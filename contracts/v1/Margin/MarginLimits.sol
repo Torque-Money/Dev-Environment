84bec2318a11203c4f0a53a4a052c99800edffd7
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-28 20:26:28 +1100

    🎉 Update

--- contracts/v1/Margin/MarginLimits.sol
+++ contracts/v1/Margin/MarginLimits.sol
@@ -36,7 +36,7 @@ abstract contract MarginLimits is Initializable, MarginAccount {
         uint256 collateralPrice = collateralPrice(account_);
         uint256 totalInitialBorrowPrice = initialBorrowPrice(account_);
 
-        // **** Make modification here ???
+        // **** Make modification here ????
 
         return (collateralPrice.mul(maxLeverage) < totalInitialBorrowPrice);
     }
