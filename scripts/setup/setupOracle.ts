793ba89b6b8ab6ac5e58247741ac26291fd81431
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-30 19:21:15 +1100

    🎉 Update

--- scripts/setup/setupOracle.ts
+++ scripts/setup/setupOracle.ts
@@ -1,5 +1,4 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {OracleTest} from "../../typechain-types";
 import {chooseConfig, ConfigType} from "../util/utilConfig";
 
 export default async function main(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
@@ -15,17 +14,6 @@ export default async function main(configType: ConfigType, hre: HardhatRuntimeEn
     const correctDecimals = config.approved.filter((approved) => approved.oracle).map((approved) => approved.decimals);
     const oracleSupported = Array(oracleApproved.length).fill(true);
     await oracle.setPriceFeed(oracleApproved, priceFeeds, reservePriceFeeds, correctDecimals, oracleSupported);
-
-    const priceDecimals = await oracle.priceDecimals();
-    if (configType === "fork") {
-        oracle = oracle as OracleTest;
-        await oracle.setPrice(config.approved[0].address, hre.ethers.BigNumber.from(10).pow(priceDecimals).mul(1));
-        await oracle.setPrice(config.approved[1].address, hre.ethers.BigNumber.from(10).pow(priceDecimals).mul(30));
-    } else if (configType === "test") {
-        oracle = oracle as OracleTest;
-        await oracle.setPrice(config.approved[0].address, hre.ethers.BigNumber.from(10).pow(priceDecimals).mul(1));
-        await oracle.setPrice(config.approved[1].address, hre.ethers.BigNumber.from(10).pow(priceDecimals).mul(2000));
-    }
 
     console.log("Setup: Oracle");
 }
