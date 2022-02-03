abab9dabed8e5f1c3e9d9f7cb15c64ea73853ec5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-04 08:59:38 +1100

    🎉 Update

--- test/7.testConverter.ts
+++ test/7.testConverter.ts
@@ -44,67 +44,67 @@ describe("Converter", async function () {
 
         await (await inToken.approve(converter.address, swapAmount)).wait();
 
-        const initialAmount = await outToken.balanceOf(signerAddress);
+        const initialOutAmount = await outToken.balanceOf(signerAddress);
 
         await (await converter.swapMaxTokenOut(inToken.address, swapAmount, outToken.address)).wait();
 
-        expect((await outToken.balanceOf(signerAddress)).gt(initialAmount)).to.equal(true);
+        expect((await outToken.balanceOf(signerAddress)).gt(initialOutAmount)).to.equal(true);
     });
 
     it("should convert WETH to another", async () => {
         await (await weth.approve(converter.address, wethSwapAmount)).wait();
 
-        const initialAmount = await outToken.balanceOf(signerAddress);
+        const initialOutAmount = await outToken.balanceOf(signerAddress);
 
         await (await converter.swapMaxTokenOut(weth.address, wethSwapAmount, outToken.address)).wait();
 
-        expect((await outToken.balanceOf(signerAddress)).gt(initialAmount)).to.equal(true);
+        expect((await outToken.balanceOf(signerAddress)).gt(initialOutAmount)).to.equal(true);
     });
 
     it("should convert another to WETH", async () => {
         await (await inToken.approve(converter.address, swapAmount)).wait();
 
-        const initialAmount = await weth.balanceOf(signerAddress);
+        const initialOutAmount = await weth.balanceOf(signerAddress);
 
         await (await converter.swapMaxTokenOut(inToken.address, swapAmount, weth.address)).wait();
 
-        expect((await weth.balanceOf(signerAddress)).gt(initialAmount)).to.equal(true);
+        expect((await weth.balanceOf(signerAddress)).gt(initialOutAmount)).to.equal(true);
     });
 
     it("should convert one token into ETH", async () => {
         await (await inToken.approve(converter.address, swapAmount)).wait();
 
-        const initialAmount = await ethers.provider.getBalance(signerAddress);
+        const initialOutAmount = await ethers.provider.getBalance(signerAddress);
 
         await converter.swapMaxEthOut(inToken.address, swapAmount);
 
-        expect((await ethers.provider.getBalance(signerAddress)).gt(initialAmount)).to.equal(true);
+        expect((await ethers.provider.getBalance(signerAddress)).gt(initialOutAmount)).to.equal(true);
     });
 
     it("should convert WETH into ETH", async () => {
         await (await weth.approve(converter.address, wethSwapAmount)).wait();
 
-        const initialAmount = await ethers.provider.getBalance(signerAddress);
+        const initialOutAmount = await ethers.provider.getBalance(signerAddress);
 
         await converter.swapMaxEthOut(weth.address, wethSwapAmount);
 
-        expect((await ethers.provider.getBalance(signerAddress)).gt(initialAmount)).to.equal(true);
+        expect((await ethers.provider.getBalance(signerAddress)).gt(initialOutAmount)).to.equal(true);
     });
 
     it("should swap some ETH into the given token", async () => {
-        const initialAmount = await outToken.balanceOf(signerAddress);
+        const initialOutAmount = await outToken.balanceOf(signerAddress);
 
         await (await converter.swapMaxEthIn(outToken.address, {value: 100})).wait();
 
-        expect((await outToken.balanceOf(signerAddress)).gt(initialAmount)).to.equal(true);
+        expect((await outToken.balanceOf(signerAddress)).gt(initialOutAmount)).to.equal(true);
     });
 
     it("should swap some ETH into WETH", async () => {
-        const initialAmount = await weth.balanceOf(signerAddress);
+        const initialOutAmount = await weth.balanceOf(signerAddress);
 
         await (await converter.swapMaxEthIn(weth.address, {value: 100})).wait();
 
-        expect((await weth.balanceOf(signerAddress)).gt(initialAmount)).to.equal(true);
+        expect((await weth.balanceOf(signerAddress)).gt(initialOutAmount)).to.equal(true);
     });
 
     // **** I need to test the amounts taken in as well as well as output
