a5befbf9f35a76ff06d452b3a1458ee0169b1281
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-05 19:25:48 +1100

    🎉 Update

--- scripts/util/utilVerify.ts
+++ scripts/util/utilVerify.ts
@@ -14,7 +14,7 @@ export function loadTempConstructor() {
 
 export function saveTempConstructor(address: string, constructorConfig: any) {
     const tempConstructor = loadTempConstructor();
-    (tempConstructor as any)[address] = Object.values(constructorConfig).map((item: any) => item.toString());
+    (tempConstructor as any)[address] = Object.values(constructorConfig).map((item: any) => (typeof item === "object" ? item : item.toString()));
     fs.writeFileSync(TEMP_CONSTRUCTOR_NAME, JSON.stringify(tempConstructor));
 }
 
