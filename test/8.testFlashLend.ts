73deb9ec19af76510a144b2d9c383c5af089bcb4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-08 20:49:57 +1100

    🎉 Update

--- test/8.testFlashLend.ts
+++ test/8.testFlashLend.ts
@@ -1,10 +1,12 @@
+import {expect} from "chai";
+import {BigNumber} from "ethers";
 import {ethers} from "hardhat";
 import config from "../config.fork.json";
 import {ERC20, FlashBorrower, FlashLender, LPool} from "../typechain-types";
 
 describe("FlashLend", async function () {
-    let borrowApproved: any;
-    let borrowToken: ERC20;
+    let tokenApproved: any;
+    let token: ERC20;
 
     let lpToken: ERC20;
 
@@ -14,22 +16,24 @@ describe("FlashLend", async function () {
 
     let signerAddress: string;
 
+    let depositAmount: BigNumber;
+
     beforeEach(async () => {
-        borrowApproved = config.approved[1];
-        borrowToken = await ethers.getContractAt("ERC20", borrowApproved.address);
+        tokenApproved = config.approved[1];
+        token = await ethers.getContractAt("ERC20", tokenApproved.address);
 
         flashLender = await ethers.getContractAt("FlashLender", config.flashLender);
         flashBorrower = await ethers.getContractAt("FlashBorrower", config.flashBorrower);
 
         pool = await ethers.getContractAt("LPool", config.leveragePoolAddress);
 
-        lpToken = await ethers.getContractAt("ERC20", await pool.PTFromLP(borrowToken.address));
+        lpToken = await ethers.getContractAt("ERC20", await pool.PTFromLP(token.address));
 
         const signer = ethers.provider.getSigner();
         signerAddress = await signer.getAddress();
 
-        const depositAmount = ethers.BigNumber.from(10).pow(borrowApproved.decimals).mul(10);
-        await (await pool.addLiquidity(borrowToken.address, depositAmount)).wait();
+        depositAmount = ethers.BigNumber.from(10).pow(tokenApproved.decimals).mul(10);
+        await (await pool.addLiquidity(token.address, depositAmount)).wait();
     });
 
     afterEach(async () => {
@@ -37,9 +41,21 @@ describe("FlashLend", async function () {
         if (LPTokenAmount.gt(0)) await (await pool.removeLiquidity(lpToken.address, LPTokenAmount)).wait();
     });
 
-    // It should execute a normal flashloan
-    it("should execute a flash loan", async () => {
-        // **** First we have to send funds to the contract to be used
+    it("should execute a flash loan successfully", async () => {
+        const maxAmount = await flashLender.maxFlashLoan(token.address);
+        expect(maxAmount).to.equal(depositAmount);
+
+        const [feePercentNumerator, feePercentDenominator] = await flashLender.feePercent();
+        const fee = await flashLender.flashFee(token.address, depositAmount);
+        expect(fee).to.equal(depositAmount.mul(feePercentNumerator).div(feePercentDenominator));
+
+        await (await token.transfer(flashBorrower.address, fee)).wait();
+
+        await (await flashBorrower.callFlashLoan(token.address, depositAmount)).wait();
+
+        expect((await pool.liquidity(token.address)).gt(depositAmount)).to.equal(true);
+        expect((await pool.tvl(token.address)).gt(depositAmount)).to.equal(true);
+        expect((await token.balanceOf(pool.address)).gt(depositAmount)).to.equal(true);
     });
 
     // It should attempt to borrow more than what is available
