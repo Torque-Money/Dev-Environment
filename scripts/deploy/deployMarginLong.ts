b18203d7f0f3f0c674c997e8146190329d16ce16
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-21 22:15:35 +1100

    🎉 Update

--- scripts/deploy/deployMarginLong.ts
+++ scripts/deploy/deployMarginLong.ts
@@ -7,8 +7,8 @@ export default async function main(configType: ConfigType, hre: HardhatRuntimeEn
     const config = chooseConfig(configType);
 
     const constructorArgs = {
-        pool: config.leveragePoolAddress,
-        oracle: config.oracleAddress,
+        pool: config.contracts.leveragePoolAddress,
+        oracle: config.contracts.oracleAddress,
         minCollateralPrice: hre.ethers.BigNumber.from(10).pow(18).mul(100),
         maxLeverageNumerator: 125,
         maxLeverageDenominator: 1,
@@ -20,10 +20,10 @@ export default async function main(configType: ConfigType, hre: HardhatRuntimeEn
     const marginLong = await hre.upgrades.deployProxy(MarginLong, Object.values(constructorArgs));
     await marginLong.deployed();
 
-    config.marginLongAddress = marginLong.address;
-    config.marginLongLogicAddress = await getImplementationAddress(hre.ethers.provider, marginLong.address);
-    console.log(`Deployed: Margin long proxy and margin long | ${marginLong.address} ${config.marginLongLogicAddress}`);
+    config.contracts.marginLongAddress = marginLong.address;
+    const implementation = await getImplementationAddress(hre.ethers.provider, marginLong.address);
+    console.log(`Deployed: Margin long, implementation | ${marginLong.address} ${implementation}`);
 
-    if (configType !== "fork") saveTempConstructor(config.marginLongLogicAddress, {});
+    if (configType !== "fork") saveTempConstructor(implementation, {});
     saveConfig(config, configType);
 }
