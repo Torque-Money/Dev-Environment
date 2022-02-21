138703ad23344e012218ceb7d77de821797efa43
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-21 22:44:39 +1100

    🎉 Update

--- scripts/deploy/deployLPToken.ts
+++ scripts/deploy/deployLPToken.ts
@@ -1,29 +1,33 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {chooseConfig, ConfigType, saveConfig} from "../utils/utilConfig";
-// import {saveTempConstructor} from "../utils/utilVerify";
-// import {getImplementationAddress} from "@openzeppelin/upgrades-core";
+import {saveTempConstructor} from "../utils/utilVerify";
+import {getImplementationAddress} from "@openzeppelin/upgrades-core";
 
 export default async function main(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
     const config = chooseConfig(configType);
 
-    // **** Here we are going to use a beacon proxy and deploy new tokens off of it
+    const LPoolToken = await hre.ethers.getContractFactory("LPoolToken");
+    const beacon = await hre.upgrades.deployBeacon(LPoolToken);
+    await beacon.deployed();
 
-    // const constructorArgs = {
-    //     converter: config.converterAddress,
-    //     oracle: config.oracleAddress,
-    //     taxPercentNumerator: 5,
-    //     taxPercentDenominator: 100,
-    //     timePerInterestApplication: hre.ethers.BigNumber.from(86400).mul(365),
-    // };
+    config.tokens.lpTokens.beaconAddress = beacon.address;
+    const implementation = await getImplementationAddress(hre.ethers.provider, beacon.address);
+    console.log(`Deployed: Beacon | ${beacon.address}`);
 
-    // const Pool = await hre.ethers.getContractFactory("LPool");
-    // const pool = await hre.upgrades.deployProxy(Pool, Object.values(constructorArgs));
-    // await pool.deployed();
+    const tokens = [];
+    for (const approved of config.tokens.approved.filter((approved) => approved.leveragePool)) {
+        const LPToken = await hre.upgrades.deployBeaconProxy(beacon, LPoolToken, [
+            config.setup.LPPrefixName + " " + approved.name,
+            config.setup.LPPrefixSymbol + approved.symbol,
+        ]);
+        await LPToken.deployed();
 
-    // config.leveragePoolAddress = pool.address;
-    // config.leveragePoolLogicAddress = await getImplementationAddress(hre.ethers.provider, pool.address);
-    // console.log(`Deployed: Pool proxy and pool | ${pool.address} ${config.leveragePoolLogicAddress}`);
+        tokens.push(LPToken.address);
 
-    // if (configType !== "fork") saveTempConstructor(config.leveragePoolLogicAddress, {});
-    // saveConfig(config, configType);
+        console.log(`Deployed: LPoolToken`);
+    }
+    config.tokens.lpTokens.tokens = tokens;
+
+    if (configType !== "fork") saveTempConstructor(implementation, {});
+    saveConfig(config, configType);
 }
