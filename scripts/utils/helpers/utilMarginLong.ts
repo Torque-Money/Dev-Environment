837283c8df9e5a3ee5f0a3ea3de6fb927fd766f0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-22 13:14:56 +1100

    🎉 Update

---
+++ scripts/utils/helpers/utilMarginLong.ts
@@ -0,0 +1,3 @@
+// **** I want a method to automatically unborrow all as well as to automatically withdraw all collateral
+
+// **** For pool AND this, add helpers for the cleanups and the things that CAN be repeated without needing to be tested individually
---
+++ scripts/utils/helpers/utilPool.ts
@@ -0,0 +1,1 @@
+// **** I want a function to automatically clean up all of my outstanding LP tokens
