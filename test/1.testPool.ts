d70fa2267e45f000eeb2d8b7d2fc5621ee579d3a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-22 22:00:54 +1100

    🎉 Update

--- test/1.testPool.ts
+++ test/1.testPool.ts
@@ -1,74 +1,83 @@
 import {expect} from "chai";
 import {BigNumber} from "ethers";
-import {ethers} from "hardhat";
+import hre from "hardhat";
 import config from "../config.fork.json";
 import {shouldFail} from "../scripts/utils/helpers/utilTest";
-import {ERC20, LPool} from "../typechain-types";
+import {getLPTokens, getPoolTokens, getTokenAmount, Token} from "../scripts/utils/helpers/utilTokens";
+import {ERC20, LPool, LPoolToken} from "../typechain-types";
 
 describe("Pool", async function () {
-    let approvedToken: any;
-    let token: ERC20;
-
-    let lpToken: ERC20;
+    let poolTokens: Token[];
+    let lpTokens: LPoolToken[];
+    let depositAmounts: BigNumber[];
 
     let pool: LPool;
 
     let signerAddress: string;
 
-    let depositAmount: BigNumber;
-
     beforeEach(async () => {
-        approvedToken = config.approved[1];
-        token = await ethers.getContractAt("ERC20", approvedToken.address);
+        poolTokens = await getPoolTokens("fork", hre);
+        depositAmounts = await getTokenAmount(
+            hre,
+            poolTokens.map((token) => token.token)
+        );
 
-        depositAmount = ethers.BigNumber.from(10).pow(approvedToken.decimals).mul(50);
+        pool = await hre.ethers.getContractAt("LPool", config.contracts.leveragePoolAddress);
 
-        pool = await ethers.getContractAt("LPool", config.leveragePoolAddress);
+        lpTokens = await getLPTokens("fork", hre, pool);
 
-        lpToken = await ethers.getContractAt("ERC20", await pool.LPFromPT(token.address));
-
-        const signer = ethers.provider.getSigner();
-        signerAddress = await signer.getAddress();
+        signerAddress = await hre.ethers.provider.getSigner().getAddress();
     });
 
     it("should stake tokens for LP tokens and redeem for an equal amount", async () => {
-        const initialBalance = await token.balanceOf(signerAddress);
+        const index = 0;
+        const poolToken = poolTokens[index].token;
+        const lpToken = lpTokens[index];
+        const depositAmount = depositAmounts[index];
 
-        const providedValue = await pool.addLiquidityOutLPTokens(token.address, depositAmount);
-        await (await pool.addLiquidity(token.address, depositAmount)).wait();
+        const initialBalance = await poolToken.balanceOf(signerAddress);
 
-        expect(await token.balanceOf(signerAddress)).to.equal(initialBalance.sub(depositAmount));
+        const providedValue = await pool.provideLiquidityOutLPTokens(poolToken.address, depositAmount);
+        await (await pool.provideLiquidity(poolToken.address, depositAmount)).wait();
+
+        expect(await poolToken.balanceOf(signerAddress)).to.equal(initialBalance.sub(depositAmount));
         expect(await lpToken.balanceOf(signerAddress)).to.equal(depositAmount);
 
-        expect(await token.balanceOf(pool.address)).to.equal(depositAmount);
-        expect(await pool.liquidity(token.address)).to.equal(depositAmount);
-        expect(await pool.tvl(token.address)).to.equal(depositAmount);
+        expect(await poolToken.balanceOf(pool.address)).to.equal(depositAmount);
+        expect(await pool.liquidity(poolToken.address)).to.equal(depositAmount);
+        expect(await pool.totalAmountLocked(poolToken.address)).to.equal(depositAmount);
 
-        expect(await pool.removeLiquidityOutPoolTokens(lpToken.address, providedValue)).to.equal(depositAmount);
-
-        await (await pool.removeLiquidity(lpToken.address, providedValue)).wait();
+        expect(await pool.redeemLiquidityOutPoolTokens(lpToken.address, providedValue)).to.equal(depositAmount);
+        await (await pool.redeemLiquidity(lpToken.address, providedValue)).wait();
 
         expect(await lpToken.balanceOf(signerAddress)).to.equal(0);
-        expect(await token.balanceOf(signerAddress)).to.equal(initialBalance);
+        expect(await poolToken.balanceOf(signerAddress)).to.equal(initialBalance);
 
-        expect(await token.balanceOf(pool.address)).to.equal(0);
-        expect(await pool.liquidity(token.address)).to.equal(0);
-        expect(await pool.tvl(token.address)).to.equal(0);
+        expect(await poolToken.balanceOf(pool.address)).to.equal(0);
+        expect(await pool.liquidity(poolToken.address)).to.equal(0);
+        expect(await pool.totalAmountLocked(poolToken.address)).to.equal(0);
     });
 
     it("should fail to stake incorrect tokens and invalid amounts", async () => {
-        await shouldFail(async () => await pool.addLiquidity(lpToken.address, 0));
-        await shouldFail(async () => await pool.removeLiquidity(lpToken.address, 0));
+        await shouldFail(async () => await pool.provideLiquidity(hre.ethers.constants.AddressZero, 1));
+        await shouldFail(async () => await pool.redeemLiquidity(hre.ethers.constants.AddressZero, 1));
 
-        await shouldFail(async () => await pool.addLiquidity(token.address, ethers.BigNumber.from(2).pow(255)));
-        await shouldFail(async () => await pool.removeLiquidity(token.address, 0));
+        const index = 0;
+        const poolToken = poolTokens[index].token;
+        const lpToken = lpTokens[index];
+
+        await shouldFail(async () => await pool.provideLiquidity(poolToken.address, hre.ethers.BigNumber.from(2).pow(255)));
+        await shouldFail(async () => await pool.redeemLiquidity(lpToken.address, hre.ethers.BigNumber.from(2).pow(255)));
     });
 
     it("should fail to access out of bounds operations", async () => {
-        await shouldFail(async () => await pool.deposit(token.address, 0));
-        await shouldFail(async () => await pool.withdraw(token.address, 0));
+        const index = 0;
+        const poolToken = poolTokens[index].token;
 
-        await shouldFail(async () => await pool.claim(token.address, 0));
-        await shouldFail(async () => await pool.unclaim(token.address, 0));
+        await shouldFail(async () => await pool.deposit(poolToken.address, 0));
+        await shouldFail(async () => await pool.withdraw(poolToken.address, 0));
+
+        await shouldFail(async () => await pool.claim(poolToken.address, 0));
+        await shouldFail(async () => await pool.unclaim(poolToken.address, 0));
     });
 });
