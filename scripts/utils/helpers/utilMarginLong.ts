8119cb9fdd46690450c8be795df40c269af17865
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-25 08:02:54 +1100

    🎉 Update

--- scripts/utils/helpers/utilMarginLong.ts
+++ scripts/utils/helpers/utilMarginLong.ts
@@ -27,6 +27,31 @@ export async function borrow(marginLong: MarginLong, tokens: ERC20[], amounts: e
     for (let i = 0; i < tokens.length; i++) await (await marginLong.borrow(tokens[i].address, amounts[i])).wait();
 }
 
-async function allowedBorrowAmount(marginLong: MarginLong, oracle: Oracle, token: ERC20) {
-    // **** Make a function used to calculate the max leverage level
+async function allowedBorrowAmount(hre: HardhatRuntimeEnvironment, marginLong: MarginLong, oracle: Oracle, token: ERC20) {
+    const ROUND_CONSTANT = 1e4;
+
+    const signerAddress = await hre.ethers.provider.getSigner().getAddress();
+
+    const collateralPrice = await marginLong.collateralPrice(signerAddress);
+    const currentPriceBorrowed = await marginLong["initialBorrowPrice(address)"](signerAddress);
+
+    const [maxLeverageNumerator, maxLeverageDenominator] = await marginLong.maxLeverage();
+    const maxLeverage = maxLeverageNumerator.mul(ROUND_CONSTANT).div(maxLeverageDenominator).toNumber() / ROUND_CONSTANT;
+
+    const [currentLeverageNumerator, currentLeverageDenominator] = await marginLong.currentLeverage(signerAddress);
+    const currentLeverage = currentLeverageNumerator.mul(ROUND_CONSTANT).div(currentLeverageDenominator).toNumber() / ROUND_CONSTANT;
+
+    if (currentPriceBorrowed.gt(0)) {
+        const numerator = Math.floor((maxLeverage / currentLeverage - 1) * ROUND_CONSTANT);
+        const denominator = ROUND_CONSTANT;
+
+        return ethers.BigNumber.from(numerator).mul(currentPriceBorrowed).div(denominator);
+    } else {
+        if (!collateralPrice) throw Error("Collateral price cannot be undefined");
+
+        const numerator = Math.floor(maxLeverage * ROUND_CONSTANT);
+        const denominator = ROUND_CONSTANT;
+
+        return collateralPrice.mul(numerator).div(denominator);
+    }
 }
