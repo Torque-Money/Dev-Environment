521745d4e1bd77847abda517069b1bce006e7e83
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-27 21:07:50 +1100

    🎉 Update

--- scripts/setup/setupResolver.ts
+++ scripts/setup/setupResolver.ts
@@ -5,7 +5,7 @@ import {chooseConfig, ConfigType} from "../utils/utilConfig";
 export default async function main(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
     const config = chooseConfig(configType);
 
-    const resolver = await hre.ethers.getContractAt("Resolver", config.contracts.flashLender);
+    const resolver = await hre.ethers.getContractAt("Resolver", config.contracts.resolverAddress);
 
     await (await resolver.setMarginLong(config.contracts.marginLongAddress)).wait();
     await (await resolver.setConverter(config.contracts.converterAddress)).wait();
