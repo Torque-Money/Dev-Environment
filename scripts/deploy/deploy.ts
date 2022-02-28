189b1b4faaf7598475414ce66f7583a8986acfca
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-28 12:44:55 +1100

    🎉 Update

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -12,12 +12,12 @@ import deployFlashLender from "./deployFlashLender";
 import deployTimelock from "./deployTimelock";
 
 export default async function main(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
-    await deployConverter(configType, hre);
-    await deployOracle(configType, hre);
-    await deployPool(configType, hre);
-    await deployLPToken(configType, hre);
-    await deployMarginLong(configType, hre);
-    await deployResolver(configType, hre);
-    await deployFlashLender(configType, hre);
+    // await deployConverter(configType, hre);
+    // await deployOracle(configType, hre);
+    // await deployPool(configType, hre);
+    // await deployLPToken(configType, hre);
+    // await deployMarginLong(configType, hre);
+    // await deployResolver(configType, hre);
+    // await deployFlashLender(configType, hre);
     await deployTimelock(configType, hre);
 }
