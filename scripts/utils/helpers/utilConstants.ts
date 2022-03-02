3edfe412cf8b0c81cae2b4c6f22faead02cb02bf
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-02 21:48:52 +1100

    🎉 Update

---
+++ scripts/utils/helpers/utilConstants.ts
@@ -0,0 +1,6 @@
+import {BigNumber} from "ethers";
+
+export const BIG_NUM = BigNumber.from(2).pow(96);
+
+export const COLLATERAL_PRICE = BigNumber.from(1);
+export const BORROW_PRICE = BigNumber.from(100);
--- scripts/utils/helpers/utilTest.ts
+++ scripts/utils/helpers/utilTest.ts
@@ -40,8 +40,3 @@ export async function testWrapper(hre: HardhatRuntimeEnvironment, callback: () =
 
     await utilClump(configType, hre);
 }
-
-export const BIG_NUM = BigNumber.from(2).pow(96);
-
-export const COLLATERAL_PRICE = BigNumber.from(1);
-export const BORROW_PRICE = BigNumber.from(100);
