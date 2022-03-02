b2c0b4c4eecbdd956cd0984e03f37b8232d56197
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-02 22:08:19 +1100

    🎉 Update

--- scripts/utils/utilConstants.ts
+++ scripts/utils/utilConstants.ts
@@ -6,4 +6,6 @@ export const BIG_NUM = BigNumber.from(2).pow(96);
 export const COLLATERAL_PRICE = BigNumber.from(1);
 export const BORROW_PRICE = BigNumber.from(100);
 
+// Manually override the config type being assigned based off of the network selected
+// (e.g. forking mainnet and testing mainnet contracts)
 export const OVERRIDE_CONFIG_TYPE: ConfigType | null = null;
