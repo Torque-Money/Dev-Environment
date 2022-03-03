433ca112842052ede8df1d2d01c90d60239348d1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-04 05:47:22 +1100

    🎉 Update

--- scripts/utils/utilConfigTypeSelector.ts
+++ scripts/utils/utilConfigTypeSelector.ts
@@ -4,15 +4,15 @@ import {ConfigType} from "./utilConfig";
 import {OVERRIDE_CONFIG_TYPE} from "./utilConstants";
 
 export default async function getConfigType(hre: HardhatRuntimeEnvironment) {
-    const {chainId} = await hre.ethers.provider.getNetwork();
-    const configType = mapChainIdToConfigType(hre, chainId);
+    const networkName = hre.network.name;
+    const configType = mapNetworkToConfig(networkName);
 
     return OVERRIDE_CONFIG_TYPE !== null ? OVERRIDE_CONFIG_TYPE : configType;
 }
 
-function mapChainIdToConfigType(hre: HardhatRuntimeEnvironment, chainId: number): ConfigType {
-    if (chainId === 1337) return "fork";
-    else if (chainId === 4) return "test";
-    else if (chainId === 250) return "main";
-    else throw Error("Chain Id not supported");
+function mapNetworkToConfig(networkName: string): ConfigType {
+    if (networkName === "localhost") return "fork";
+    else if (networkName === "testnet") return "test";
+    else if (networkName === "mainnet") return "main";
+    else throw Error("Network not supported");
 }
