e9ae0947fee9b3e3120a54ff5d7d8d624c669207
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-07 21:03:45 +1100

    🎉 Update

---
+++ test/verification/5.verifyFlashLend.ts
@@ -0,0 +1,28 @@
+import {expect} from "chai";
+import hre from "hardhat";
+import {getFlashLenderTokens} from "../../scripts/utils/helpers/utilTokens";
+
+import {chooseConfig} from "../../scripts/utils/utilConfig";
+import getConfigType from "../../scripts/utils/utilConfigTypeSelector";
+import {ERC20Upgradeable, FlashLender} from "../../typechain-types";
+
+describe("Verify: FlashLend", async function () {
+    const configType = await getConfigType(hre);
+    const config = chooseConfig(configType);
+
+    let flashLendToken: ERC20Upgradeable[];
+
+    let flashLender: FlashLender;
+
+    before(async () => {
+        flashLender = await hre.ethers.getContractAt("FlashLender", config.contracts.flashLender);
+
+        flashLendToken = await getFlashLenderTokens(configType, hre);
+    });
+
+    it("should verify the pool", async () => expect(await flashLender.pool()).to.equal(config.contracts.leveragePoolAddress));
+
+    it("should check all approved tokens", async () => {
+        // **** Here
+    });
+});
