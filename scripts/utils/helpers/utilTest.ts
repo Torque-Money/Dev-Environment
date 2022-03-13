a80e4c25a1f5d3675f58e8ce475650223b1fd21a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-13 20:43:40 +1100

    🎉 Update

--- scripts/utils/helpers/utilTest.ts
+++ scripts/utils/helpers/utilTest.ts
@@ -25,7 +25,6 @@ export function approxEqual(a: BigNumber, b: BigNumber, percentError: number) {
     expect(a.sub(b).abs().mul(DISCRIMINATOR).lt(b)).to.equal(true);
 }
 
-// **** Replace all comparisons with this one and check if it fixes the issue
 export function expectAddressEqual(address1: string, address2: string) {
     expect(address1.toLowerCase()).to.equal(address2.toLowerCase());
 }
