70c84424df20fed74d34bdf69ff89af0c7a90db1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-19 08:14:27 +1100

    🎉 Update

--- scripts/utils/tokens/utilGetTokens.ts
+++ scripts/utils/tokens/utilGetTokens.ts
@@ -2,38 +2,34 @@ import {HardhatRuntimeEnvironment} from "hardhat/types";
 
 import {ERC20Upgradeable, LPoolToken} from "../../../typechain-types";
 
-import {chooseConfig, ConfigType} from "../config/utilConfig";
+import {chooseConfig, Config, ConfigType} from "../config/utilConfig";
 
 type Filter = "leveragePool" | "marginLongBorrow" | "marginLongCollateral" | "flashLender" | "oracle";
 
 // Get filtered approved tokens
-export function getFilteredApprovedTokens(config: any, filter: Filter) {
-    const config = chooseConfig(configType);
-
-    return config.tokens.approved.filter((approved) => approved.setup[filter]);
+export function getFilteredApprovedTokens(config: Config, filter: Filter) {
+    return config.tokens.approved.filter((approved) => approved.setup && approved.setup[filter]);
 }
 
 // Get token address filted by approved configuration
-export function getFilteredTokenAddresses(config: any, filter: Filter) {
-    return getFilteredApprovedTokens(configType, filter).map((approved) => approved.address);
+export function getFilteredTokenAddresses(config: Config, filter: Filter) {
+    return getFilteredApprovedTokens(config, filter).map((approved) => approved.address);
 }
 
 // Get tokens filtered by their approved configuration
 export async function getFilteredTokens(
-    configType: ConfigType,
+    config: Config,
     hre: HardhatRuntimeEnvironment,
     filter: "leveragePool" | "marginLongBorrow" | "marginLongCollateral" | "flashLender" | "oracle"
 ) {
     let tokens: ERC20Upgradeable[] = [];
-    for (const address of getFilteredTokenAddresses(configType, filter)) tokens.push(await hre.ethers.getContractAt("ERC20Upgradeable", address));
+    for (const address of getFilteredTokenAddresses(config, filter)) tokens.push(await hre.ethers.getContractAt("ERC20Upgradeable", address));
 
     return tokens;
 }
 
 // Get a list of LP tokens
-export async function getLPTokens(configType: ConfigType, hre: HardhatRuntimeEnvironment) {
-    const config = chooseConfig(configType);
-
+export async function getLPTokens(config: Config, hre: HardhatRuntimeEnvironment) {
     let tokens: LPoolToken[] = [];
     for (const approved of config.tokens.lpTokens.tokens) tokens.push(await hre.ethers.getContractAt("LPoolToken", approved));
 
