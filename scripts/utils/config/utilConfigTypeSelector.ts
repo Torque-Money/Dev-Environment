d2e8579448e1c71c027d92060de2341bf43a43b3
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-19 08:05:52 +1100

    🎉 Update

--- scripts/utils/config/utilConfigTypeSelector.ts
+++ scripts/utils/config/utilConfigTypeSelector.ts
@@ -6,13 +6,13 @@ import {OVERRIDE_CONFIG_TYPE} from "./utilConstants";
 // Get the config type from the name unless overriden
 export default function getConfigType(hre: HardhatRuntimeEnvironment) {
     const networkName = hre.network.name;
-    const configType = mapNetworkToConfig(networkName);
+    const configType = mapNetworkToConfigType(networkName as any);
 
     return OVERRIDE_CONFIG_TYPE !== null ? OVERRIDE_CONFIG_TYPE : configType;
 }
 
 // Map a specific network to the given config type
-function mapNetworkToConfig(networkName: string): ConfigType {
+function mapNetworkToConfigType(networkName: "localhost" | "testnet" | "mainnet"): ConfigType {
     if (networkName === "localhost") return "fork";
     else if (networkName === "testnet") return "test";
     else if (networkName === "mainnet") return "main";
