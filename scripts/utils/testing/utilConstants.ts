20b092839ea4b589fd22fc46de871710a56b6217
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-24 22:49:22 +1100

    🎉 Update

--- scripts/utils/testing/utilConstants.ts
+++ scripts/utils/testing/utilConstants.ts
@@ -2,7 +2,11 @@ import {BigNumber} from "ethers";
 
 export const ROUND_CONSTANT = 1e5;
 
-export const BIG_NUM = BigNumber.from(2).pow(96);
+export const BIG_NUM = BigNumber.from(2).pow(96); // **** Change this
+
+// **** These should be split most likely since one is for testing and the other is for constants
+
+// **** Change title of repo since it wont technically be just a monorepo, it will be the development repo
 
 export const COLLATERAL_PRICE = BigNumber.from(1);
 export const BORROW_PRICE = BigNumber.from(100);
