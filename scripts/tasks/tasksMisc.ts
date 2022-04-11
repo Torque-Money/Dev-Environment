508ea182cbdea548940fb6aea75616d3b8cff112
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-11 20:04:59 +1000

    🎉 Update

--- scripts/tasks/tasksMisc.ts
+++
@@ -1,7 +0,0 @@
-import {task} from "hardhat/config";
-
-import {verifyAll} from "../utils/deployment/utilVerify";
-
-export default function main() {
-    task("verify-all", "Verify all contracts on block explorer", async (args, hre) => await verifyAll(hre));
-}
