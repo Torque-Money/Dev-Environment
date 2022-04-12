c8ce5bf3f15915e872c1bbf754579376e2cb96ae
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-12 15:02:28 +1000

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,11 +16,11 @@ export default {
         tests: "src/test/js",
     },
     networks: {
-        // opera: {
-        //     chainId: 250,
-        //     url: process.env.NETWORK_URL_OPERA,
-        //     accounts: [process.env.PRIVATE_KEY_OPERA],
-        // },
+        opera: {
+            chainId: 250,
+            url: process.env.NETWORK_URL_OPERA,
+            accounts: [process.env.PRIVATE_KEY_OPERA],
+        },
     },
     etherscan: {
         apiKey: {
