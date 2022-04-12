6f6bb251eca504599219595f09f2d31fa2570399
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-12 12:09:50 +1000

    🎉 Update

--- src/test/sol/TorqueTAU.t.sol
+++ src/test/sol/TorqueTAU.t.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
+import {CheatCodes} from "./CheatCodes.t.sol";
 
 import {TorqueTAU} from "contracts/TorqueTAU/TorqueTAU.sol";
 
 contract TorqueTAUTest is DSTest {
+    CheatCodes cheats;
     TorqueTAU tau;
 
     function setUp() public {
+        cheats = CheatCodes(HEVM_ADDRESS);
         tau = new TorqueTAU();
     }
 
