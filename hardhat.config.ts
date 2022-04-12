666c3add520107d29bf1038ff5d1390e08f0ea81
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-12 10:12:41 +1000

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,6 @@ import { task, HardhatUserConfig } from "hardhat/config";
 require("dotenv").config();
 
 export default {
-    solidity: "0.8.10",
     paths: {
         sources: "src/contracts",
         tests: "src/test/js",
