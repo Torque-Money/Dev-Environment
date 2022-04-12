c3512d7359dbeba9c8d383b875ce5b4a84098fae
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-12 12:19:12 +1000

    🎉 Update

--- src/test/sol/TorqueTAU.t.sol
+++ src/test/sol/TorqueTAU.t.sol
@@ -10,12 +10,24 @@ contract TorqueTAUTest is DSTest {
     CheatCodes cheats;
     TorqueTAU tau;
 
+    uint256 constant INITIAL_SUPPLY = 10000 * 1e18;
+
     function setUp() public {
         cheats = CheatCodes(HEVM_ADDRESS);
         tau = new TorqueTAU();
+        tau.initialize(INITIAL_SUPPLY);
     }
 
-    function testMint() public {
-        cheats.expectRevert();
+    // Check the initial supply minted
+    function testInitialSupply() public {
+        assertEq(tau.totalSupply(), INITIAL_SUPPLY, "Total supply does not match initially supply minted");
+        assertEq(tau.balanceOf(msg.sender), INITIAL_SUPPLY, "Balance of minter does not match supply minted");
     }
+
+    // Mint with 
+    function testMintNoRole() public {
+    }
+
+    // Mint with roles
+    function testBurnNoRole() public {}
 }
