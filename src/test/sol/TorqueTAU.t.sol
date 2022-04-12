04368df5f7c655de2d00383312923d381b16e70f
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-12 12:31:41 +1000

    🎉 Update

--- src/test/sol/TorqueTAU.t.sol
+++ src/test/sol/TorqueTAU.t.sol
@@ -2,21 +2,17 @@
 pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
-import {CheatCodes} from "./CheatCodes.t.sol";
 
 import {TorqueTAU} from "contracts/TorqueTAU/TorqueTAU.sol";
 
 contract TorqueTAUTest is DSTest {
-    CheatCodes cheats;
     TorqueTAU tau;
 
     uint256 constant INITIAL_SUPPLY = 10000 * 1e18;
 
     uint256 constant MINT_AMOUNT = 10 * 1e18;
-    uint256 constant BURN_AMOUNT = 10 * 1e18;
 
     function setUp() public {
-        cheats = CheatCodes(HEVM_ADDRESS);
         tau = new TorqueTAU();
         tau.initialize(INITIAL_SUPPLY);
     }
@@ -27,23 +23,21 @@ contract TorqueTAUTest is DSTest {
         assertEq(tau.balanceOf(address(this)), INITIAL_SUPPLY, "Balance of minter does not match supply minted");
     }
 
-    // Fail to mint tokens
-    function testFailMint() public {
+    // Fail to mint and burn tokens
+    function testFailMintBurn() public {
         tau.mint(address(this), MINT_AMOUNT);
+        tau.burn(address(this), MINT_AMOUNT);
     }
 
-    // Fail to burn tokens
-    function testFailBurn() public {
-        tau.burn(address(this), BURN_AMOUNT);
-    }
+    // Mint and burn tokens
+    function testMintBurn() public {
+        tau.grantRole(tau.TOKEN_MINTER_ROLE(), address(this));
+        tau.grantRole(tau.TOKEN_BURNER_ROLE(), address(this));
 
-    // Mint tokens
-    function testMint() public {
         tau.mint(address(this), MINT_AMOUNT);
-    }
+        tau.burn(address(this), MINT_AMOUNT);
 
-    // Burn tokens
-    function testBurn() public {
-        tau.burn(address(this), BURN_AMOUNT);
+        tau.revokeRole(tau.TOKEN_BURNER_ROLE(), address(this));
+        tau.revokeRole(tau.TOKEN_MINTER_ROLE(), address(this));
     }
 }
