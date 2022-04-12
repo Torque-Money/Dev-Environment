c4edad87c77fdcad065c5dd7477fbeec58165bd6
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-12 10:12:24 +1000

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,19 +2,21 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+import { task, HardhatUserConfig } from "hardhat/config";
+
 require("dotenv").config();
 
-module.exports = {
+export default {
     solidity: "0.8.10",
     paths: {
         sources: "src/contracts",
         tests: "src/test/js",
     },
     networks: {
-        mainnet: {
+        opera: {
             chainId: 250,
-            url: NETWORK_URL,
-            accounts: [process.env.PRIVATE_KEY],
+            url: process.env.NETWORK_URL_OPERA,
+            accounts: [process.env.PRIVATE_KEY_OPERA],
         },
     },
-};
+} as HardhatUserConfig;
