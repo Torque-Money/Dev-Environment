f899c3adf719151753a9f4b3cc7543fb5842fc67
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-12 14:06:53 +1000

    🎉 Update

--- src/scripts/TAU.deploy.ts
+++ src/scripts/TAU.deploy.ts
@@ -13,8 +13,9 @@ async function main() {
     const tau = (await hre.upgrades.deployProxy(TAU, [data.config.TAUInitialSupply])) as TorqueTAU;
     await tau.deployed();
 
-    data.contracts.TAU = tau.address;
+    // **** Just do a few more tests to make sure that the token works before attempting to deploy to mainnet
 
+    data.contracts.TAU = tau.address;
     saveData(data);
 }
 
