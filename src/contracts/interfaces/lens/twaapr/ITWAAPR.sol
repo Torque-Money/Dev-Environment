c046c8b40e6adefbc160c299a6576d15074c3c39
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 08:55:10 +1000

    🎉 Update

--- src/contracts/interfaces/lens/twaapr/ITWAAPR.sol
+++ src/contracts/interfaces/lens/twaapr/ITWAAPR.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import {IRegistry} from "../../utils/IRegistry.sol";
 
+import "../strategy/IStrategy.sol";
+
 // Provides an interface for a contract to support the TWAAPR (time weighted average APR)
 interface ITWAAPR is IRegistry {
     // Check if the oracle can be updated
@@ -14,9 +16,9 @@ interface ITWAAPR is IRegistry {
 
     // Get the cumulative time weighted APR for a given strategy.
     // Reverts if strategy is invalid.
-    function cumulative(address strategy) external view returns (uint256 cumulativeAPR);
+    function cumulative(IStrategy strategy) external view returns (uint256 cumulativeAPR);
 
     // Get the APR for a given strategy.
     // Reverts if strategy is invalid.
-    function consult(address strategy) external view returns (uint256 apr);
+    function consult(IStrategy strategy) external view returns (uint256 apr);
 }
