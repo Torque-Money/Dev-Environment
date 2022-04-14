250160d3ef6d216e543b3888e90fa3ef6811151b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-14 21:59:27 +1000

    🎉 Update

--- src/contracts/interfaces/lens/strategy-manager/IStrategyManagerCore.sol
+++ src/contracts/interfaces/lens/strategy-manager/IStrategyManagerCore.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.0;
 
 // Provides an interface for a contract to support the strategy manager core
 interface IStrategyManagerCore {
-    // Deposit given amount of assets from the vault to the strategy
+    // Deposit given amount of assets from the vault to the strategy.
+    // Reverts if there are not enough funds.
     function deposit(uint256[] calldata amount) external;
 
-    // Deposit all assets from the vault to the strategy
+    // Deposit all assets from the vault to the strategy.
     function depositAll() external;
 
-    // Withdraw given amount of assets from the strategy to the vault
+    // Withdraw given amount of assets from the strategy to the vault.
+    // Reverts if there are not enough funds.
     function withdraw(uint256[] calldata amount) external;
 
     // Withdraw all assets from the strategy to the vault
---
+++ src/contracts/interfaces/lens/strategy/IStrategy.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+// Provides an interface for a contract to support a strategy
+interface IStrategy {
+    // Deposit a given amount of funds into the strategy
+}
