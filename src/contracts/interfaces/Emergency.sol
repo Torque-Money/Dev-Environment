af4b55072eede486d6e01b873ca3c732990ea903
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-14 20:15:06 +1000

    🎉 Update

--- src/contracts/interfaces/Emergency.sol
+++ src/contracts/interfaces/Emergency.sol
@@ -5,5 +5,8 @@ pragma solidity ^0.8.0;
  *  Provides a contract with the option to eject its funds in the event that they get locked within
  */
 interface Emergency {
+    /**
+     *  Allow a contract to eject tokens locked within
+     */
     function inCaseTokensGetStuck(address token, uint256 amount) external;
 }
