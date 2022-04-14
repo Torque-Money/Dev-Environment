1b201b26f0fad0815ed5c5a1bcc73898234b151e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-14 21:16:05 +1000

    🎉 Update

--- src/contracts/interfaces/lens/ITorqueVaultV1.sol
+++ src/contracts/interfaces/lens/ITorqueVaultV1.sol
@@ -9,27 +9,16 @@ interface ITorqueVaultV1 is IERC20Upgradeable {
     function tokenCount() external view returns (uint256 _tokenCount);
 
     // Gets a token supported by the vault by its index. Must be less than token count or else will revert
-    function getTokenByIndex(uint256 index)
-        external
-        view
-        returns (address token);
+    function getTokenByIndex(uint256 index) external view returns (address token);
 
     // Previews the amount of shares for depositing a given amount of tokens into the vault.
-    function previewDeposit(uint256[] calldata amount)
-        external
-        view
-        returns (uint256 shares);
+    function previewDeposit(uint256[] calldata amount) external view returns (uint256 shares);
 
     // Deposit vault supported tokens into the vault for shares. It is important that the tokens supported match the correct ratios or else additional funds deposited will be lost. Reverts if funds are not available.
-    function deposit(uint256[] calldata amount)
-        external
-        returns (uint256 shares);
+    function deposit(uint256[] calldata amount) external returns (uint256 shares);
 
     // Previews the amount of tokens for redeeming a given amount of shares
-    function previewRedeem(uint256 shares)
-        external
-        view
-        returns (uint256[] calldata asset);
+    function previewRedeem(uint256 shares) external view returns (uint256[] calldata asset);
 
     //
 }
