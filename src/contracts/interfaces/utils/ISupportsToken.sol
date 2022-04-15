ed973da5947ed1dcff3edd13d653b0a383dd06e0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 20:22:38 +1000

    🎉 Update

---
+++ src/contracts/interfaces/utils/ISupportsToken.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Used for checking what tokens a contract supports and in what order.
+interface ISupportsToken {
+    // Returns the amount of the given asset owned by the contract.
+    // Reverts if the token is not supported.
+    function balance(IERC20 token) external returns (uint256 amount);
+
+    // Returns if a token is supported by the contract.
+    function isSupportedToken(address token) external returns (bool supportedToken);
+
+    // Returns the number of tokens the contract supports.
+    function tokenCount() external view returns (uint256 count);
+
+    // Gets a token supported by the contract by its index.
+    // Reverts if the index is not less than the token count.
+    function tokenByIndex(uint256 index) external view returns (IERC20 token);
+}
