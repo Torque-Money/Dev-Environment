5507b3e2f5e468f0e55737a0d794dcdddd2944df
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 10:48:23 +1000

    🎉 Update

--- src/contracts/interfaces/utils/IEmergency.sol
+++ src/contracts/interfaces/utils/IEmergency.sol
@@ -6,6 +6,5 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // Provides an interface for a contract to eject its funds in the event that they get locked within.
 interface IEmergency {
     // Allow a contract to eject tokens locked within.
-    // DANGEROUS - Should be overriden for each smart contract to add authorization.
     function inCaseTokensGetStuck(IERC20 token, uint256 amount) external;
 }
