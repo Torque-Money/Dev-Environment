f899ea3b7595aa88e9d09106a61da0edb336e558
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 22:01:42 +1000

    🎉 Update

--- src/contracts/lens/strategy/BeefyLPStrategy.sol
+++ src/contracts/lens/strategy/BeefyLPStrategy.sol
@@ -1,4 +1,9 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-contract BeefyLPStrategy {}
+// This strategy will take two tokens and will deposit them into the correct LP pair for the given pool.
+// It will then take the LP token and deposit it into a Beefy vault.
+
+contract BeefyLPStrategy {
+
+}
