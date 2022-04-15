0aeaf77f7501b272635d00edf73313760bc1c034
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 22:45:25 +1000

    🎉 Update

--- src/contracts/lens/strategy/BeefyLPStrategy.sol
+++ src/contracts/lens/strategy/BeefyLPStrategy.sol
@@ -1,9 +1,15 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import {IVault} from "@beefy/contracts/BIFI/interfaces/beefy/IVault.sol";
+
+import {IStrategy} from "../../interfaces/lens/strategy/IStrategy.sol";
+
+import {SupportsToken} from "../../utils/SupportsToken.sol";
+
 // This strategy will take two tokens and will deposit them into the correct LP pair for the given pool.
 // It will then take the LP token and deposit it into a Beefy vault.
 
-contract BeefyLPStrategy {
+contract BeefyLPStrategy is IStrategy, SupportsToken {
 
 }
