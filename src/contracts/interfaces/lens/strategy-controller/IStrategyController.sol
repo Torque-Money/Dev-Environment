1d185e91e77e6ed078823f033c6b01adc3414157
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 14:15:53 +1000

    🎉 Update

--- src/contracts/interfaces/lens/strategy-controller/IStrategyController.sol
+++ src/contracts/interfaces/lens/strategy-controller/IStrategyController.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.0;
 
 // Provides an interface for a contract to support the strategy controller.
 interface IStrategyController {
-    // Check if the strategy is updateable.
+    // Check if the strategy can be updated.
     function isUpdateable() external view returns (bool updateable);
 
     // Update the strategy.
+    // Reverts if the strategy is not currently upgradeable.
     function update() external;
 
     event Update(address indexed caller);
