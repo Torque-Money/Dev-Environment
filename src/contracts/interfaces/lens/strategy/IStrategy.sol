5b56ab2468ea30b06067f35210bcbbad28b69d45
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 21:15:58 +1000

    🎉 Update

--- src/contracts/interfaces/lens/strategy/IStrategy.sol
+++ src/contracts/interfaces/lens/strategy/IStrategy.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import {ISupportsToken} from "../../utils/ISupportsToken.sol";
+
 // A strategy that integrates with a vault to earn rewards on deposited tokens.
-interface IStrategy {
+interface IStrategy is ISupportsToken {
     // Deposit a given amount of funds into the strategy.
     // Reverts if sender does not have appropriate funds or has not allocated allowance.
     function deposit(uint256[] calldata amount) external;
@@ -13,9 +15,9 @@ interface IStrategy {
     // Reverts if there are not enough funds available in the contract.
     function withdraw(uint256[] calldata amount) external;
 
-    // Get the balance of funds available to be withdrawn from the strategy.
-    function balance(IERC20 token) external view returns (uint256 amount);
+    // Get the current APY for the strategy.
+    function APY() external returns (uint256 apy, uint256 decimals);
 
-    event Deposit(address indexed caller, uint256[] amount);
-    event Withdraw(address indexed caller, uint256[] amount);
+    // Update the current APY for the strategy.
+    function updateAPY() external;
 }
