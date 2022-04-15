49f353c0f0d9ee44e8a50655dfcc1b048dad82f7
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 14:37:49 +1000

    🎉 Update

--- src/contracts/utils/Registry.sol
+++ src/contracts/utils/Registry.sol
@@ -1,14 +1,30 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 import {IRegistry} from "../interfaces/utils/IRegistry.sol";
 
-contract Registry is IRegistry {
+contract Registry is IRegistry, Initializable, AccessControlUpgradeable {
     using EnumerableSet for EnumerableSet.AddressSet;
 
+    bytes32 public REGISTRY_ADMIN_ROLE;
+
     EnumerableSet.AddressSet private _set;
+
+    function __Registry_init() internal onlyInitializing {
+        __AccessControl_init();
+
+        __Registry_init_unchained();
+    }
+
+    function __Registry_init_unchained() internal onlyInitializing {
+        REGISTRY_ADMIN_ROLE = keccak256("REGISTRY_ADMIN_ROLE");
+        _setRoleAdmin(REGISTRY_ADMIN_ROLE, REGISTRY_ADMIN_ROLE);
+        _grantRole(REGISTRY_ADMIN_ROLE, _msgSender());
+    }
 
     function add(address entry) external override {
         require(_set.add(entry), "Registry: Cannot add element to registry");
