74c1559f34a10ee55c4212f096cbf3d3368c9f10
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-15 21:18:41 +1000

    🎉 Update

--- src/contracts/interfaces/lens/strategy-controller/IStrategyController.sol
+++ src/contracts/interfaces/lens/strategy-controller/IStrategyController.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8.0;
 // Controls a vault and what strategy it should implement.
 interface IStrategyController {
     // Check if the strategy can be updated.
-    function isUpdateable() external view returns (bool updateable);
+    function isStrategyUpdateable() external view returns (bool updateable);
 
     // Update the strategy.
-    // Reverts if the strategy is not currently upgradeable.
-    function update() external;
+    // Reverts if the strategy is not upgradeable.
+    function updateStrategy() external;
 
-    event Update(address indexed caller);
+    // Check if the oracle can be updated.
+    function isOracleUpdateable() external view returns (bool updateable);
+
+    // Update the oracle.
+    // Reverts if the oracle is not upgradeable.
+    function updateOracle() external;
+
+    event UpdateStrategy(address indexed caller);
+    event UpdateOracle(address indexed caller);
 }
