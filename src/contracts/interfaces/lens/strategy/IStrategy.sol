5136d0e8a42fb193c9e1c1392212ab3d0e06eb61
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-17 07:48:34 +1000

    🎉 Update

--- src/contracts/interfaces/lens/strategy/IStrategy.sol
+++ src/contracts/interfaces/lens/strategy/IStrategy.sol
@@ -5,22 +5,26 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {ISupportsToken} from "../../utils/ISupportsToken.sol";
 
+import {IVaultV1} from "../vault/IVaultV1.sol";
+
 // A strategy that integrates with a vault to earn rewards on deposited tokens.
-// Each strategy should only belong to one vault / controller at once otherwise vault funds distributed to wrong vaults.
 interface IStrategy is ISupportsToken {
-    // Deposit a given amount of funds from the sender into the strategy.
+    // Get the vault used by the strategy.
+    function getVault() external returns (IVaultV1 vault);
+
+    // Deposit a given amount of funds from the vault into the strategy.
     // Reverts if sender does not have appropriate funds or has not allocated allowance.
     function deposit(uint256[] calldata amount) external;
 
-    // Deposit all of the senders funds into the strategy.
+    // Deposit all of the vaults funds into the strategy.
     // Reverts if sender has not approved funds.
     function depositAll() external;
 
-    // Withdraw a given amount of the contracts funds to the sender.
+    // Withdraw a given amount of the contracts funds to the vault.
     // Reverts if there are not enough funds available in the contract.
     function withdraw(uint256[] calldata amount) external;
 
-    // Withdraw all of the contracts funds to the sender.
+    // Withdraw all of the contracts funds to the vault.
     function withdrawAll() external;
 
     // Get the current APY and decimals for the strategy.
@@ -29,8 +33,6 @@ interface IStrategy is ISupportsToken {
     // Update the current APY for the strategy.
     // Calling APY after does not necessarily need to equal the submitted APY.
     function updateAPY(uint256 apy) external;
-
-    // **** There needs to be some sort of interface for injecting and ejecting the funds from the strategy in the vault ?
 
     event Deposit(address indexed caller, uint256[] amount);
     event DepositAll(address indexed caller);
