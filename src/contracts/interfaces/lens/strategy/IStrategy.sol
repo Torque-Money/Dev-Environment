17db13b23a4606a0cead7c6f43e3bd746760b418
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-17 07:55:28 +1000

    🎉 Update

--- src/contracts/interfaces/lens/strategy/IStrategy.sol
+++ src/contracts/interfaces/lens/strategy/IStrategy.sol
@@ -10,19 +10,19 @@ import {IVaultV1} from "../vault/IVaultV1.sol";
 // A strategy that integrates with a vault to earn rewards on deposited tokens.
 // Strategy should only ever integrate with a single vault.
 interface IStrategy is ISupportsToken {
-    // Deposit a given amount of funds from the vault into the strategy.
+    // Deposit a given amount of funds from the caller into the strategy.
     // Reverts if sender does not have appropriate funds or has not allocated allowance.
     function deposit(uint256[] calldata amount) external;
 
-    // Deposit all of the vaults funds into the strategy.
+    // Deposit all of the callers funds into the strategy.
     // Reverts if sender has not approved funds.
     function depositAll() external;
 
-    // Withdraw a given amount of the contracts funds to the vault.
+    // Withdraw a given amount of the contracts funds to the caller.
     // Reverts if there are not enough funds available in the contract.
     function withdraw(uint256[] calldata amount) external;
 
-    // Withdraw all of the contracts funds to the vault.
+    // Withdraw all of the contracts funds to the caller.
     function withdrawAll() external;
 
     // Get the current APY and decimals for the strategy.
