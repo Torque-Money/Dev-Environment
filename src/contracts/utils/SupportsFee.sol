1a9a89d14f42f062ed80ead11924aa7b843badb2
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-17 12:30:23 +1000

    🎉 Update

--- src/contracts/utils/SupportsFee.sol
+++ src/contracts/utils/SupportsFee.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.0;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 import {ISupportsFee} from "../interfaces/utils/ISupportsFee.sol";
 
-abstract contract SupportsFee is Initializable, ISupportsFee {
+abstract contract SupportsFee is Initializable, AccessControlUpgradeable, ISupportsFee {
+    bytes32 public FEE_ADMIN_ROLE;
+
     address private recipient;
 
     function __SupportsFee_init(address _recipient) internal onlyInitializing {
@@ -13,11 +16,15 @@ abstract contract SupportsFee is Initializable, ISupportsFee {
     }
 
     function __SupportsFee_init_unchained(address _recipient) internal onlyInitializing {
+        FEE_ADMIN_ROLE = keccak256("FEE_ADMIN_ROLE");
+        _setRoleAdmin(FEE_ADMIN_ROLE, FEE_ADMIN_ROLE);
+        _grantRole(FEE_ADMIN_ROLE, _msgSender());
+
         recipient = _recipient;
     }
 
     // Set the fee recipient.
-    function setFeeRecipient(address _recipient) external virtual override {
+    function setFeeRecipient(address _recipient) external virtual override onlyRole(FEE_ADMIN_ROLE) {
         recipient = _recipient;
     }
 
