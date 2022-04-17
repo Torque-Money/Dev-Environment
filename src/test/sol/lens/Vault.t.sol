babda7a75b4bdf00dc0f7d33dfb3188abd15b6e9
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-17 14:03:15 +1000

    🎉 Update

--- src/test/sol/lens/Vault.t.sol
+++ src/test/sol/lens/Vault.t.sol
@@ -4,16 +4,22 @@ pragma solidity ^0.8.0;
 import {DSTest} from "ds-test/test.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {MockStrategy} from "@contracts/mocks/MockStrategy.sol";
+import {MockStrategy} from "../../mocks/MockStrategy.sol";
+import {TorqueVaultV1} from "@contracts/lens/vault/TorqueVaultV1.sol";
 
-import {Config} from "../Config.sol";
+import {Config} from "../helpers/Config.sol";
 
 contract VaultTest is DSTest {
-    MockStrategy private strategy;
     IERC20[] private token;
+
+    TorqueVaultV1 private vault;
+    MockStrategy private strategy;
 
     function setUp() external {
         token = Config.getTokens();
+
+        vault = new TorqueVaultV1();
+        // vault.initialize(token, );
 
         strategy = new MockStrategy();
         strategy.initialize(token, Config.getInitialAPY());
