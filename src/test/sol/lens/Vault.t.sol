e5dac9aea0cf742d3c253621a954104a0dcd28b6
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-17 13:06:25 +1000

    🎉 Update

--- src/test/sol/lens/Vault.t.sol
+++ src/test/sol/lens/Vault.t.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "ds-test/test.sol";
+import {DSTest} from "ds-test/test.sol";
 
 contract VaultTest is DSTest {
     function setUp() public {
--- src/test/sol/tau/TorqueTAU.t.sol
+++ src/test/sol/tau/TorqueTAU.t.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "ds-test/test.sol";
+import {DSTest} from "ds-test/test.sol";
 
 import {TorqueTAU} from "@contracts/tau/TorqueTAU.sol";
 
