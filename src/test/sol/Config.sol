1d388393593a59868e0e8fde8947d2f0814fa3b5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-17 13:24:59 +1000

    🎉 Update

--- src/test/sol/Config.sol
+++ src/test/sol/Config.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 library Config {
-    function getTokens() internal returns (IERC20[] memory tokens) {
-        IERC20[] memory tokens = new IERC20[](2);
+    function getTokens() internal pure returns (IERC20[] memory tokens) {
+        tokens = new IERC20[](2);
         tokens[0] = IERC20(0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83); // wFTM
         tokens[1] = IERC20(0x04068DA6C83AFCFA0e13ba15A6696662335D5B75); // USDC
     }
 
-    function getInitialAPY() internal returns (uint256 apy) {
-        return 20;
+    function getInitialAPY() internal pure returns (uint256 apy) {
+        apy = 20;
     }
 }
