fdb73a5de8cc1453576325729e06572d93819fa4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-18 16:49:16 +1000

    🎉 Update

--- src/test/sol/lens/strategy/APY.t.sol
+++ src/test/sol/lens/strategy/APY.t.sol
@@ -1,9 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import {StrategyBase} from "./StrategyBase.sol";
 
@@ -12,7 +10,6 @@ import {BeefyLPStrategy} from "@contracts/lens/strategy/BeefyLPStrategy.sol";
 
 contract DepositWithdrawTest is StrategyBase {
     using SafeMath for uint256;
-    using SafeERC20 for IERC20;
 
     BeefyLPStrategy private strategy;
 
@@ -25,7 +22,7 @@ contract DepositWithdrawTest is StrategyBase {
     function testUpdateAPY() public {
         (uint256 initialAPY, uint256 decimals) = strategy.APY();
 
-        uint256 newAPY = (Config.getInitialAPY() + 100) * 10**decimals;
+        uint256 newAPY = Config.getInitialAPY().add(100).mul(10**decimals);
 
         strategy.updateAPY(newAPY);
 
