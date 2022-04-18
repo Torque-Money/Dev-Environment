589f9c46f35c432c36e2e35490c5e64b90d60f62
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-18 10:06:51 +1000

    🎉 Update

--- src/test/sol/lens/vault/Authorize.t.sol
+++ src/test/sol/lens/vault/Authorize.t.sol
@@ -5,8 +5,43 @@ import {ICheatCodes} from "../../helpers/ICheatCodes.sol";
 
 import {VaultBase} from "./VaultBase.sol";
 
+import {Config} from "../../helpers/Config.sol";
 import {Empty} from "../../helpers/Empty.sol";
 import {MockStrategy} from "../../../mocks/MockStrategy.sol";
 import {TorqueVaultV1} from "@contracts/lens/vault/TorqueVaultV1.sol";
 
-contract AuthorizeTest is VaultBase {}
+contract AuthorizeTest is VaultBase {
+    TorqueVaultV1 vault;
+    Empty empty;
+    ICheatCodes cheats;
+
+    function setUp() public override {
+        super.setUp();
+
+        vault = _getVault();
+        empty = _getEmpty();
+        cheats = Config.getCheatCodes();
+    }
+
+    modifier impersonate() {
+        cheats.startPrank(address(empty));
+        _;
+        cheats.stopPrank();
+    }
+
+    function testFailInjectFunds() public impersonate {
+        vault.depositIntoStrategy(Config.getTokenAmount());
+    }
+
+    function testFailInjectAllFunds() public impersonate {
+        vault.depositAllIntoStrategy();
+    }
+
+    function testFailEjectFunds() public impersonate {
+        vault.withdrawFromStrategy(Config.getTokenAmount());
+    }
+
+    function testFailEjectAllFunds() public impersonate {
+        vault.withdrawAllFromStrategy();
+    }
+}
