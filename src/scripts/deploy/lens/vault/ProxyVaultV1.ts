077c49a57b8479eb50045bf02bfb996bee64a848
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-18 22:00:50 +1000

    🎉 Update

--- src/scripts/deploy/lens/vault/ProxyVaultV1.ts
+++ src/scripts/deploy/lens/vault/ProxyVaultV1.ts
@@ -1,6 +1,23 @@
 import hre from "hardhat";
+import { loadData } from "../../../utils/data";
 
 async function main() {
+    const data = loadData();
+
+    const beacon = "";
+
+    // wFTM, USDC
+    const tokens = ["0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83", "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75"];
+    const strategy = "";
+    const feeRecipient = data.contracts.timelock;
+    const feePercent = 0;
+    const feeDenominator = 1;
+
+    const VaultV1 = await hre.ethers.getContractFactory("TorqueVaultV1");
+    const vaultV1 = await hre.upgrades.deployBeaconProxy(beacon, VaultV1, [tokens, strategy, feeRecipient, feePercent, feeDenominator]);
+    await vaultV1.deployed();
+
+    console.log("VaultV1 proxy:", vaultV1.address);
 }
 
 main()
