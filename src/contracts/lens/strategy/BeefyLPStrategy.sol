ae32f1e3c42b3ea8ec33299ad98b2be24da2daf1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-18 12:27:19 +1000

    🎉 Update

--- src/contracts/lens/strategy/BeefyLPStrategy.sol
+++ src/contracts/lens/strategy/BeefyLPStrategy.sol
@@ -20,8 +20,6 @@ import {Emergency} from "../../utils/Emergency.sol";
 // This strategy will take two tokens and will deposit them into the correct LP pair for the given pool.
 // It will then take the LP token and deposit it into a Beefy vault.
 
-// **** MAKE SURE TO REMOVE THE DS-TEST ONCE DONE
-
 contract BeefyLPStrategy is Initializable, AccessControlUpgradeable, IStrategy, SupportsToken, Emergency {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
