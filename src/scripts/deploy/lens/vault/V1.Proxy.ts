4604893602c6825adefb2f4f4737308a1ca72d74
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-19 14:48:08 +1000

    🎉 Update

--- src/scripts/deploy/lens/vault/V1.Proxy.ts
+++ src/scripts/deploy/lens/vault/V1.Proxy.ts
@@ -9,7 +9,7 @@ async function main() {
 
     // wFTM, USDC
     const tokens = ["0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83", "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75"];
-    const strategy = "";
+    const strategy = "0x6Ad07E659563490d40377a98a7f0f62ed7d38C41";
     const feeRecipient = data.contracts.timelock;
     const feePercent = 0;
     const feeDenominator = 100;
