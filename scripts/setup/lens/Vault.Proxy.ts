8c4f97c9c59f4871090017c57b2fb319984bdec2
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-02 21:47:59 +1000

    🎉 Update

---
+++ scripts/setup/lens/Vault.Proxy.ts
@@ -0,0 +1,21 @@
+import hre from "hardhat";
+
+async function main() {
+    const BeefyLPStrategy = await hre.ethers.getContractFactory("BeefyLPStrategy");
+    const beacon = await hre.upgrades.deployBeacon(BeefyLPStrategy);
+    await beacon.deployed();
+
+    // **** First of all we need to get the strategy 
+
+    console.log("BeefyLPStrategy beacon:", beacon.address);
+    console.log("BeefyLPStrategy implementation:", await hre.upgrades.beacon.getImplementationAddress(beacon.address));
+
+    const beacon = 
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
