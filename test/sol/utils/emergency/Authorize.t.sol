190d94c63ea05c5906f0089e39652c3f98a0cc18
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-03 13:54:39 +1000

    🎉 Update

--- test/sol/utils/emergency/Authorize.t.sol
+++ test/sol/utils/emergency/Authorize.t.sol
@@ -4,25 +4,27 @@ pragma solidity ^0.8.0;
 import {ICheatCodes} from "../../helpers/ICheatCodes.sol";
 
 import {EmergencyBase} from "./EmergencyBase.sol";
+import {Impersonate} from "../../helpers/Impersonate.sol";
 
 import {Config} from "../../helpers/Config.sol";
 import {MockEmergency} from "../../../mocks/MockEmergency.sol";
 
-contract Withdraw is EmergencyBase {
+contract Authorize is EmergencyBase, Impersonate {
     MockEmergency private emergency;
+    address private empty;
 
     function setUp() public virtual override {
         super.setUp();
 
         emergency = _getEmergency();
+        empty = _getEmpty();
     }
 
-    // Test that an ERC20 token is withdrawable
-    // **** This probably requires the funding to supply the account with ETH before this occurs
+    // Check that an approved account will be able to use emergency withdraw
 
-    // Test that ETH is withdrawable
+    // Check that a non approved account will not be able to use an emergency withdraw
 
-    function _getCheats() internal view virtual override returns (ICheatCodes _cheats) {
+    function _getCheats() internal view virtual override(EmergencyBase, Impersonate) returns (ICheatCodes _cheats) {
         return super._getCheats();
     }
 }
