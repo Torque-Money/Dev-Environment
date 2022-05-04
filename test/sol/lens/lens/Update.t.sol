36be4a123c9d08bf9ecc7e307ca396d089ca3a0a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-05 09:07:18 +1000

    🎉 Update

--- test/sol/lens/lens/Update.t.sol
+++ test/sol/lens/lens/Update.t.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 
 import {BaseLens} from "./BaseLens.sol";
 
-contract UpdateTest is BaseLens {}
+contract UpdateTest is BaseLens {
+    // **** We first need to test that the strategy can handle movements over the different vaults and ensure the tokens are still intact
+}
