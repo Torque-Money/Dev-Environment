4a47ac51312544564bc566d7e47dffea56571891
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-04 19:41:42 +1000

    🎉 Update

---
+++ test/mocks/MockSupportsToken.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+import {SupportsTokenUpgradeable} from "../../src/utils/SupportsTokenUpgradeable.sol";
+
+contract MockSupportsFee is Initializable, SupportsTokenUpgradeable {
+    constructor(IERC20Upgradeable[] memory token) {
+        _initialize(token);
+    }
+
+    function _initialize(IERC20Upgradeable[] memory token) internal initializer {
+        __SupportsToken_init(token);
+    }
+}
