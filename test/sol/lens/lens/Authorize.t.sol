22bb9735375e0ae2d6e4672945244bd739e77ed9
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-05 09:01:35 +1000

    🎉 Update

--- test/sol/lens/lens/Authorize.t.sol
+++ test/sol/lens/lens/Authorize.t.sol
@@ -5,20 +5,13 @@ import {BaseLens} from "./BaseLens.sol";
 import {BaseImpersonate} from "../../bases/BaseImpersonate.sol";
 
 contract AuthorizeTest is BaseLens, BaseImpersonate {
-    // // Fail to deposit into the strategy due to lack of authorization.
-    // function testFailDeposit() public impersonate(vm, _empty) {
-    //     _strategy.deposit(_tokenAmount);
-    // }
-    // // Fail to deposit all into the strategy due to lack of authorization.
-    // function testFailDepositAll() public impersonate(vm, _empty) {
-    //     _strategy.depositAll();
-    // }
-    // // Fail to withdraw from the strategy due to lack of authorization.
-    // function testFailWithdraw() public impersonate(vm, _empty) {
-    //     _strategy.withdraw(_tokenAmount);
-    // }
-    // // Fail to withdraw all from the strategy due to lack of authorization.
-    // function testFailWithdrawAll() public impersonate(vm, _empty) {
-    //     _strategy.withdrawAll();
-    // }
+    // Should set the vault
+    function testSetVault() public {
+        _lens.setVault(_vault);
+    }
+
+    // Should fail to set the vault due to lack of authorization
+    function testFailUnauthorizedSetVault() public impersonate(vm, _empty) {
+        _lens.setVault(_vault);
+    }
 }
