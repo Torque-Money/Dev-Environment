f5133fb96dfbc4f7ecd198c94a37c28c6f315693
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-05 23:00:01 +1000

    🎉 Update

--- scripts/deploy/lens/BeefyLPStrategy.Proxy.ts
+++ scripts/deploy/lens/BeefyLPStrategy.Proxy.ts
@@ -10,9 +10,9 @@ async function main() {
     // wFTM, USDC
     const tokens = ["0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83", "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75"];
     const initialAPY = 18.14 * 10 ** 4;
-    const uniRouter = "0xF491e7B69E4244ad4002BC14e878a34207E38c29";
-    const uniFactory = "0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3";
-    const beVault = "0x41D44B276904561Ac51855159516FD4cB2c90968";
+    const uniRouter = "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52";
+    const uniFactory = "0xEF45d134b73241eDa7703fa787148D9C9F4950b0";
+    const beVault = "0xA4e2EE5a7fF51224c27C98098D8DB5C770bAAdbE";
 
     const BeefyLPStrategy = await hre.ethers.getContractFactory("BeefyLPStrategy");
     const beefyLPStrategy = await hre.upgrades.deployBeaconProxy(beacon, BeefyLPStrategy, [tokens, initialAPY, uniRouter, uniFactory, beVault]);
