3ae99bb3a7a0b03d275ebd4b5224490404d0464d
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-06 23:07:01 +1000

    🎉 Update

--- scripts/setup/lens/BeefyLPStrategy.ts
+++ scripts/setup/lens/BeefyLPStrategy.ts
@@ -14,17 +14,17 @@ async function main() {
     await (await strategy.grantRole(STRATEGY_CONTROLLER_ROLE, vault.address)).wait();
     console.log("Setup | BeefyLPStrategy | Assign vault as strategy controller");
 
-    // Assign the emergency to the timelock (and revoke)
-    const EMERGENCY_ADMIN_ROLE = await strategy.EMERGENCY_ADMIN_ROLE();
-    await (await strategy.grantRole(EMERGENCY_ADMIN_ROLE, data.contracts.timelock)).wait();
-    await (await strategy.renounceRole(EMERGENCY_ADMIN_ROLE, caller)).wait();
-    console.log("Setup | BeefyLPStrategy | Transfer ownership of emergency role");
+    // // Assign the emergency to the timelock (and revoke)
+    // const EMERGENCY_ADMIN_ROLE = await strategy.EMERGENCY_ADMIN_ROLE();
+    // await (await strategy.grantRole(EMERGENCY_ADMIN_ROLE, data.contracts.timelock)).wait();
+    // await (await strategy.renounceRole(EMERGENCY_ADMIN_ROLE, caller)).wait();
+    // console.log("Setup | BeefyLPStrategy | Transfer ownership of emergency role");
 
-    // Assign strategy admin to the timelock (and revoke)
-    const STRATEGY_ADMIN_ROLE = await strategy.STRATEGY_ADMIN_ROLE();
-    await (await strategy.grantRole(STRATEGY_ADMIN_ROLE, data.contracts.timelock)).wait();
-    await (await strategy.renounceRole(STRATEGY_ADMIN_ROLE, caller)).wait();
-    console.log("Setup | BeefyLPStrategy | Transfer ownership of admin role");
+    // // Assign strategy admin to the timelock (and revoke)
+    // const STRATEGY_ADMIN_ROLE = await strategy.STRATEGY_ADMIN_ROLE();
+    // await (await strategy.grantRole(STRATEGY_ADMIN_ROLE, data.contracts.timelock)).wait();
+    // await (await strategy.renounceRole(STRATEGY_ADMIN_ROLE, caller)).wait();
+    // console.log("Setup | BeefyLPStrategy | Transfer ownership of admin role");
 }
 
 main()
