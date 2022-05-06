49b60b9239f761dbba62d7dcf024b7e99d95c173
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-07 09:05:56 +1000

    🎉 Update

--- scripts/setup/lens/Vault.ts
+++ scripts/setup/lens/Vault.ts
@@ -9,24 +9,24 @@ async function main() {
     const vault = await hre.ethers.getContractAt("Vault", data.contracts["VaultV2.0"].proxies[0]);
     const caller = await hre.ethers.provider.getSigner().getAddress();
 
-    const VAULT_CONTROLLER_ROLE = await vault.VAULT_CONTROLLER_ROLE();
-    await (await vault.grantRole(VAULT_CONTROLLER_ROLE, lens)).wait();
-    console.log("Setup | Vault | Assign vault controller to lens");
+    // const VAULT_CONTROLLER_ROLE = await vault.VAULT_CONTROLLER_ROLE();
+    // await (await vault.grantRole(VAULT_CONTROLLER_ROLE, lens)).wait();
+    // console.log("Setup | Vault | Assign vault controller to lens");
 
-    // const FEE_ADMIN_ROLE = await vault.FEE_ADMIN_ROLE();
-    // await (await vault.grantRole(FEE_ADMIN_ROLE, data.contracts.timelock)).wait();
-    // await (await vault.renounceRole(FEE_ADMIN_ROLE, caller)).wait();
-    // console.log("Setup | Vault | Assign fee role");
+    const FEE_ADMIN_ROLE = await vault.FEE_ADMIN_ROLE();
+    await (await vault.grantRole(FEE_ADMIN_ROLE, data.contracts.timelock)).wait();
+    await (await vault.renounceRole(FEE_ADMIN_ROLE, caller)).wait();
+    console.log("Setup | Vault | Assign fee role");
 
-    // const EMERGENCY_ADMIN_ROLE = await vault.EMERGENCY_ADMIN_ROLE();
-    // await (await vault.grantRole(EMERGENCY_ADMIN_ROLE, data.contracts.timelock)).wait();
-    // await (await vault.renounceRole(EMERGENCY_ADMIN_ROLE, caller)).wait();
-    // console.log("Setup | Vault | Emergency role");
+    const EMERGENCY_ADMIN_ROLE = await vault.EMERGENCY_ADMIN_ROLE();
+    await (await vault.grantRole(EMERGENCY_ADMIN_ROLE, data.contracts.timelock)).wait();
+    await (await vault.renounceRole(EMERGENCY_ADMIN_ROLE, caller)).wait();
+    console.log("Setup | Vault | Emergency role");
 
-    // const VAULT_ADMIN_ROLE = await vault.VAULT_ADMIN_ROLE();
-    // await (await vault.grantRole(VAULT_ADMIN_ROLE, data.contracts.timelock)).wait();
-    // await (await vault.renounceRole(VAULT_ADMIN_ROLE, caller)).wait();
-    // console.log("Setup | Vault | Admin role");
+    const VAULT_ADMIN_ROLE = await vault.VAULT_ADMIN_ROLE();
+    await (await vault.grantRole(VAULT_ADMIN_ROLE, data.contracts.timelock)).wait();
+    await (await vault.renounceRole(VAULT_ADMIN_ROLE, caller)).wait();
+    console.log("Setup | Vault | Admin role");
 }
 
 main()
