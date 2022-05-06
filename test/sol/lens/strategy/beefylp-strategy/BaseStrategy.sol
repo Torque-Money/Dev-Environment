68778266bd4f83080a0ef75c0b10f38f4038c3ef
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-06 18:20:06 +1000

    🎉 Update

--- test/sol/lens/strategy/beefylp-strategy/BaseStrategy.sol
+++ test/sol/lens/strategy/beefylp-strategy/BaseStrategy.sol
@@ -1,6 +1,10 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import {IUniswapV2Router02} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import {IUniswapV2Factory} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import {IBeefyVaultV6} from "../../../../../lib/beefy/IBeefyVaultV6.sol";
+
 import {Base} from "../../../bases/Base.sol";
 import {BaseUsesToken} from "../../../bases/BaseUsesToken.sol";
 
@@ -15,7 +19,8 @@ abstract contract BaseStrategy is Base, BaseUsesToken {
         BaseUsesToken.setUp();
 
         _strategy = new BeefyLPStrategy();
-        _strategy.initialize(_token, Config.getUniRouter(), Config.getUniFactory(), Config.getBeefyVault());
+        (IUniswapV2Router02 router, IUniswapV2Factory factory, IBeefyVaultV6 beVault) = Config.getBeefyLPVaultParams();
+        _strategy.initialize(_token, router, factory, beVault);
 
         _strategy.grantRole(_strategy.STRATEGY_CONTROLLER_ROLE(), address(this));
 
