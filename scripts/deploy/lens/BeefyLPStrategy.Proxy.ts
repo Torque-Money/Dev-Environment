0a802a48f2bbbea8be5ab2ead4a0af341bb15e13
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-06 22:56:06 +1000

    🎉 Update

--- scripts/deploy/lens/BeefyLPStrategy.Proxy.ts
+++ scripts/deploy/lens/BeefyLPStrategy.Proxy.ts
@@ -10,15 +10,15 @@ async function main() {
     // wFTM, USDC
     const tokens = ["0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83", "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75"];
 
-    // Beefy USDC-FTM LP SushiSwap
-    const uniRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
-    const uniFactory = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
-    const beVault = "0xb870e4755C737D2753D7298D0e70344077905Ed5";
+    // // Beefy USDC-FTM LP SushiSwap
+    // const uniRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+    // const uniFactory = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
+    // const beVault = "0xb870e4755C737D2753D7298D0e70344077905Ed5";
 
-    // // Beefy USDC-FTM LP SpiritSwap
-    // const uniRouter = "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52";
-    // const uniFactory = "0xEF45d134b73241eDa7703fa787148D9C9F4950b0";
-    // const beVault = "0xA4e2EE5a7fF51224c27C98098D8DB5C770bAAdbE";
+    // Beefy USDC-FTM LP SpiritSwap
+    const uniRouter = "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52";
+    const uniFactory = "0xEF45d134b73241eDa7703fa787148D9C9F4950b0";
+    const beVault = "0xA4e2EE5a7fF51224c27C98098D8DB5C770bAAdbE";
 
     // // Beefy USDC-FTM LP WigoSwap
     // const uniRouter = "0x5023882f4D1EC10544FCB2066abE9C1645E95AA0";
