a36b12c1ef7495c1e12a984d3d6b9a0cfb7b0068
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-07 18:16:01 +1000

    🎉 Update

--- src/interfaces/lens/IVaultETHWrapper.sol
+++ src/interfaces/lens/IVaultETHWrapper.sol
@@ -8,13 +8,13 @@ import {IVault} from "./IVault.sol";
 // Wraps around a vault and allows for ETH to ERC20 deposits by autowrapping.
 interface IVaultETHWrapper {
     // Get the wrapped ETH token 'WETH' that the vault wrapper users.
-    function WETH() 
+    function WETH() external view returns (IWETH weth);
 
     // Deposits senders funds into the vault and exchanges any ETH for its wrapped equivalent.
     // Sender will receive the same number of shares as if they had deposited themselves.
     function deposit(IVault vault, uint256[] calldata amount) external payable returns (uint256 shares);
 
-    // Redeems senders shares and exchanges and wrapped ETH equivalents for ETH.
+    // Redeems senders shares and exchanges their wrapped ETH equivalent for ETH.
     // Sender will receive their amounts as if they had redeemed the shares themselves.
     function redeem(IVault vault, uint256 shares) external returns (uint256[] calldata amount);
 }
