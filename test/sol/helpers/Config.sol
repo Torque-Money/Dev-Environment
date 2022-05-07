7128df03442158121c8d9fa2ca1571f0cdfc82a8
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-07 19:46:12 +1000

    🎉 Update

--- test/sol/helpers/Config.sol
+++ test/sol/helpers/Config.sol
@@ -56,17 +56,17 @@ library Config {
         //     IBeefyVaultV6(0x70c6AF9Dff8C19B3db576E5E199B22A883874f05)
         // ); // Beefy USDC-FTM LP WigoSwap
 
-        return (
-            IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506),
-            IUniswapV2Factory(0xc35DADB65012eC5796536bD9864eD8773aBc74C4),
-            IBeefyVaultV6(0xb870e4755C737D2753D7298D0e70344077905Ed5)
-        ); // Beefy USDC-FTM LP SushiSwap
-
         // return (
-        //     IUniswapV2Router02(0xF491e7B69E4244ad4002BC14e878a34207E38c29),
-        //     IUniswapV2Factory(0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3),
-        //     IBeefyVaultV6(0x41D44B276904561Ac51855159516FD4cB2c90968)
-        // ); // Beefy USDC-FTM LP SpookySwap
+        //     IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506),
+        //     IUniswapV2Factory(0xc35DADB65012eC5796536bD9864eD8773aBc74C4),
+        //     IBeefyVaultV6(0xb870e4755C737D2753D7298D0e70344077905Ed5)
+        // ); // Beefy USDC-FTM LP SushiSwap
+
+        return (
+            IUniswapV2Router02(0xF491e7B69E4244ad4002BC14e878a34207E38c29),
+            IUniswapV2Factory(0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3),
+            IBeefyVaultV6(0x41D44B276904561Ac51855159516FD4cB2c90968)
+        ); // Beefy USDC-FTM LP SpookySwap
     }
 
     function getFos() internal pure returns (uint256 fosPercent, uint256 fosDenominator) {
