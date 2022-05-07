c07f2f9f7df846cbed11aac78e91079d4333fe50
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-07 16:05:13 +1000

    🎉 Update

--- src/interfaces/lens/IVaultETHWrapper.sol
+++ src/interfaces/lens/IVaultETHWrapper.sol
@@ -1,10 +1,15 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import {IWETH} from "../../../lib/weth/IWETH.sol";
+
 import {IVault} from "./IVault.sol";
 
 // Wraps around a vault and allows for ETH to ERC20 deposits by autowrapping.
 interface IVaultETHWrapper {
+    // Get the wrapped ETH token 'WETH' that the vault wrapper users.
+    function WETH() 
+
     // Deposits senders funds into the vault and exchanges any ETH for its wrapped equivalent.
     // Sender will receive the same number of shares as if they had deposited themselves.
     function deposit(IVault vault, uint256[] calldata amount) external payable returns (uint256 shares);
