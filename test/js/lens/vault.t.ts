5273273e093f85b9a8ca172c0bcd5f2b2a62df8b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-08 22:55:53 +1000

    🎉 Update

--- test/js/lens/vault.t.ts
+++ test/js/lens/vault.t.ts
@@ -1,12 +1,14 @@
 import hre from "hardhat";
 
+import { Vault, VaultETHWrapper } from "../../../typechain-types/src/lens/vault";
+
 import { loadData } from "../../../scripts/utils";
 
 async function main() {
     const data = loadData();
 
-    const wrapper = await hre.ethers.getContractAt("VaultETHWrapper", data.contracts.VaultETHWrapper.proxy);
-    const vault = await hre.ethers.getContractAt("Vault", data.contracts.Vault.proxies[0]);
+    const wrapper = (await hre.ethers.getContractAt("VaultETHWrapper", data.contracts.VaultETHWrapper.proxy)) as VaultETHWrapper;
+    const vault = (await hre.ethers.getContractAt("Vault", data.contracts.Vault.proxies[0])) as Vault;
 
     const ftm = await hre.ethers.getContractAt("IERC20Upgradeable", "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83");
     const usdc = await hre.ethers.getContractAt("IERC20Upgradeable", "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75");
@@ -14,11 +16,21 @@ async function main() {
     // Approve the tokens for use with the wrapper
     await ftm.approve(wrapper.address, hre.ethers.constants.MaxUint256);
     await usdc.approve(wrapper.address, hre.ethers.constants.MaxUint256);
-    console.log("Approved tokens");
+    console.log("Test | Vault | Approved tokens");
 
     // Test the deposit
+    const amount = [1, 1];
+    await wrapper.deposit(vault.address, amount);
+    console.log("Test | Vault | Deposited");
 
     // Test the withdraw
+    const caller = await hre.ethers.provider.getSigner().getAddress();
+
+    await vault.approve(wrapper.address, hre.ethers.constants.MaxUint256);
+
+    const shares = await vault.balanceOf(caller);
+    await wrapper.redeem(vault.address, shares);
+    console.log("Test | Vault | Redeemed");
 }
 
 main()
