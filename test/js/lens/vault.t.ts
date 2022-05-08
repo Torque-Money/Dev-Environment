052631a1eaeee3cd98f62a35fae10e641700c75b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-08 22:50:56 +1000

    🎉 Update

--- test/js/lens/vault.t.ts
+++ test/js/lens/vault.t.ts
@@ -11,6 +11,11 @@ async function main() {
     const ftm = await hre.ethers.getContractAt("IERC20Upgradeable", "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83");
     const usdc = await hre.ethers.getContractAt("IERC20Upgradeable", "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75");
 
+    // Approve the tokens for use with the wrapper
+    await ftm.approve(wrapper.address, hre.ethers.constants.MaxUint256);
+    await usdc.approve(wrapper.address, hre.ethers.constants.MaxUint256);
+    console.log("Approved tokens");
+
     // Test the deposit
 
     // Test the withdraw
