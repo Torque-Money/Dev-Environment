6b9315610bdeb885f44f357dbb922da711a4fbd4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-15 08:21:19 +1000

    🎉 Update

--- src/lens/lens/LensController.sol
+++ src/lens/lens/LensController.sol
@@ -3,4 +3,17 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
-contract LensController
+import {ILensController} from "../../interfaces/lens/ILensController.sol";
+
+import {ILens} from "../../interfaces/lens/ILens.sol";
+
+contract LensController is ILensController {
+    // Get the lens that the controller controls.
+    function getLens() external view override returns (ILens lens) {}
+
+    // Returns whether or not the lens can update the strategy.
+    function isUpdateable() external view override returns (bool _isUpdateable) {}
+
+    // Update the strategy.
+    function update() external override {}
+}
