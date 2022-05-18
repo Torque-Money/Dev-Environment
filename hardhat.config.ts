7503ea19e64b308c72250c292ee937915f45a577
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-18 22:54:59 +1000

    🎉 Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,10 +9,7 @@ require("dotenv").config();
 
 export default {
     solidity: {
-        compilers: [
-            { version: "0.8.10", settings: { optimizer: { enabled: true, runs: 200 } } },
-            { version: "0.6.6", settings: { optimizer: { enabled: true, runs: 200 } } },
-        ],
+        compilers: [{ version: "0.8.10", settings: { optimizer: { enabled: true, runs: 200 } } }],
     },
     paths: {
         sources: "src",
